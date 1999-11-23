d33e6d04d9095b7e6993546b1bba52f6b3948da8
Luke Ehresman <luke@ehresman.org>
1999-11-23 23:00:34 +0000

    Added deleting of messages

--- config/config.php
+++ config/config.php
@@ -11,6 +11,22 @@ $imapServerAddress = "adam.usa.om.org";
 /* This is displayed right after they log in */
 $motd = "  Welcome to OM's webmail system, SquirrelMail.  We are currently in beta, and have not yet released a full version of SquirrelMail.  Please feel free to look around, and please report any bugs to <A HREF=\"mailto:nathan@usa.om.org\">Nathan</A> or <A HREF=\"mailto:luke@usa.om.org\">Luke</A>.";
 
-/* SquirrelMail version number */
+/* SquirrelMail version number -- DO NOT CHANGE */
 $version = "0.0.1";
+
+/* The following are related to deleting messages.
+ *   $move_to_trash
+ *         - if this is set to "true", when "delete" is pressed, it will attempt
+ *           to move the selected messages to the folder named $trash_folder.  If
+ *           it's set to "false", we won't even attempt to move the messages, just
+ *           delete them.
+ *   $trash_folder
+ *         - This is the path to the default trash folder.  For Cyrus IMAP, it
+ *           would be "INBOX.Trash", but for UW it would be "Trash".  We need the
+ *           full path name here.
+ */
+
+$move_to_trash = true;
+$trash_folder = "INBOX.Trash";
+
 ?>
