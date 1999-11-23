842d581564b7d0b7d2bbc338cf0c53df17f73e73
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
1999-11-23 03:17:53 +0000

    Links successfully to todo and bug manager

--- trunk/squirrelmail/functions/page_header.php
+++ trunk/squirrelmail/functions/page_header.php
@@ -22,7 +22,7 @@
       echo "         <FONT FACE=\"Arial,Helvetica\">Addresses</FONT>&nbsp&nbsp";
       echo "         <FONT FACE=\"Arial,Helvetica\">Options</FONT>&nbsp&nbsp";
       echo "      </TD><TD ALIGN=right WIDTH=50%>";
-      echo "         <FONT FACE=\"Arial,Helvetica\"><A HREF=\"http://adam.usa.om.org/~luke/main.php\" TARGET=_top>Todos & Bugs</A></FONT>&nbsp&nbsp";
+      echo "         <FONT FACE=\"Arial,Helvetica\"><A HREF=\"http://adam.usa.om.org/~luke/main.php3\" TARGET=_top>Todos & Bugs</A></FONT>&nbsp&nbsp";
       echo "         <FONT FACE=\"Arial,Helvetica\">Help!</FONT>";
       echo "      </TD>";
       echo "</TABLE>";
