abb55148cfa4d5bcc7c083a2410bd64dfca5c888
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
1999-11-28 21:44:50 +0000

    Added the folders option in the header

--- trunk/squirrelmail/functions/page_header.php
+++ trunk/squirrelmail/functions/page_header.php
@@ -10,18 +10,20 @@
       /** Here is the header and wrapping table **/
       $shortBoxName = readShortMailboxName($mailbox, ".");
       echo "<TABLE BGCOLOR=FFFFFF BORDER=0 COLS=2 WIDTH=100% CELLSPACING=0 CELLPADDING=2>";
-      echo "   <TR BGCOLOR=A0B8C8 WIDTH=50%>";
-      echo "      <TD ALIGN=left>";
-      echo "         <FONT FACE=\"Arial,Helvetica\" SIZE=-1><A HREF=\"signout.php\" TARGET=_top><B>Sign Out</B></A></FONT>";
-      echo "      </TD><TD ALIGN=right WIDTH=50%>";
+      echo "   <TR BGCOLOR=A0B8C8 WIDTH=100%>";
+      echo "      <TD ALIGN=left WIDTH=30%>";
+      echo "         <FONT FACE=\"Arial,Helvetica\"><A HREF=\"signout.php\" TARGET=_top><B>Sign Out</B></A></FONT>";
+      echo "      </TD><TD ALIGN=right WIDTH=70%>";
       echo "         <FONT FACE=\"Arial,Helvetica\"><div align=right>Current Folder: <B>$shortBoxName</div></B></FONT>";
       echo "      </TD>";
-      echo "   </TR><TR>";
-      echo "      <TD ALIGN=left WIDTH=50%>";
+      echo "   </TR></TABLE>\n";
+      echo "<TABLE BGCOLOR=FFFFFF BORDER=0 COLS=2 WIDTH=100% CELLSPACING=0 CELLPADDING=2><TR>";
+      echo "      <TD ALIGN=left WIDTH=70%>";
       echo "         <FONT FACE=\"Arial,Helvetica\">Compose</FONT>&nbsp&nbsp";
       echo "         <FONT FACE=\"Arial,Helvetica\">Addresses</FONT>&nbsp&nbsp";
+      echo "         <FONT FACE=\"Arial,Helvetica\"><A HREF=\"folders.php\">Folders</A></FONT>&nbsp&nbsp";
       echo "         <FONT FACE=\"Arial,Helvetica\">Options</FONT>&nbsp&nbsp";
-      echo "      </TD><TD ALIGN=right WIDTH=50%>";
+      echo "      </TD><TD ALIGN=right WIDTH=30%>";
       echo "         <FONT FACE=\"Arial,Helvetica\"><A HREF=\"http://adam.usa.om.org/~luke/main.php3\" TARGET=_top>Todos & Bugs</A></FONT>&nbsp&nbsp";
       echo "         <FONT FACE=\"Arial,Helvetica\">Help!</FONT>";
       echo "      </TD>";
