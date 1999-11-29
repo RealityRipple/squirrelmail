1d1f287eba6ead2f5cfe58ccaa01dc554d475b52
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
1999-11-29 00:59:12 +0000

    added soem files

---
+++ trunk/squirrelmail/src/move_messages.php
@@ -0,0 +1,57 @@
+<?
+   include("../config/config.php");
+   include("../functions/mailbox.php");
+   include("../functions/strings.php");
+   include("../functions/page_header.php");
+   include("../functions/display_messages.php");
+
+   $imapConnection = fsockopen($imapServerAddress, 143, &$errorNumber, &$errorString);
+   if (!$imapConnection) {
+      echo "Error connecting to IMAP Server.<br>";
+      echo "$errorNumber : $errorString<br>";
+      exit;
+   }
+   $serverInfo = fgets($imapConnection, 256);
+
+   // login
+   fputs($imapConnection, "1 login $username $key\n");
+   $read = fgets($imapConnection, 1024);
+
+   if (strpos($read, "NO")) {
+      error_username_password_incorrect();
+      exit;
+   }
+
+   // switch to the mailbox, and get the number of messages in it.
+   selectMailbox($imapConnection, $mailbox, $numMessages);
+
+   // Marks the selected messages ad 'Deleted'
+   $j = 0;
+   $i = 0;
+
+   while ($j < count($msg)) {
+      if ($msg[$i]) {
+         /** check if they would like to move it to the trash folder or not */
+         if ($move_to_trash == true) {
+            $success = copyMessages($imapConnection, $msg[$i], $msg[$i], $trash_folder);
+            if ($success == true)
+               setMessageFlag($imapConnection, $msg[$i], $msg[$i], "Deleted");
+         } else {
+            setMessageFlag($imapConnection, $msg[$i], "Deleted");
+         }
+         $j++;
+      }
+      $i++;
+   }
+
+   if ($auto_expunge == true)
+      expungeBox($imapConnection, $mailbox, $numMessages);
+
+   // Log out this session
+   fputs($imapConnection, "1 logout");
+
+   echo "<HTML><BODY TEXT=\"#000000\" BGCOLOR=\"#FFFFFF\" LINK=\"#0000EE\" VLINK=\"#0000EE\" ALINK=\"#0000EE\">";
+   displayPageHeader($mailbox);
+
+   messages_deleted_message($mailbox, $sort, $startMessage);
+?>
