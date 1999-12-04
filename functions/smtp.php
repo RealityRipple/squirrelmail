45a065a2f95f7b59533caf087fb1493286e5e8ad
Luke Ehresman <luke@ehresman.org>
1999-12-04 21:42:37 +0000

    Composing messages works great!

--- functions/smtp.php
+++ functions/smtp.php
@@ -16,9 +16,11 @@
       }
    }
 
-   function sendMessage($smtpServerAddress, $smtpPort, $to, $subject, $body) {
-      $to = addslashes($to);
-      $body = addslashes($body);
+   function sendMessage($smtpServerAddress, $smtpPort, $username, $domain, $t, $c, $b, $subject, $body, $version) {
+      $to = parseAddrs($t);
+      $cc = parseAddrs($c);
+      $bcc = parseAddrs($b);
+      $body = $body;
       $from = "$username@$domain";
 
       echo "<FONT FACE=\"Arial,Helvetica\">";
@@ -28,27 +30,39 @@
          echo "$errorNumber : $errorString<br>";
          exit;
       }
-      echo htmlspecialchars(fgets($smtpConnection, 1024)) . "<BR>";
 
+      $to_list = getLineOfAddrs($to);
+      $cc_list = getLineOfAddrs($cc);
+
+      /** Lets introduce ourselves */
+      fputs($smtpConnection, "HELO $domain\n");
+      /** Ok, who is sending the message? */
       fputs($smtpConnection, "MAIL FROM:<$from>\n");
-      echo htmlspecialchars(fgets($smtpConnection, 1024)) . "<BR>";
 
-      fputs($smtpConnection, "RCPT TO:<$to>\n");
-      echo htmlspecialchars(fgets($smtpConnection, 1024)) . "<BR>";
+      /** send who the recipients are */
+      for ($i = 0; $i < count($to); $i++) {
+         fputs($smtpConnection, "RCPT TO:<$to[$i]>\n");
+      }
+      for ($i = 0; $i < count($cc); $i++) {
+         fputs($smtpConnection, "RCPT TO:<$cc[$i]>\n");
+      }
+      for ($i = 0; $i < count($bcc); $i++) {
+         fputs($smtpConnection, "RCPT TO:<$bcc[$i]>\n");
+      }
 
+      /** Lets start sending the actual message */
       fputs($smtpConnection, "DATA\n");
-      echo htmlspecialchars(fgets($smtpConnection, 1024)) . "<BR>";
+      fputs($smtpConnection, "Subject: $subject\n"); // Subject
+      fputs($smtpConnection, "To: <$to_list>\n");    // Who it's TO
+      if ($cc_list) {
+         fputs($smtpConnection, "Cc: <$cc_list>\n"); // Who the CCs are
+      }
+      fputs($smtpConnection, "X-Mailer: SquirrelMail (version $version)\n"); // Identify SquirrelMail
 
-      fputs($smtpConnection, "Subject: $subject\n");
-      fputs($smtpConnection, "Date: " . date() . "\n");
-      fputs($smtpConnection, "To: <$to>\n");
-      fputs($smtpConnection, "From: <$from>\n");
-      fputs($smtpConnection, "$body\n");
-      fputs($smtpConnection, ".\n");
-      echo htmlspecialchars(fgets($smtpConnection, 1024)) . "<BR>";
+      fputs($smtpConnection, "$body\n"); // send the body of the message
+      fputs($smtpConnection, ".\n"); // end the DATA part
+      fputs($smtpConnection, "QUIT\n"); // log off
 
-      fputs($smtpConnection, "QUIT\n");
-      echo htmlspecialchars(fgets($smtpConnection, 1024)) . "<BR>";
       echo "</FONT>";
    }
 ?>
