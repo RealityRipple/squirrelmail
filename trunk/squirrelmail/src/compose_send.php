a3d8ecdca745cabcfcfe201e6287e01e5b656f34
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
1999-12-04 20:05:29 +0000

    updated some sending stuff

--- trunk/squirrelmail/src/compose_send.php
+++ trunk/squirrelmail/src/compose_send.php
@@ -5,6 +5,6 @@
    include("../functions/mailbox.php");
    include("../functions/smtp.php");
 
-   sendMessage($passed_to, $passed_subject, $passed_body);
+   sendMessage($smtpServerAddress, $smtpPort, $passed_to, $passed_subject, $passed_body);
    echo "<A HREF=\"right_main.php\">RETURN</A>";
 ?>
