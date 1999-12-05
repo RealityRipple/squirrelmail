91ba4c227d5c6041290b947e00ea165595e99adc
Luke Ehresman <luke@ehresman.org>
1999-12-05 18:20:09 +0000

    Beautified top of message list a bit

--- src/folders_create.php
+++ src/folders_create.php
@@ -1,3 +1,6 @@
+<HTML>
+<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=webmail.php?right_frame=folders.php" TARGET=_top>
+<BODY TEXT="#000000" BGCOLOR="#FFFFFF" LINK="#0000EE" VLINK="#0000EE" ALINK="#0000EE">
 <?
    include("../config/config.php");
    include("../functions/strings.php");
@@ -8,7 +11,7 @@
    fputs($imapConnection, "1 create \"$subfolder.$folder_name\"\n");
    fputs($imapConnection, "1 logout\n");
 
-   echo "<BR><BR><A HREF=\"webmail.php?right_frame=folders.php\" TARGET=_top>Return</A>";
+   echo "<CENTER><BR><BR>You will be automatically forwarded.<BR>If not, <A HREF=\"webmail.php?right_frame=folders.php\" TARGET=_top>click here</A></CENTER>";
 ?>
-
+</BODY></HTML>
 
