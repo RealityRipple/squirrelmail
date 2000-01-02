db30ec26fde0f4d38232621549a6185c2acfc0b6
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-01-02 20:22:08 +0000

    updated the config file for themable support, and added 2 themes

---
+++ trunk/squirrelmail/config/default_theme.php
@@ -0,0 +1,19 @@
+<?
+   /** This is the default theme that comes standard with SquirrelMail.
+       Most of the colors were taken from mail.yahoo.com's theme on their
+       site.
+    **/
+
+    $color[0]   = "DCDCDC"; // Light Gray
+    $color[1]   = "800000"; // Red
+    $color[2]   = "CC0000"; // Light Red
+    $color[3]   = "A0B8C8"; // Left Background
+    $color[4]   = "FFFFFF"; // Background
+    $color[5]   = "FFFFCC"; // Light Yellow
+    $color[6]   = "000000"; // Left Text
+    $color[7]   = "0000CC"; // Link
+    $color[8]   = "000000"; // Text
+    $color[9]   = "ABABAB"; // Darker light gray
+    $color[10]  = "666666"; // Darker light gray
+    $color[11]  = "770000"; // Special folder color
+?>
