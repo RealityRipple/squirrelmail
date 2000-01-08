21c55656eee94a375c81ac8a3e04d4b89f4557f9
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-01-08 20:38:51 +0000

    added some debugging information

--- trunk/squirrelmail/src/options_submit.php
+++ trunk/squirrelmail/src/options_submit.php
@@ -13,13 +13,16 @@
    echo "<BODY TEXT=\"$color[8]\" BGCOLOR=\"$color[4]\" LINK=\"$color[7]\" VLINK=\"$color[7]\" ALINK=\"$color[7]\">\n";
    displayPageHeader($color, "None");
 
-   setPref($data_dir, $username, "full_name", $full_name);
-   setPref($data_dir, $username, "reply_to", $reply_to);
+   setPref($data_dir, $username, "full_name", stripslashes($full_name));
+   setPref($data_dir, $username, "reply_to", stripslashes($reply_to));
    setPref($data_dir, $username, "chosen_theme", $chosentheme);
    setPref($data_dir, $username, "move_to_trash", $movetotrash);
    setPref($data_dir, $username, "wrap_at", $wrapat);
    setPref($data_dir, $username, "editor_size", $editorsize);
    setPref($data_dir, $username, "use_signature", $usesignature);
+
+   echo $signature_edit;
+   echo stripslashes($signature_edit);
 
    setSig($data_dir, $username, stripslashes($signature_edit));
 
