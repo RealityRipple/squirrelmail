789c5c9a0d2c40887d0c2cacd278f84cc574bbe8
Luke Ehresman <luke@ehresman.org>
2000-03-25 12:17:34 +0000

    added folders_subscribe

---
+++ src/folders_subscribe.php
@@ -0,0 +1,44 @@
+<?
+   if (!isset($config_php))
+      include("../config/config.php");
+   if (!isset($strings_php))
+      include("../functions/strings.php");
+   if (!isset($page_header_php))
+      include("../functions/page_header.php");
+   if (!isset($imap_php))
+      include("../functions/imap.php");
+   if (!isset($display_messages_php))
+      include("../functions/display_messages.php");
+
+   include("../src/load_prefs.php");
+
+   $imapConnection = sqimap_login($username, $key, $imapServerAddress, $imapPort, 0);
+   $dm = sqimap_get_delimiter($imapConnection);
+
+   if ($method == "sub") {
+      $mailbox = trim($mailbox);
+      sqimap_subscribe ($imapConnection, $mailbox);
+   } else {
+      sqimap_unsubscribe ($imapConnection, $mailbox);
+   }
+
+   echo "<html><BODY TEXT=\"$color[8]\" BGCOLOR=\"$color[4]\" LINK=\"$color[7]\" VLINK=\"$color[7]\" ALINK=\"$color[7]\">\n";
+   displayPageHeader($color, "None");
+   echo "<BR><BR><BR><CENTER><B>";
+   if ($method == "sub") {
+      echo _("Subscribed Successfully!");
+      echo "</B><BR><BR>";
+      echo _("You have been successfully subscribed.");
+   } else {
+      echo _("Unsubscribed Successfully!");
+      echo "</B><BR><BR>";
+      echo _("You have been successfully unsubscribed.");
+   }
+   echo "<BR><A HREF=\"webmail.php?right_frame=folders.php\" TARGET=_top>";
+   echo _("Click here");
+   echo "</A> ";
+   echo _("to continue.");
+   echo "</CENTER>";
+   echo "</BODY></HTML>";
+?>
+
