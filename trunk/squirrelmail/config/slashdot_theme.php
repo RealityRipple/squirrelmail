26e624566f7aa7c6badbd6e208346c90a15a8f4d
pallo <pallo@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-03-31 09:26:50 +0000

    Fixed color codes.

--- trunk/squirrelmail/config/slashdot_theme.php
+++ trunk/squirrelmail/config/slashdot_theme.php
@@ -7,16 +7,16 @@
        for Nerds" web site, www.slashdot.org.
     **/
 
-    $color[0]   = "DCDCDC"; // (light gray)     TitleBar
-    $color[1]   = "800000"; // (red)
-    $color[2]   = "CC0000"; // (light red)      Warning/Error Messages
-    $color[3]   = "006668"; // (/. color)       Left Bar Background
-    $color[4]   = "FFFFFF"; // (white)          Normal Background
-    $color[5]   = "BABABA"; // (darker gray)    Table Headers
-    $color[6]   = "000000"; // (black)          Text on left bar
-    $color[7]   = "004949"; // (light /. color) Links
-    $color[8]   = "000000"; // (black)          Normal text
-    $color[9]   = "ABABAB"; // (mid-gray)       Darker version of #0
-    $color[10]  = "666666"; // (dark gray)      Darker version of #9
-    $color[11]  = "770000"; // (dark red)       Special Folders color
+    $color[0]   = "#DCDCDC"; // (light gray)     TitleBar
+    $color[1]   = "#800000"; // (red)
+    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+    $color[3]   = "#006668"; // (/. color)       Left Bar Background
+    $color[4]   = "#FFFFFF"; // (white)          Normal Background
+    $color[5]   = "#BABABA"; // (darker gray)    Table Headers
+    $color[6]   = "#000000"; // (black)          Text on left bar
+    $color[7]   = "#004949"; // (light /. color) Links
+    $color[8]   = "#000000"; // (black)          Normal text
+    $color[9]   = "#ABABAB"; // (mid-gray)       Darker version of #0
+    $color[10]  = "#666666"; // (dark gray)      Darker version of #9
+    $color[11]  = "#770000"; // (dark red)       Special Folders color
 ?>
