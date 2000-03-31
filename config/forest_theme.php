99f9e19c3a21a349d9e15a0f675fe76fd9e9b228
pallo <pallo@squirrelmail.org>
2000-03-31 09:26:50 +0000

    Fixed color codes.

--- config/forest_theme.php
+++ config/forest_theme.php
@@ -6,16 +6,16 @@
        This theme uses Forest-ish colors (mostly green)
     **/
 
-    $color[0]   = "9EBFA7"; // (light purp)     TitleBar
-    $color[1]   = "800000"; // (red)
-    $color[2]   = "CC0000"; // (light red)      Warning/Error Messages
-    $color[3]   = "244B30"; // (purple)         Left Bar Background
-    $color[4]   = "DEFFE7"; // (light purple)   Normal Background
-    $color[5]   = "B6D7BF"; // (darker gray)    Table Headers
-    $color[6]   = "DDFFDD"; // (black)          Text on left bar
-    $color[7]   = "3C7F50"; // (light purple)   Links
-    $color[8]   = "000000"; // (black)          Normal text
-    $color[9]   = "7E9F87"; // (mid purp)       Darker version of #0
-    $color[10]  = "5E7F67"; // (dark purp)      Darker version of #9
-    $color[11]  = "4A9B62"; // (dark red)       Special Folders color
+    $color[0]   = "#9EBFA7"; // (light purp)     TitleBar
+    $color[1]   = "#800000"; // (red)
+    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+    $color[3]   = "#244B30"; // (purple)         Left Bar Background
+    $color[4]   = "#DEFFE7"; // (light purple)   Normal Background
+    $color[5]   = "#B6D7BF"; // (darker gray)    Table Headers
+    $color[6]   = "#DDFFDD"; // (black)          Text on left bar
+    $color[7]   = "#3C7F50"; // (light purple)   Links
+    $color[8]   = "#000000"; // (black)          Normal text
+    $color[9]   = "#7E9F87"; // (mid purp)       Darker version of #0
+    $color[10]  = "#5E7F67"; // (dark purp)      Darker version of #9
+    $color[11]  = "#4A9B62"; // (dark red)       Special Folders color
 ?>
--- config/seaspray_theme.php
+++ config/seaspray_theme.php
@@ -4,16 +4,16 @@
        Theme Name:   "Sea Spray Theme"
     **/
 
-    $color[0]   = "7CA4A4"; 
-    $color[1]   = "800000";
-    $color[2]   = "CC0000";
-    $color[3]   = "588080";
-    $color[4]   = "DCFFFF";
-    $color[5]   = "98C0C0";
-    $color[6]   = "000000";
-    $color[7]   = "184040";
-    $color[8]   = "000000";
-    $color[9]   = "78A0A0";
-    $color[10]  = "386060";
-    $color[11]  = "FFFFB0";
+    $color[0]   = "#7CA4A4"; 
+    $color[1]   = "#800000";
+    $color[2]   = "#CC0000";
+    $color[3]   = "#588080";
+    $color[4]   = "#DCFFFF";
+    $color[5]   = "#98C0C0";
+    $color[6]   = "#000000";
+    $color[7]   = "#184040";
+    $color[8]   = "#000000";
+    $color[9]   = "#78A0A0";
+    $color[10]  = "#386060";
+    $color[11]  = "#FFFFB0";
 ?>
