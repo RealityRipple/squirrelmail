b85b8d6040ce1d2d8e7bdf60113b90f5ff305c33
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-04-18 01:10:27 +0000

    re-fixed bug for fetching headers.  (:

--- trunk/squirrelmail/functions/imap_messages.php
+++ trunk/squirrelmail/functions/imap_messages.php
@@ -40,7 +40,8 @@
     **  Returns some general header information -- FROM, DATE, and SUBJECT
     ******************************************************************************/
    function sqimap_get_small_header ($imap_stream, $id, &$from, &$subject, &$date) {
-      fputs ($imap_stream, "a001 FETCH $id:$id BODY[HEADER.FIELDS (DATE FROM SUBJECT)]\r\n");
+      //fputs ($imap_stream, "a001 FETCH $id BODY[HEADER.FIELDS (DATE FROM SUBJECT)]\r\n");
+      fputs ($imap_stream, "a001 FETCH $id RFC822.HEADER\r\n");
       $read = sqimap_read_data ($imap_stream, "a001", true, $response, $message);
 
       $subject = _("(no subject)");
