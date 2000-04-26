1b3e4077f3accf76feeb67ed06e6ef1b19e11f23
oricn <oricn@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-04-26 20:58:23 +0000

    Fixed a glich that caused the 'Viewing message' line to show a strange colour when in an empty folder. (Line 163)

--- functions/mailbox_display.php
+++ functions/mailbox_display.php
@@ -160,7 +160,7 @@
          echo "<CENTER>". _("Viewing messages ") ."<B>$startMessage</B>". _(" to ") ."<B>$endMessage</B> ($numMessages total)</CENTER>\n";
          echo "</TD></TR>\n";
       } else if ($startMessage == $endMessage) {
-         echo "<TR BGCOLOR=\"$color[4]>\"TD>";
+         echo "<TR BGCOLOR=\"$color[4]\"><TD>";
          echo "<CENTER>". _("Viewing message ") ."<B>$startMessage</B> ($numMessages ". _("total") .")</CENTER>\n";
          echo "</TD></TR>\n";
       }
