012fa21cde02553312f1bedca4642ec8e1f09fe0
Luke Ehresman <luke@ehresman.org>
2000-04-30 20:27:19 +0000

    added blue theme

---
+++ config/plain_blue_theme.php
@@ -0,0 +1,19 @@
+<?php
+   /** Author:       Luke Ehresman
+       Date:         April 30, 2000
+       Theme Name:   "Plain Blue"
+    **/
+
+    $color[0]   = "#B2C5D4"; // (light gray)     TitleBar
+    $color[1]   = "#800000"; // (red)
+    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+    $color[3]   = "#6795B9"; // (green-blue)     Left Bar Background
+    $color[4]   = "#FFFFFF"; // (white)          Normal Background
+    $color[5]   = "#98B0C3"; // (light yellow)   Table Headers
+    $color[6]   = "#000000"; // (black)          Text on left bar
+    $color[7]   = "#0000CC"; // (blue)           Links
+    $color[8]   = "#000000"; // (black)          Normal text
+    $color[9]   = "#94ADC1"; // (mid-gray)       Darker version of #0
+    $color[10]  = "#8D97A0"; // (dark gray)      Darker version of #9
+    $color[11]  = "#770000"; // (dark red)       Special Folders color
+?>
