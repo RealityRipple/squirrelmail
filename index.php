d32cce5ff1d5fa7c7d2d9afc96a9c4ba16e331be
pallo <pallo@squirrelmail.org>
2000-05-04 21:36:21 +0000

    Try a little harder to guess th URL.

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
-   $headers = getallheaders();
-   $path = substr($PHP_SELF, 0, strrpos($PHP_SELF, '/'));
-   $location = $headers["Host"] . $path;
 
-   header("Location: http://$location/src/login.php\n\n");
+   // Get the path
+   $path = substr($PHP_SELF, 0, strrpos($PHP_SELF, '/'));
+
+   // Check if this is a HTTPS or regular HTTP request
+   $proto = "http://";
+   if(isset($HTTPS) && $HTTPS == 'on' ) {
+     $proto = "https://";
+   }
+
+   // Get the hostname from the Host header or server config.
+   // Fallback is to omit the server name and use a relative URI,
+   // although this is not RFC 2616 compliant.
+   if(isset($HTTP_HOST) && !empty($HTTP_HOST)) {
+     $location = $proto . $HTTP_HOST . $path;
+   } else if(isset($SERVER_NAME) && !empty($SERVER_NAME)) {
+     $location = $proto . $SERVER_NAME . $path;
+   } else {
+     $location = $path;
+   }
+
+   // Redirect
+   header("Location: $location/src/login.php\n\n");
    exit();
 ?>
