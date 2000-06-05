7cc620484ad5e4131408dfffe2d1169b54c044cd
lbergman <lbergman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-06-05 20:09:12 +0000

    added help to menu choices

--- trunk/squirrelmail/functions/page_header.php
+++ trunk/squirrelmail/functions/page_header.php
@@ -85,6 +85,7 @@
       echo "         <A HREF=\"addressbook.php\">" . _("Addresses") . "</A>&nbsp;&nbsp;\n";
       echo "         <A HREF=\"folders.php\">" . _("Folders") . "</A>&nbsp;&nbsp;\n";
       echo "         <A HREF=\"options.php\">" . _("Options") . "</A>&nbsp;&nbsp;\n";
+      echo "         <A HREF=\"webmail.php?right_frame=help.php\" TARGET=\"Help Me!\">" . _("Help") . "</A>&nbsp&nbsp";
       echo "      </TD><TD ALIGN=right WIDTH=\"30%\">\n";
       echo "         <A HREF=\"http://squirrelmail.sourceforge.net/index.php3?from=1\" TARGET=\"_top\">SquirrelMail</A>\n";
       echo "      </TD>\n";
