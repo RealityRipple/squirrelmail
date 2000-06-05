4c5a7448ea2bb94ffcb694ac782dbc5e3ad2958a
lbergman <lbergman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-06-05 14:47:35 +0000

    added dark blue/grey theme

---
+++ config/bluesteel_theme.php
@@ -0,0 +1,31 @@
+<?php
+   /** Author:       Luke Ehresman
+       Date:         January 2, 2000
+       Theme Name:   "Default Theme"
+
+       This is the default theme that comes standard with SquirrelMail.
+       Most of the colors were taken from mail.yahoo.com's theme on their
+       site.
+
+       To create a new theme, just copy this file, and edit the colors.
+       Be sure to rename the file and keep the original, just in case.
+
+       NOTE:
+       Please email any themes you create to luke@usa.om.org for inclusion
+       in further releases of SquirrelMail and for download from the web
+       page.
+    **/
+
+    $color[0]   = "#A8A8A8"; // (darkish grey)   TitleBar
+    $color[1]   = "#800000"; // (red)
+    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+    $color[3]   = "#475A94"; // (darkish blue)   Left Bar Background
+    $color[4]   = "#DEDFDF"; // (grey)           Normal Background
+    $color[5]   = "#FFFFCC"; // (light yellow)   Table Headers
+    $color[6]   = "#000000"; // (black)          Text on left bar
+    $color[7]   = "#0000CC"; // (blue)           Links
+    $color[8]   = "#000000"; // (black)          Normal text
+    $color[9]   = "#ABABAB"; // (mid-gray)       Lighter version of #0
+    $color[10]  = "#666666"; // (dark gray)      Darker version of #0
+    $color[11]  = "#770000"; // (dark red)       Special Folders color
+?>
