d80f064a778ee0352c59711fba5b81c9b2ca2bcc
gustavf <gustavf@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-08-04 14:30:39 +0000

    Fixed yet another bug with small letters in QP encoding-

--- trunk/squirrelmail/functions/mime.php
+++ trunk/squirrelmail/functions/mime.php
@@ -536,7 +536,7 @@
             $replace = ereg_replace("_", " ", $res[3]);
 	    // Convert lowercase Quoted Printable to uppercase for
 	    // quoted_printable_decode to understand it.
-	    while (ereg("(=([0-9][a-f])|([a-f][0-9]))", $replace, $res)) {
+	    while (ereg("(=([0-9][a-f])|([a-f][0-9])|([a-f][0-9]))", $replace, $res)) {
 	       $replace = str_replace($res[1], strtoupper($res[1]), $replace);
 	    }
             $replace = quoted_printable_decode($replace);
