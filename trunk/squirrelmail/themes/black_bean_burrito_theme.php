a0374b61aad4fcef9d0ebb838920a96dc6c19c24
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2000-08-21 15:00:11 +0000

    added a theme from Ray Black

---
+++ trunk/squirrelmail/themes/black_bean_burrito_theme.php
@@ -0,0 +1,19 @@
+<?php
+   /** Author:       Ray Black
+       Date:         August 15, 2000
+       Theme Name:   "Black Bean Burrito"
+    **/
+
+    $color[0]   = "#595959"; // (mid-dark gray)		TitleBar
+    $color[1]   = "#F88888"; // (light red)			Quoted Replies in Emails
+    $color[2]   = "#ff7171"; // (pinkish)			Warning/Error Messages
+    $color[3]   = "#000000"; // (black)				Left Bar Background
+    $color[4]   = "#000000"; // (black)				Normal Background
+    $color[5]   = "#373700"; // (dark yellow-green)	Table Headers
+    $color[6]   = "#D0D0D0"; // (off-white)			Text on left bar
+    $color[7]   = "#cfcfa0"; // (very dull yellow)	Links
+    $color[8]   = "#D0D0D0"; // (off-white)			Normal text
+    $color[9]   = "#414141"; // (dark gray)			Darker version of #0
+    $color[10]  = "#292929"; // (darker gray)		Darker version of #9
+    $color[11]  = "#AAAA44"; // (light red)			Special Folders color
+?>
