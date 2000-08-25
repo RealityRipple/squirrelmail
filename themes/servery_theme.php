c3fdafaa2ecbac8274dbe65f8d45ba21bf57e9ac
Luke Ehresman <luke@ehresman.org>
2000-08-25 18:48:07 +0000

    added theme

---
+++ themes/servery_theme.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+  0: Title Bar at the top of the page header
+  1: <not currently used>
+  2: Error messages (usually red)
+  3: Left folder list background color
+  4: Normal background color
+  5: Header of the message index (From, Date, Subject)
+  6: Normal text on the left folder list
+  7: Links in the right frame
+  8: Normal text (usually black) 
+  9: Darker version of #0
+ 10: Darker version of #9
+ 11: Special folders color (Inbox, Trash, Sent)
+/*
+   /** Author:       Ondrej Sury <ondrej@globe.cz>
+       Date:         August 18, 2000
+       Theme Name:   "Servery.cz Theme"
+
+    **/
+
+    $color[0]   = "#F1EEE3"; // (light gray)     TitleBar
+    $color[1]   = "#800000"; // (red)
+    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+    $color[3]   = "#7D6F3F"; // (green-blue)     Left Bar Background
+    $color[4]   = "#FFFFFF"; // (white)          Normal Background
+    $color[5]   = "#C7B98B"; // (light yellow)   Table Headers
+    $color[6]   = "#000000"; // (black)          Text on left bar
+    $color[7]   = "#434031"; // (blue)           Links
+    $color[8]   = "#000000"; // (black)          Normal text
+    $color[9]   = "#C7B98B"; // (mid-gray)       Darker version of #0
+    $color[10]  = "#F1EEE3"; // (dark gray)      Darker version of #9
+    $color[11]  = "#770000"; // (dark red)       Special Folders color
+?>
