289a0b24c7e51019a822df8c9bfbf33c400bc62b
lbergman <lbergman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-02-09 00:26:02 +0000

    replaced "foo" with 'foo' while doing other

--- trunk/squirrelmail/src/search.php
+++ trunk/squirrelmail/src/search.php
@@ -10,93 +10,93 @@
       exit;
    }
    if(!isset($username) || !isset($key)) {
-      include ("../themes/default_theme.php");
-      include ("../functions/display_messages.php");
+      include ('../themes/default_theme.php');
+      include ('../functions/display_messages.php');
       printf('<html><BODY TEXT="%s" BGCOLOR="%s" LINK="%s" VLINK="%s" ALINK="%s">',
               $color[8], $color[4], $color[7], $color[7], $color[7]);
       plain_error_message(_("You need a valid user and password to access this page!")
-                          . "<br><a href=\"../src/login.php\">"
-                          . _("Click here to log back in.") . "</a>.", $color);
-      echo "</body></html>";
+                          . '<br><a href="../src/login.php">'
+                          . _("Click here to log back in.") . '</a>.', $color);
+      echo '</body></html>';
       exit;
    }
 
    if (!isset($strings_php))
-      include("../functions/strings.php");
+      include('../functions/strings.php');
    if (!isset($i18n_php))
-      include("../functions/i18n.php");
+      include('../functions/i18n.php');
    if (!isset($config_php))
-      include("../config/config.php");
+      include('../config/config.php');
    if (!isset($page_header_php))
-      include("../functions/page_header.php");
+      include('../functions/page_header.php');
    if (!isset($imap_php))
-      include("../functions/imap.php");
+      include('../functions/imap.php');
    if (!isset($imap_search_php))
-      include("../functions/imap_search.php");
+      include('../functions/imap_search.php');
    if (!isset($array_php))
-      include("../functions/array.php");
+      include('../functions/array.php');
 
-   include("../src/load_prefs.php");
+   include('../src/load_prefs.php');
 
    displayPageHeader($color, $mailbox);
    $imapConnection = sqimap_login($username, $key, $imapServerAddress, $imapPort, 0);
 
-   do_hook("search_before_form");
+   do_hook('search_before_form');
    echo "<br>\n";
    echo "      <table width=95% align=center cellpadding=2 cellspacing=0 border=0>\n";
    echo "      <tr><td bgcolor=\"$color[0]\">\n";
    echo "          <center><b>"._("Search")."</b></center>\n";
    echo "      </td></tr>\n";
-   echo "      <tr><td align=center>";
+   echo '      <tr><td align=center>';
 
    echo "<FORM ACTION=\"search.php\" NAME=s>\n";
    echo "   <TABLE WIDTH=75%>\n";
    echo "     <TR>\n";
    echo "       <TD WIDTH=33%>\n";
-   echo "         <TT><SMALL><SELECT NAME=\"mailbox\">";
+   echo '         <TT><SMALL><SELECT NAME="mailbox">';
 
    $boxes = sqimap_mailbox_list($imapConnection);
    for ($i = 0; $i < count($boxes); $i++) {
-	  if (!in_array("noselect", $boxes[$i]["flags"])) {
-         $box = $boxes[$i]["unformatted"];
-         $box2 = replace_spaces($boxes[$i]["unformatted-disp"]);
+	  if (!in_array('noselect', $boxes[$i]['flags'])) {
+         $box = $boxes[$i]['unformatted'];
+         $box2 = replace_spaces($boxes[$i]['unformatted-disp']);
          if ($mailbox == $box)
             echo "         <OPTION VALUE=\"$box\" SELECTED>$box2\n";
          else
             echo "         <OPTION VALUE=\"$box\">$box2\n";
       }
    }
-   echo "         </SELECT></SMALL></TT>";
+   echo '         </SELECT></SMALL></TT>';
    echo "       </TD>\n";
    echo "        <TD ALIGN=\"CENTER\" WIDTH=33%>\n";
    if (!isset($what))
-       $what = "";
-   $what_disp = ereg_replace(",", " ", $what);
-   $what_disp = str_replace("\\\\", "\\", $what_disp);
-   $what_disp = str_replace("\\\"", "\"", $what_disp);
-   $what_disp = str_replace("\"", "&quot;", $what_disp);
+       $what = '';
+   $what_disp = ereg_replace(',', ' ', $what);
+   $what_disp = str_replace('\\', '\', $what_disp);
+   $what_disp = str_replace('\"', '"', $what_disp);
+   $what_disp = str_replace('"', '&quot;', $what_disp);
    echo "          <INPUT TYPE=\"TEXT\" SIZE=\"20\" NAME=\"what\" VALUE=\"$what_disp\">\n";
-   echo "        </TD>";
+   echo '        </TD>';
    echo "       <TD ALIGN=\"RIGHT\" WIDTH=33%>\n";
-   echo "         <SELECT NAME=\"where\">";
+   echo '         <SELECT NAME="where">';
    
-   if (isset($where) && $where == "BODY") echo "           <OPTION VALUE=\"BODY\" SELECTED>"._("Body")."\n";
-   else echo "           <OPTION VALUE=\"BODY\">"._("Body")."\n";
+   if (isset($where) && $where == 'BODY') echo '           <OPTION VALUE="BODY" SELECTED>'._("Body")."\n";
+   else echo '           <OPTION VALUE="BODY">'._("Body")."\n";
    
-   if (isset($where) && $where == "TEXT") echo "           <OPTION VALUE=\"TEXT\" SELECTED>"._("Everywhere")."\n";
-   else echo "           <OPTION VALUE=\"TEXT\">"._("Everywhere")."\n";
+   if (isset($where) && $where == 'TEXT') echo '           <OPTION VALUE="TEXT" SELECTED>'._("Everywhere")."\n";
+   else echo '           <OPTION VALUE="TEXT">'._("Everywhere")."\n";
    
-   if (isset($where) && $where == "SUBJECT") echo "           <OPTION VALUE=\"SUBJECT\" SELECTED>"._("Subject")."\n";
-   else echo "           <OPTION VALUE=\"SUBJECT\">"._("Subject")."\n";
+   if (isset($where) && $where == 'SUBJECT') echo '           <OPTION VALUE="SUBJECT" SELECTED>'._("Subject")."\n";
+   else echo '           <OPTION VALUE="SUBJECT">'._("Subject")."\n";
    
-   if (isset($where) && $where == "FROM") echo "           <OPTION VALUE=\"FROM\" SELECTED>"._("From")."\n";
-   else echo "           <OPTION VALUE=\"FROM\">"._("From")."\n";
+   if (isset($where) && $where == 'FROM') echo '           <OPTION VALUE="FROM" SELECTED>'._("From")."\n";
+   else echo '           <OPTION VALUE="FROM">'._("From")."\n";
    
-   if (isset($where) && $where == "CC") echo "           <OPTION VALUE=\"Cc\" SELECTED>"._("Cc")."\n";
-   else echo "           <OPTION VALUE=\"CC\">"._("Cc")."\n";
+   if (isset($where) && $where == 'CC') echo '           <OPTION VALUE="Cc" SELECTED>'._("Cc")."\n";
+   else echo '           <OPTION VALUE="CC">'._("Cc")."\n";
    
-   if (isset($where) && $where == "TO") echo "           <OPTION VALUE=\"TO\" SELECTED>"._("To")."\n";
-   else echo "           <OPTION VALUE=\"TO\">"._("To")."\n";
+   if (isset($where) && $where == 'TO') echo '           <OPTION VALUE="TO" SELECTED>'._("To")."\n";
+   else echo '           <OPTION VALUE="TO">'._("To")."\n";
    
    echo "         </SELECT>\n";
    echo "        </TD>\n";
--- trunk/squirrelmail/src/signout.php
+++ trunk/squirrelmail/src/signout.php
@@ -14,20 +14,20 @@
    session_start();
 
    if (!isset($strings_php))
-      include("../functions/strings.php");
+      include('../functions/strings.php');
 
-   include ("../src/load_prefs.php");
+   include ('../src/load_prefs.php');
 
    if (!isset($config_php))
-      include("../config/config.php");
+      include('../config/config.php');
    if (!isset($i18n_php))
-      include("../functions/i18n.php");
+      include('../functions/i18n.php');
    if (!isset($prefs_php))
-      include ("../functions/prefs.php");
+      include ('../functions/prefs.php');
    if (!isset($plugin_php))
-      include ("../functions/plugin.php");
+      include ('../functions/plugin.php');
 
-   set_up_language(getPref($data_dir, $username, "language"));
+   set_up_language(getPref($data_dir, $username, 'language'));
 
    // If a user hits reload on the last page, $base_uri isn't set
    // because it was deleted with the session.
@@ -37,16 +37,16 @@
        $base_uri = $regs[1];
    }
 
-   do_hook("logout");
-   setcookie("username", "", 0, $base_uri);
-   setcookie("key", "", 0, $base_uri);
-   setcookie("logged_in", "", 0, $base_uri);
+   do_hook('logout');
+   setcookie('username', '', 0, $base_uri);
+   setcookie('key', '', 0, $base_uri);
+   setcookie('logged_in', '', 0, $base_uri);
    session_destroy();
 ?>
 <HTML>
    <HEAD>
 <?php
-   if ($theme_css != "") {
+   if ($theme_css != '') {
       printf ('<LINK REL="stylesheet" TYPE="text/css" HREF="%s">', 
                $theme_css);
       echo "\n";
@@ -54,31 +54,31 @@
    
    echo "<TITLE>$org_title - Signout</TITLE>\n";
    echo "</HEAD><BODY TEXT=$color[8] BGCOLOR=$color[4] LINK=$color[7] VLINK=$color[7] ALINK=$color[7]>\n";
-   echo "<BR><BR><TABLE BGCOLOR=FFFFFF BORDER=0 COLS=1 WIDTH=50% CELLSPACING=0 CELLPADDING=2 ALIGN=CENTER>";
+   echo '<BR><BR><TABLE BGCOLOR="FFFFFF" BORDER="0" COLS="1" WIDTH="50%" CELLSPACING="0" CELLPADDING="2" ALIGN="CENTER">';
    echo "   <TR BGCOLOR=$color[0] WIDTH=100%>";
-   echo "      <TD ALIGN=CENTER>";
-   echo "         <B>";
+   echo '      <TD ALIGN="CENTER">';
+   echo '         <B>';
    echo _("Sign Out");
-   echo "</B>";
-   echo "      </TD>";
-   echo "   </TR>";
+   echo '</B>';
+   echo '      </TD>';
+   echo '   </TR>';
    echo "   <TR BGCOLOR=$color[4] WIDTH=100%>";
-   echo "      <TD ALIGN=CENTER>";
-   echo "         <BR>";
+   echo '      <TD ALIGN=CENTER>';
+   echo '         <BR>';
    echo _("You have been successfully signed out.");
-   echo "<BR>";
-   echo "<A HREF=\"login.php\" TARGET=_top>";
+   echo '<BR>';
+   echo '<A HREF="login.php" TARGET=_top>';
    echo _("Click here to log back in.");
-   echo "</A><BR><BR>";
-   echo "      </TD>";
-   echo "   </TR>";
+   echo '</A><BR><BR>';
+   echo '      </TD>';
+   echo '   </TR>';
    echo "   <TR BGCOLOR=$color[0] WIDTH=100%>";
-   echo "      <TD ALIGN=CENTER>";
-	echo "			<br>";
-   echo "      </TD>";
-   echo "   </TR>";
-   echo "</TABLE>";
-	echo "<br><br>";
+   echo '      <TD ALIGN="CENTER">;
+	echo '			<br>';
+   echo '      </TD>';
+   echo '   </TR>';
+   echo '</TABLE>';
+	echo '<br><br>';
 ?>
 </BODY>
 </HTML>
