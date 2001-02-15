8890dd9396f87e027ae0279d4fa05be6fc6098c6
jbnance <jbnance@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-02-15 18:01:20 +0000

    Included no -- prefix support for signature.

--- trunk/squirrelmail/src/options.php
+++ trunk/squirrelmail/src/options.php
@@ -53,6 +53,7 @@
       if (isset($email_address)) setPref($data_dir, $username, 'email_address', $email_address);
       if (isset($reply_to)) setPref($data_dir, $username, 'reply_to', $reply_to);  
       setPref($data_dir, $username, 'use_signature', $usesignature);  
+      setPref($data_dir, $username, 'prefix_sig', $prefixsig);
       if (isset($signature_edit)) setSig($data_dir, $username, $signature_edit);
       
       do_hook('options_personal_save');
