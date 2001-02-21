510524155eea670a4ebb6d4d1395e380b2fa9067
fidian <fidian@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-02-21 00:19:47 +0000

    * My last color was a smidge too bright. * Hey, where's the collapseable folders option for 1.1??

--- trunk/squirrelmail/themes/black_bean_burrito_theme.php
+++ trunk/squirrelmail/themes/black_bean_burrito_theme.php
@@ -16,5 +16,5 @@
     $color[9]   = "#414141"; // (dark gray)			Darker version of #0
     $color[10]  = "#292929"; // (darker gray)		Darker version of #9
     $color[11]  = "#AAAA44"; // (light red)			Special Folders color
-    $color[12]  = "#303030"; // (dark gray)             Alternate color for message backround in index table
+    $color[12]  = "#202020"; // (dark gray)             Alternate color for message backround in index table
 ?>
