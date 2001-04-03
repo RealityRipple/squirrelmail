f69b827b9c1f4fc8c40c337ea310e4c26581a1e1
fidian <fidian@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-04-03 21:55:45 +0000

    * Consider it done.

--- trunk/squirrelmail/src/folders_create.php
+++ trunk/squirrelmail/src/folders_create.php
@@ -50,8 +50,7 @@
       $subfolder_orig = $subfolder;
    }
 
-   if (trim($subfolder_orig) == '[ ' . _('None') . ' ]' ||
-       trim($subfolder_orig) == '') {
+   if (trim($subfolder_orig) == '') {
       sqimap_mailbox_create ($imapConnection, $folder_prefix.$folder_name, "");
    } else {
       sqimap_mailbox_create ($imapConnection, $subfolder.$dm.$folder_name, "");
