5202797f766f34284d3ce03996948ec54febb40f
lkehresman <lkehresman@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-05-21 12:50:06 +0000

    added a grid of colors to message highlighting (patch from chris riley (anxious))

--- trunk/squirrelmail/src/options_highlight.php
+++ trunk/squirrelmail/src/options_highlight.php
@@ -30,7 +30,8 @@
       $identname = str_replace('\\"', '"', $identname);
       $identname = str_replace('"', '&quot;', $identname);
       if ($color_type == 1) $newcolor = $newcolor_choose;
-      else $newcolor = $newcolor_input;
+      elseif ($color_type == 2) $newcolor = $newcolor_input;
+      else $newcolor = $color_type;
  
       $newcolor = ereg_replace(',', '', $newcolor);
       $newcolor = ereg_replace('#', '', $newcolor);
@@ -49,13 +50,10 @@
    displayPageHeader($color, 'None');
 ?>
    <br>
-   <table width="95%" align="center" border="0" cellpadding="2" cellspacing="0">
-   <tr><td bgcolor="<?php echo $color[0] ?>" align="center">
+   <table width="95%" align="center" border="0" cellpadding="2" cellspacing="0"><tr><td bgcolor="<?php echo $color[0] ?>">
+      <center><b><?php echo _("Options") . ' - ' . _("Message Highlighting"); ?></b></center>
+   </td></tr></table>
 
-      <b><?php echo _("Options") . ' - ' . _("Message Highlighting"); ?></b>
-
-      <table width="100%" cellpadding="1" cellspacing="1" border="0">
-      <tr><td bgcolor="<?php echo $color[4] ?>" align="center">
 <?php
    echo '<br><center>[<a href="options_highlight.php?action=add">' . _("New") . '</a>]';
    echo ' - [<a href="options.php">'._("Done").'</a>]</center><br>'."\n";
@@ -77,7 +75,7 @@
       echo "</table>\n";
       echo "<br>\n";
    } else {
-      echo _("No highlighting is defined") . "<br>\n";
+      echo '<center>' . _("No highlighting is defined") . "</center><br>\n";
       echo "<br>\n";
    }
    if ($action == 'edit' || $action == 'add') {
@@ -102,6 +100,127 @@
       $color_list[12] = 'dfdfdf';
       $color_list[13] = 'ffffff';               
       
+      # helpful color chart from http://www.visibone.com/colorlab/big.html
+      $new_color_list["0,0"] = 'cccccc';
+      $new_color_list["0,1"] = '999999';
+      $new_color_list["0,2"] = '666666';
+      $new_color_list["0,3"] = '333333';
+      $new_color_list["0,4"] = '000000';
+      
+      # red
+      $new_color_list["1,0"] = 'ff0000';
+      $new_color_list["1,1"] = 'cc0000';
+      $new_color_list["1,2"] = '990000';
+      $new_color_list["1,3"] = '660000';
+      $new_color_list["1,4"] = '330000';
+      
+      $new_color_list["2,0"] = 'ffcccc';
+      $new_color_list["2,1"] = 'cc9999';
+      $new_color_list["2,2"] = '996666';
+      $new_color_list["2,3"] = '663333';
+      $new_color_list["2,4"] = '330000';
+      
+      $new_color_list["3,0"] = 'ffcccc';
+      $new_color_list["3,1"] = 'ff9999';
+      $new_color_list["3,2"] = 'ff6666';
+      $new_color_list["3,3"] = 'ff3333';
+      $new_color_list["3,4"] = 'ff0000';
+      
+      # green
+      $new_color_list["4,0"] = '00ff00';
+      $new_color_list["4,1"] = '00cc00';
+      $new_color_list["4,2"] = '009900';
+      $new_color_list["4,3"] = '006600';
+      $new_color_list["4,4"] = '003300';
+      
+      $new_color_list["5,0"] = 'ccffcc';
+      $new_color_list["5,1"] = '99cc99';
+      $new_color_list["5,2"] = '669966';
+      $new_color_list["5,3"] = '336633';
+      $new_color_list["5,4"] = '003300';
+      
+      $new_color_list["6,0"] = 'ccffcc';
+      $new_color_list["6,1"] = '99ff99';
+      $new_color_list["6,2"] = '66ff66';
+      $new_color_list["6,3"] = '33ff33';
+      $new_color_list["6,4"] = '00ff00';
+      
+      # blue
+      $new_color_list["7,0"] = '0000ff';
+      $new_color_list["7,1"] = '0000cc';
+      $new_color_list["7,2"] = '000099';
+      $new_color_list["7,3"] = '000066';
+      $new_color_list["7,4"] = '000033';
+      
+      $new_color_list["8,0"] = 'ccccff';
+      $new_color_list["8,1"] = '9999cc';
+      $new_color_list["8,2"] = '666699';
+      $new_color_list["8,3"] = '333366';
+      $new_color_list["8,4"] = '000033';
+      
+      $new_color_list["9,0"] = 'ccccff';
+      $new_color_list["9,1"] = '9999ff';
+      $new_color_list["9,2"] = '6666ff';
+      $new_color_list["9,3"] = '3333ff';
+      $new_color_list["9,4"] = '0000ff';
+      
+      # yellow
+      $new_color_list["10,0"] = 'ffff00';
+      $new_color_list["10,1"] = 'cccc00';
+      $new_color_list["10,2"] = '999900';
+      $new_color_list["10,3"] = '666600';
+      $new_color_list["10,4"] = '333300';
+                                     
+      $new_color_list["11,0"] = 'ffffcc';
+      $new_color_list["11,1"] = 'cccc99';
+      $new_color_list["11,2"] = '999966';
+      $new_color_list["11,3"] = '666633';
+      $new_color_list["11,4"] = '333300';
+                                     
+      $new_color_list["12,0"] = 'ffffcc';
+      $new_color_list["12,1"] = 'ffff99';
+      $new_color_list["12,2"] = 'ffff66';
+      $new_color_list["12,3"] = 'ffff33';
+      $new_color_list["12,4"] = 'ffff00';
+      
+      # cyan
+      $new_color_list["13,0"] = '00ffff';
+      $new_color_list["13,1"] = '00cccc';
+      $new_color_list["13,2"] = '009999';
+      $new_color_list["13,3"] = '006666';
+      $new_color_list["13,4"] = '003333';
+                                       
+      $new_color_list["14,0"] = 'ccffff';
+      $new_color_list["14,1"] = '99cccc';
+      $new_color_list["14,2"] = '669999';
+      $new_color_list["14,3"] = '336666';
+      $new_color_list["14,4"] = '003333';
+                                       
+      $new_color_list["15,0"] = 'ccffff';
+      $new_color_list["15,1"] = '99ffff';
+      $new_color_list["15,2"] = '66ffff';
+      $new_color_list["15,3"] = '33ffff';
+      $new_color_list["15,4"] = '00ffff';
+      
+      # magenta
+      $new_color_list["16,0"] = 'ff00ff';
+      $new_color_list["16,1"] = 'cc00cc';
+      $new_color_list["16,2"] = '990099';
+      $new_color_list["16,3"] = '660066';
+      $new_color_list["16,4"] = '330033';
+                                   
+      $new_color_list["17,0"] = 'ffccff';
+      $new_color_list["17,1"] = 'cc99cc';
+      $new_color_list["17,2"] = '996699';
+      $new_color_list["17,3"] = '663366';
+      $new_color_list["17,4"] = '330033';
+                                   
+      $new_color_list["18,0"] = 'ffccff';
+      $new_color_list["18,1"] = 'ff99ff';
+      $new_color_list["18,2"] = 'ff66ff';
+      $new_color_list["18,3"] = 'ff33ff';
+      $new_color_list["18,4"] = 'ff00ff';
+
       $selected_input = '';
       $selected_choose = '';
       
@@ -115,7 +234,7 @@
                ${"selected".$i} = ' selected';
                continue;
             }
-	     }
+	 }
       }
       if (!isset($message_highlight_list[$theid]['color']))
          $selected_choose = ' checked';
@@ -164,11 +283,29 @@
       echo "            <option value=\"$color_list[13]\"$selected13>" . _("White") . "\n";
       echo "         </select><br>\n";
       echo "         <input type=\"radio\" name=color_type value=2$selected_input> &nbsp;". _("Other:") ."<input type=\"text\" value=\"";
-      if ($selected_input)
-        echo $message_highlight_list[$theid]["color"];
+      if ($selected_input) echo $message_highlight_list[$theid]["color"];
       echo '" name="newcolor_input" size="7"> '._("Ex: 63aa7f")."<br>\n";
       echo "      </td>\n";
       echo "   </tr>\n";
+
+      # Show grid of color choices
+      echo "<tr bgcolor=$color[0]><td colspan='2'>\n";
+      echo "<table border=0 cellpadding=\"2\" align=\"center\" cellspacing=\"1\">\n";
+      $current_color = $message_highlight_list[$theid]['color'];
+      for($x = 0; $x < 5; $x++) {
+        echo "<tr>\n";
+        for($y = 0; $y < 19; $y++) {
+          $gridindex = "$y,$x";
+          $gridcolor = $new_color_list[$gridindex];
+	  $selected = ($gridcolor == $current_color)?'SELECTED':'' ;
+          echo "<td bgcolor=#$gridcolor><input type='radio' name=color_type value='#$gridcolor' $selected>\n";
+          echo "</td>\n";
+        }
+        echo "</tr>\n";
+      }
+      echo "</table>\n";
+      echo "</tr></td>\n";
+
       echo '   <tr><td><small><small>&nbsp;</small></small></td></tr>' . "\n";
       echo "   <tr bgcolor=\"$color[0]\">\n";
       echo '      <td align="right" width="25%"><b>' . "\n";
@@ -176,26 +313,16 @@
       echo "      </b></td>\n";
       echo '      <td width="75%">' . "\n";
       echo "         <select name=match_type>\n";
-      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'from')
-        echo "            <option value=\"from\" selected>From\n";
-      else
-        echo "            <option value=\"from\">From\n";
-      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'to')
-        echo "            <option value=\"to\" selected>To\n";
-      else
-        echo "            <option value=\"to\">To\n";
-      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'cc')
-        echo "            <option value=\"cc\" selected>Cc\n";
-      else
-        echo "            <option value=\"cc\">Cc\n";
-      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'to_cc')
-        echo "            <option value=\"to_cc\" selected>To or Cc\n";
-      else
-        echo "            <option value=\"to_cc\">To or Cc\n";
-      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'subject')
-        echo "            <option value=\"subject\" selected>Subject\n";
-      else
-        echo "            <option value=\"subject\">Subject\n";
+      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'from')    echo "            <option value=\"from\" selected>From\n";
+      else                                                         echo "            <option value=\"from\">From\n";
+      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'to')      echo "            <option value=\"to\" selected>To\n";
+      else                                                         echo "            <option value=\"to\">To\n";
+      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'cc')      echo "            <option value=\"cc\" selected>Cc\n";
+      else                                                         echo "            <option value=\"cc\">Cc\n";
+      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'to_cc')   echo "            <option value=\"to_cc\" selected>To or Cc\n";
+      else                                                         echo "            <option value=\"to_cc\">To or Cc\n";
+      if (isset($message_highlight_list[$theid]['match_type']) && $message_highlight_list[$theid]['match_type'] == 'subject') echo "            <option value=\"subject\" selected>Subject\n";
+      else                                                         echo "            <option value=\"subject\">Subject\n";
       echo "         </select>\n";
       if (isset($message_highlight_list[$theid]['value']))
           $disp = $message_highlight_list[$theid]['value'];
@@ -210,14 +337,7 @@
       echo "</table>\n";
       echo '<center><input type="submit" value="' . _("Submit") . "\"></center>\n";
       echo "</form>\n";
+      do_hook('options_highlight_bottom');
    } 
 ?>
-
-   <?php do_hook('options_highlight_bottom'); ?>
-
-    </td></tr>
-    </table>
-
-</td></tr>
-</table>
 </body></html>
