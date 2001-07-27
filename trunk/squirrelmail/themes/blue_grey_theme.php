3fe474619d1e80d8c031bf09694096e266bf04ac
fidian <fidian@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-07-27 21:08:05 +0000

    Added Blue Gray theme Come on people!  If you see it on the list, add it!  Don't just sit there and hope it magically gets added to the source.

---
+++ trunk/squirrelmail/themes/blue_grey_theme.php
@@ -0,0 +1,24 @@
+<?php
+   /** Author:       M.J. Prinsen
+       Date:         July 20, 2001
+       Theme Name:   "Blue-grey"
+
+       Deep Ocean is a theme that is very blue with a dark grey background.
+    **/
+
+    global $color;
+    $color[0]   = "#6188A9"; // (middle blue)    TitleBar
+    $color[1]   = "#800000"; // (red)
+    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+    $color[3]   = "#294763"; // (dark blue)      Left Bar Background
+    $color[4]   = "#929292"; // (dark grey)      Normal Background or C0C0C0
+    $color[5]   = "#597D9D"; // (middle blue)	 Table Headers
+    $color[6]   = "#FFFFFF"; // (white)          Text on left bar
+    $color[7]   = "#FFFFFF"; // (white)          Links
+    $color[8]   = "#FFFFFF"; // (white)          Normal text
+    $color[9]   = "#587B99"; // (middle gray)    Darker version of #0
+    $color[10]  = "#496E8B"; // (dark gray)      Darker version of #9
+    $color[11]  = "#A7C5F3"; // (light blue)     Special Folders color
+    $color[12]  = "#7092B4";
+?>
+)
