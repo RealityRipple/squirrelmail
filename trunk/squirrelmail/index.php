8198faf860daa7ac0ba635c38fe2a745d0eb140b
philippe_mingo <philippe_mingo@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-09-26 08:29:27 +0000

    Operation "foo_once". Step 1: Check file including and change it to the _once system.

--- trunk/squirrelmail/index.php
+++ trunk/squirrelmail/index.php
@@ -1,5 +1,17 @@
 <?php
-   include "./functions/strings.php";
+
+   /**
+    **  index.php -- Displays the main frameset
+    **
+    **  Copyright (c) 1999-2000 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Redirects to the login page.
+    **
+    **  $Id$
+    **/
+
+   require_once('./functions/strings.php');
 
    $location = get_location();
    header("Location: $location/src/login.php\n\n");
