89ef55ec818717dc0e820acb97a4171e126480c5
philippe_mingo <philippe@squirrelmail.org>
2001-10-29 13:00:45 +0000

    Added submit and title functions.

--- functions/options.php
+++ functions/options.php
@@ -12,7 +12,7 @@
 
     function OptionSelect( $title, $name, $data, $default, $show = '', $store = '' ) {
 
-        echo "<tr><td align=right valign=middle nowrap>$title</td><td>" .
+        echo "<tr><td align=right valign=middle nowrap>$title: </td><td>" .
              "<select name=\"$name\">";
         foreach( $data as $key => $opt ) {
             if ( $store == '' ) {
@@ -39,7 +39,7 @@
 
     function OptionRadio( $title, $name, $data, $default, $show = '', $store = '', $sep = '&nbsp; &nbsp;'  ) {
 
-        echo "<tr><td align=right valign=middle nowrap>$title</td><td>";
+        echo "<tr><td align=right valign=middle nowrap>$title: </td><td>";
         foreach( $data as $key => $opt ) {
             if ( $store == '' ) {
                 $vl = $key;
@@ -65,7 +65,7 @@
 
     function OptionText( $title, $name, $value, $size ) {
 
-        echo "<tr><td align=right valign=middle nowrap>$title</td><td>" .
+        echo "<tr><td align=right valign=middle nowrap>$title: </td><td>" .
              "<input name=\"$name\" value=\"$value\" size=\"$size\">" .
              "</td></tr>\n";
 
@@ -75,9 +75,22 @@
 
         if ( $value )
             $chk = 'checked';
-        echo "<tr><td align=right valign=middle nowrap>$title</td><td>" .
+        echo "<tr><td align=right valign=middle nowrap>$title: </td><td>" .
              "<input type=\"checkbox\" name=\"$name\" $chk> $comment" .
              "</td></tr>\n";
+
+    }
+
+    function OptionTitle( $title ) {
+
+        echo "<tr><td colspan=2 align=left valign=middle nowrap><b>$title</b></td></tr>\n";
+
+    }
+
+    function OptionSubmit() {
+
+        echo '<tr><td>&nbsp;</td><td><input type="submit" value="' . _("Submit") . '" name="submit_folder">' .
+             '</td></tr>';
 
     }
 
