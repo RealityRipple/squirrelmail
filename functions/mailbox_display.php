e9e90b180f98c3ca199c74922e440b3259981433
teepe <wouter@teepe.com>
2001-11-02 15:46:20 +0000

    changed displayal of folders to move to from unformatted-disp to formatted

--- functions/mailbox_display.php
+++ functions/mailbox_display.php
@@ -544,7 +544,7 @@
       for ($i = 0; $i < count($boxes); $i++) {
          if (!in_array("noselect", $boxes[$i]['flags'])) {
             $box = $boxes[$i]['unformatted'];
-            $box2 = str_replace(' ', '&nbsp;', $boxes[$i]['unformatted-disp']);
+            $box2 = str_replace(' ', '&nbsp;', $boxes[$i]['formatted']);
             echo "         <OPTION VALUE=\"$box\">$box2</option>\n";
          }
       }
