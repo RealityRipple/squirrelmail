d4713804b357c27b0ef51346d65b8c23a88fb0b1
philippe_mingo <philippe@squirrelmail.org>
2001-11-08 15:33:15 +0000

    ever more

--- plugins/squirrelspell/doc/index.php
+++ plugins/squirrelspell/doc/index.php
@@ -1,3 +1,20 @@
 <?php
-header("Location: http://www.mricon.com/");
+
+   /**
+    **  index.php -- Displays the main frameset
+    **
+    **  Copyright (c) 1999-2000 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Redirects to the login page.
+    **
+    **  $Id$
+    **/
+
+   require_once('../../../functions/strings.php');
+
+   $location = get_location();
+   header("Location: $location/src/login.php\n\n");
+   exit();
+
 ?>
