c7d2f91715fbdff706ec20e7b2eabd586e482ffa
philippe_mingo <philippe_mingo@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-11-08 15:31:05 +0000

    Sec. Stuff

---
+++ trunk/squirrelmail/plugins/filters/index.php
@@ -0,0 +1,20 @@
+<?php
+
+   /**
+    **  index.php -- Displays the main frameset
+    **
+    **  Copyright (c) 1999-2001 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Redirects to the login page.
+    **
+    **  $Id$
+    **/
+
+   require_once('../../functions/strings.php');
+
+   $location = get_location();
+   header("Location: $location/src/login.php\n\n");
+   exit();
+
+?>
