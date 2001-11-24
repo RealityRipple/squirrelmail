95f9c531cd466df5c71e4f91647e73bbab2185e4
philippe_mingo <philippe@squirrelmail.org>
2001-11-24 09:04:05 +0000

    mail_fetch adaptation

--- plugins/mail_fetch/setup.php
+++ plugins/mail_fetch/setup.php
@@ -1,16 +1,26 @@
 <?php
 
+   /**
+    **  mail_fetch/setup.php
+    **
+    **  Copyright (c) 1999-2001 The Squirrelmail Development Team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Setup of the mailfetch plugin.
+    **
+    **  $Id$
+    **/
+
     require_once( '../plugins/mail_fetch/functions.php' );
 
     function squirrelmail_plugin_init_mail_fetch() {
         global $squirrelmail_plugin_hooks;
         global $mailbox, $imap_stream, $imapConnection;
 
-        $squirrelmail_plugin_hooks["menuline"]["mail_fetch"] = 'mail_fetch_link';
-        $squirrelmail_plugin_hooks["options_save"]["mail_fetch"] = "mail_fetch_save_pref";
-        $squirrelmail_plugin_hooks["loading_prefs"]["mail_fetch"] = "mail_fetch_load_pref";
-        $squirrelmail_plugin_hooks["login_verified"]["mail_fetch"] = "mail_fetch_setnew";
-        $squirrelmail_plugin_hooks["left_main_before"]["mail_fetch"] = "mail_fetch_login";
+        $squirrelmail_plugin_hooks['menuline']['mail_fetch'] = 'mail_fetch_link';
+        $squirrelmail_plugin_hooks['loading_prefs']['mail_fetch'] = 'mail_fetch_load_pref';
+        $squirrelmail_plugin_hooks['login_verified']['mail_fetch'] = 'mail_fetch_setnew';
+        $squirrelmail_plugin_hooks['left_main_before']['mail_fetch'] = 'mail_fetch_login';
         $squirrelmail_plugin_hooks['optpage_register_block']['mail_fetch'] = 'mailfetch_optpage_register_block';
 
     }
@@ -31,10 +41,9 @@
         global $mailfetch_lmos_, $mailfetch_uidl_, $mailfetch_login_, $mailfetch_fref_;
         global $PHP_SELF;
 
-        if( stristr( $PHP_SELF, 'mail_fetch'    ) ) {
-            $mailfetch_server_number = getPref($data_dir, $username,    "mailfetch_server_number");
-            if (!isset($mailfetch_server_number)) $mailfetch_server_number=0;
-            $mailfetch_cypher = getPref($data_dir, $username, "mailfetch_cypher");
+        if( stristr( $PHP_SELF, 'mail_fetch' ) ) {
+            $mailfetch_server_number = getPref($data_dir, $username, 'mailfetch_server_number', 0);
+            $mailfetch_cypher = getPref($data_dir, $username, 'mailfetch_cypher', 'on' );
             if ($mailfetch_server_number<1) $mailfetch_server_number=0;
             for ($i=0;$i<$mailfetch_server_number;$i++) {
                 $mailfetch_server_[$i] = getPref($data_dir, $username, "mailfetch_server_$i");
@@ -46,71 +55,6 @@
                 $mailfetch_fref_[$i]   = getPref($data_dir, $username, "mailfetch_fref_$i");
                 $mailfetch_uidl_[$i]   = getPref($data_dir, $username, "mailfetch_uidl_$i");
                 if( $mailfetch_cypher   == 'on' ) $mailfetch_pass_[$i] =    decrypt( $mailfetch_pass_[$i] );
-            }
-        }
-    }
-
-    function mail_fetch_save_pref() {
-        global $username,$data_dir;
-        global $mf_server, $mf_user, $mf_pass, $mf_lmos, $mf_alias;
-        global $mf_cypher;
-        global $mf_uidl;
-        global $mf_login, $mf_fref, $submit_mailfetch;
-        global $mf_action, $mf_sn, $mf_subfolder;
-
-        if (isset($submit_mailfetch))   {
-            if ($mf_action=="add") {
-                //add new server
-                if ($mf_sn<1) $mf_sn=0;
-                if (!isset($mf_server)) return;
-                setPref($data_dir,$username,"mailfetch_server_$mf_sn", (isset($mf_server)?$mf_server:""));
-                setPref($data_dir,$username,"mailfetch_alias_$mf_sn", (isset($mf_alias)?$mf_alias:""));
-                setPref($data_dir,$username,"mailfetch_user_$mf_sn",(isset($mf_user)?$mf_user:""));
-                setPref($data_dir,$username,"mailfetch_pass_$mf_sn",(isset($mf_pass)?encrypt( $mf_pass )    :""));
-                if( $mf_cypher <> 'on' ) SetPref($data_dir,$username,"mailfetch_cypher",    'on');
-                setPref($data_dir,$username,"mailfetch_lmos_$mf_sn",(isset($mf_lmos)?$mf_lmos:""));
-                setPref($data_dir,$username,"mailfetch_login_$mf_sn",(isset($mf_login)?$mf_login:""));
-                setPref($data_dir,$username,"mailfetch_fref_$mf_sn",(isset($mf_fref)?$mf_fref:""));
-                setPref($data_dir,$username,"mailfetch_subfolder_$mf_sn",(isset($mf_subfolder)?$mf_subfolder:""));
-                $mf_sn++;
-                setPref($data_dir,$username,"mailfetch_server_number", $mf_sn);
-            } elseif ($mf_action=="confirm_modify") {
-                //modify    a server
-                if (!isset($mf_server)) return;
-                setPref($data_dir,$username,"mailfetch_server_$mf_sn", (isset($mf_server)?$mf_server:""));
-                setPref($data_dir,$username,"mailfetch_alias_$mf_sn", (isset($mf_alias)?$mf_alias:""));
-                setPref($data_dir,$username,"mailfetch_user_$mf_sn",(isset($mf_user)?$mf_user:""));
-                setPref($data_dir,$username,"mailfetch_pass_$mf_sn",(isset($mf_pass)?encrypt( $mf_pass )    :""));
-                if( $mf_cypher <> 'on' ) setPref($data_dir,$username,"mailfetch_cypher", 'on');
-                setPref($data_dir,$username,"mailfetch_lmos_$mf_sn",(isset($mf_lmos)?$mf_lmos:""));
-                setPref($data_dir,$username,"mailfetch_login_$mf_sn",(isset($mf_login)?$mf_login:""));
-                setPref($data_dir,$username,"mailfetch_fref_$mf_sn",(isset($mf_fref)?$mf_fref:""));
-                setPref($data_dir,$username,"mailfetch_subfolder_$mf_sn",(isset($mf_subfolder)?$mf_subfolder:""));
-            } elseif ($mf_action=="confirm_delete") {
-                //delete    a server
-                $mailfetch_server_number    = getPref($data_dir, $username, "mailfetch_server_number");
-                if ($mf_sn+1==$mailfetch_server_number) {
-                    //is the last server, whe can only decrase $mailfetch_server_number
-                    $mailfetch_server_number--;
-                    setPref($data_dir,$username,"mailfetch_server_number", $mailfetch_server_number);
-                } else {
-                    //if not the last, all the sequel server come up one step
-                    //then whe decrase $mailfetch_server_number
-                    $mailfetch_server_number--;
-                    for ($i=$mf_sn;$i<$mailfetch_server_number;$i++) {
-                        $tmp=$i+1;
-                        setPref($data_dir,$username,"mailfetch_server_$mf_sn", getPref($data_dir, $username, "mailfetch_server_$tmp"));
-                        setPref($data_dir,$username,"mailfetch_alias_$mf_sn", getPref($data_dir, $username, "mailfetch_alias_$tmp"));
-                        setPref($data_dir,$username,"mailfetch_user_$mf_sn", getPref($data_dir, $username, "mailfetch_user_$tmp"));
-                        setPref($data_dir,$username,"mailfetch_pass_$mf_sn",(isset($mf_pass)?encrypt( $mf_pass ) :""));
-                        // if( $mf_cypher <> 'on' ) setPref($data_dir,$username,"mailfetch_cypher", 'on');
-                        setPref($data_dir,$username,"mailfetch_lmos_$mf_sn", getPref($data_dir, $username, "mailfetch_lmos_$tmp"));
-                        setPref($data_dir,$username,"mailfetch_login_$mf_sn", getPref($data_dir, $username, "mailfetch_login_$tmp"));
-                        setPref($data_dir,$username,"mailfetch_fref_$mf_sn", getPref($data_dir, $username, "mailfetch_fref_$tmp"));
-                        setPref($data_dir,$username,"mailfetch_subfolder_$mf_sn", getPref($data_dir, $username, "mailfetch_subfolder_$tmp"));
-                    }
-                    setPref($data_dir,$username,"mailfetch_server_number", $mailfetch_server_number);
-                }
             }
         }
     }
@@ -129,9 +73,9 @@
 
         $outMsg = '';
 
-        $mailfetch_server_number = getPref($data_dir, $username, "mailfetch_server_number");
+        $mailfetch_server_number = getPref($data_dir, $username, 'mailfetch_server_number');
         if (!isset($mailfetch_server_number)) $mailfetch_server_number=0;
-        $mailfetch_cypher = getPref($data_dir, $username, "mailfetch_cypher");
+        $mailfetch_cypher = getPref($data_dir, $username, 'mailfetch_cypher');
         if ($mailfetch_server_number<1) $mailfetch_server_number=0;
 
         for ($i_loop=0;$i_loop<$mailfetch_server_number;$i_loop++) {
