49e32f9f0e293f224e709cf5328e0da8129f2218
philippe_mingo <philippe_mingo@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-12-12 08:53:36 +0000

    Sintax fix

--- trunk/squirrelmail/functions/display_messages.php
+++ trunk/squirrelmail/functions/display_messages.php
@@ -12,98 +12,84 @@
  * $Id$
  */
 
-/*****************************************************************/
-/*** THIS FILE NEEDS TO HAVE ITS FORMATTING FIXED!!!           ***/
-/*** PLEASE DO SO AND REMOVE THIS COMMENT SECTION.             ***/
-/***    + Base level indent should begin at left margin, as    ***/
-/***      the first line of the function definition below.     ***/
-/***    + All identation should consist of four space blocks   ***/
-/***    + Tab characters are evil.                             ***/
-/***    + all comments should use "slash-star ... star-slash"  ***/
-/***      style -- no pound characters, no slash-slash style   ***/
-/***    + FLOW CONTROL STATEMENTS (if, while, etc) SHOULD      ***/
-/***      ALWAYS USE { AND } CHARACTERS!!!                     ***/
-/***    + Please use ' instead of ", when possible. Note "     ***/
-/***      should always be used in _( ) function calls.        ***/
-/*** Thank you for your help making the SM code more readable. ***/
-/*****************************************************************/
+function error_username_password_incorrect() {
 
-function error_username_password_incorrect($color) {
     echo '<BR>'.
-                "<TABLE COLS=1 WIDTH=75% NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>".
-                '<TR>'.
-                    "<TD BGCOLOR=\"$color[0]\">".
-                        '<B><CENTER>ERROR</CENTER></B>'.
-                    '</TD></TR><TR><TD>'.
-                        '<CENTER><BR>' . _("Unknown user or password incorrect.") .
-                        '<BR><A HREF="login.php" TARGET=_top>' .
-                        _("Click here to try again") .
-                        '</A>.</CENTER>'.
-                    '</TD></TR>'.
+                '<TABLE COLS=1 WIDTH=75% NOBORDER BGCOLOR="#FFFFFF" ALIGN=CENTER>'.
+                '<TR BGCOLOR="#DCDCDC">'.
+                    '<TH>' . _("ERROR") . '</TH>'.
+                '</TR>' .
+                '<TR><TD>'.
+                    '<CENTER><BR>' . _("Unknown user or password incorrect.") .
+                    '<BR><A HREF="login.php" TARGET=_top>' .
+                    _("Click here to try again") .
+                    '</A>.</CENTER>'.
+                '</TD></TR>'.
                 '</TABLE>'.
             '</BODY></HTML>';
+}
 
+function general_info($motd, $org_logo, $version, $org_name, $color) {
+    echo '<BR>';
+    echo "<TABLE COLS=1 WIDTH=80% CELLSPACING=0 CELLPADDING=2 NOBORDER ALIGN=CENTER><TR><TD BGCOLOR=\"$color[9]\">";
+    echo '<TABLE COLS=1 WIDTH=100% CELLSPACING=0 CELLPADDING=3 NOBORDER BGCOLOR="#FFFFFF" ALIGN=CENTER>';
+    echo '   <TR>';
+    echo "      <TD BGCOLOR=\"$color[0]\">";
+    echo '         <B><CENTER>';
+    printf (_("Welcome to %s's WebMail system"), $org_name);
+    echo '         </CENTER></B>';
+    echo '   <TR><TD BGCOLOR="#FFFFFF">';
+    echo '      <TABLE COLS=2 WIDTH=90% CELLSPACING=0 CELLPADDING=3 NOBORDER align="center">';
+    echo '         <TR>';
+    echo '            <TD BGCOLOR="#FFFFFF"><CENTER>';
+    if ( strlen($org_logo) > 3 ) {
+        echo "               <IMG SRC=\"$org_logo\">";
+    } else {
+        echo "               <B>$org_name</B>";
     }
+    echo '         <BR><CENTER>';
+    printf (_("Running SquirrelMail version %s (c) 1999-2001."), $version);
+    echo '            </CENTER><BR>';
+    echo '            </CENTER></TD></TR><TR>';
+    echo '            <TD BGCOLOR="#FFFFFF">';
+    echo "               $motd";
+    echo '            </TD>';
+    echo '         </TR>';
+    echo '      </TABLE>';
+    echo '   </TD></TR>';
+    echo '</TABLE>';
+    echo '</TD></TR></TABLE>';
+}
 
-    function general_info($motd, $org_logo, $version, $org_name, $color) {
-      echo '<BR>';
-      echo "<TABLE COLS=1 WIDTH=80% CELLSPACING=0 CELLPADDING=2 NOBORDER ALIGN=CENTER><TR><TD BGCOLOR=\"$color[9]\">";
-      echo '<TABLE COLS=1 WIDTH=100% CELLSPACING=0 CELLPADDING=3 NOBORDER BGCOLOR="#FFFFFF" ALIGN=CENTER>';
-      echo '   <TR>';
-      echo "      <TD BGCOLOR=\"$color[0]\">";
-      echo '         <B><CENTER>';
-      printf (_("Welcome to %s's WebMail system"), $org_name);
-      echo '         </CENTER></B>';
-      echo '   <TR><TD BGCOLOR="#FFFFFF">';
-      echo '      <TABLE COLS=2 WIDTH=90% CELLSPACING=0 CELLPADDING=3 NOBORDER align="center">';
-      echo '         <TR>';
-      echo '            <TD BGCOLOR="#FFFFFF"><CENTER>';
-      if (strlen($org_logo) > 3)
-         echo "               <IMG SRC=\"$org_logo\">";
-      else
-         echo "               <B>$org_name</B>";
-      echo '         <BR><CENTER>';
-      printf (_("Running SquirrelMail version %s (c) 1999-2000."), $version);
-      echo '            </CENTER><BR>';
-      echo '            </CENTER></TD></TR><TR>';
-      echo '            <TD BGCOLOR="#FFFFFF">';
-      echo "               $motd";
-      echo '            </TD>';
-      echo '         </TR>';
-      echo '      </TABLE>';
-      echo '   </TD></TR>';
-      echo '</TABLE>';
-      echo '</TD></TR></TABLE>';
-   }
+function error_message($message, $mailbox, $sort, $startMessage, $color) {
+    $urlMailbox = urlencode($mailbox);
+    
+    echo '<BR>';
+    echo "<TABLE COLS=1 WIDTH=70% NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>";
+    echo '   <TR>';
+    echo "      <TD BGCOLOR=\"$color[0]\">";
+    echo "         <FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>';
+    echo '   </TD></TR><TR><TD>';
+    echo "      <CENTER><BR>$message<BR>\n";
+    echo '      <BR>';
+    echo "         <A HREF=\"right_main.php?sort=$sort&startMessage=$startMessage&mailbox=$urlMailbox\">";
+    printf (_("Click here to return to %s"), $mailbox);
+    echo '</A>.';
+    echo '   </TD></TR>';
+    echo '</TABLE>';
+}
 
-    function error_message($message, $mailbox, $sort, $startMessage, $color) {
-      $urlMailbox = urlencode($mailbox);
-
-      echo '<BR>';
-      echo "<TABLE COLS=1 WIDTH=70% NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>";
-      echo '   <TR>';
-      echo "      <TD BGCOLOR=\"$color[0]\">";
-      echo "         <FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>';
-      echo '   </TD></TR><TR><TD>';
-      echo "      <CENTER><BR>$message<BR>\n";
-      echo '      <BR>';
-      echo "         <A HREF=\"right_main.php?sort=$sort&startMessage=$startMessage&mailbox=$urlMailbox\">";
-      printf (_("Click here to return to %s"), $mailbox);
-      echo '</A>.';
-      echo '   </TD></TR>';
-      echo '</TABLE>';
-    }
-
-    function plain_error_message($message, $color) {
-      echo '<BR>';
-      echo "<TABLE COLS=1 WIDTH=70% NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>";
-      echo '   <TR>';
-      echo "      <TD BGCOLOR=\"$color[0]\">";
-      echo "         <FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>';
-      echo '   </TD></TR><TR><TD>';
-      echo "      <CENTER><BR>$message";
-      echo '      </CENTER>';
-      echo '   </TD></TR>';
-      echo '</TABLE>';
-    }
+function plain_error_message($message, $color) {
+    echo '<BR>';
+    echo "<TABLE COLS=1 WIDTH=70% NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>";
+    echo '   <TR>';
+    echo "      <TD BGCOLOR=\"$color[0]\">";
+    echo "         <FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>';
+    echo '   </TD></TR><TR><TD>';
+    echo "      <CENTER><BR>$message";
+    echo '      </CENTER>';
+    echo '   </TD></TR>';
+    echo '</TABLE>';
+}
+    
 ?>
