cb91a061ec64bd2032733d271f47587f17e46001
thomppj <thomppj@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-12-23 07:43:22 +0000

    Added Christmas theme.

---
+++ trunk/squirrelmail/themes/christmas.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * christmas.php
+ *    Name:   Christmas
+ *    Author: Jon Tai
+ *    Date:   December 23, 2001
+ *    Comment: This is a Christmas theme shipped with SquirrelMail 1.2.0.
+ *
+ * Copyright (c) 2001-2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+global $color;
+$color[0]   = "#5C9F5C"; // TitleBar
+$color[1]   = "#AF0000"; // Unseen messages color (unused?)
+$color[2]   = "#CC0000"; // Warning/Error Messages
+$color[3]   = "#308030"; // Left Bar Background
+$color[4]   = "#FFFFFF"; // Normal Background
+$color[5]   = "#88BA88"; // Table Headers
+$color[6]   = "#DEDEDE"; // Text on left bar
+$color[7]   = "#EE0000"; // Links
+$color[8]   = "#000000"; // Normal text
+$color[9]   = "#308F30"; // Darker version of #0
+$color[10]  = "#88AA88"; // Dimmed out folder color
+$color[11]  = "#EE0000"; // Special folders / Unseen messages color
+$color[12]  = "#DDFFDD"; // Alternate table row color
+
+?>
