520f95c669923363b9895eb0b30ccb0c85d26d9c
teepe <teepe@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-12-25 02:16:52 +0000

    theme style police

--- trunk/squirrelmail/themes/slashdot_theme.php
+++ trunk/squirrelmail/themes/slashdot_theme.php
@@ -1,24 +1,32 @@
 <?php
-   /** Author:       Luke Ehresman
-       Date:         January 3, 2000
-       Theme Name:   "Slashdot Theme"
 
-       This theme tries to mimic the color scheme from the "News
-       for Nerds" web site, www.slashdot.org.
-    **/
+/**
+ * slashdot_theme.php
+ *    Name:    Slashdot Theme
+ *    Author:  Luke Ehresman
+ *    Date:    January 3, 2000
+ *    Comment: This theme tries to mimic the color scheme from the
+ *             "News for Nerds" web site, www.slashdot.org.
+ *
+ * Copyright (c) 2000-2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
 
-    global $color;
-    $color[0]   = "#DCDCDC"; // (light gray)     TitleBar
-    $color[1]   = "#800000"; // (red)
-    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
-    $color[3]   = "#006668"; // (/. color)       Left Bar Background
-    $color[4]   = "#FFFFFF"; // (white)          Normal Background
-    $color[5]   = "#BABABA"; // (darker gray)    Table Headers
-    $color[6]   = "#000000"; // (black)          Text on left bar
-    $color[7]   = "#004949"; // (light /. color) Links
-    $color[8]   = "#000000"; // (black)          Normal text
-    $color[9]   = "#ABABAB"; // (mid-gray)       Darker version of #0
-    $color[10]  = "#666666"; // (dark gray)      Darker version of #9
-    $color[11]  = "#770000"; // (dark red)       Special Folders color
-    $color[12]  = "#EAEAEA";
+global $color;
+$color[0]   = "#DCDCDC"; // (light gray)     TitleBar
+$color[1]   = "#800000"; // (red)
+$color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+$color[3]   = "#006668"; // (/. color)       Left Bar Background
+$color[4]   = "#FFFFFF"; // (white)          Normal Background
+$color[5]   = "#BABABA"; // (darker gray)    Table Headers
+$color[6]   = "#000000"; // (black)          Text on left bar
+$color[7]   = "#004949"; // (light /. color) Links
+$color[8]   = "#000000"; // (black)          Normal text
+$color[9]   = "#ABABAB"; // (mid-gray)       Darker version of #0
+$color[10]  = "#666666"; // (dark gray)      Darker version of #9
+$color[11]  = "#770000"; // (dark red)       Special Folders color
+$color[12]  = "#EAEAEA";
+
 ?>
