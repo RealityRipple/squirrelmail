72195bf3388c4ba48e5f9303e6b1ef90f5eb2496
teepe <teepe@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-12-25 01:41:18 +0000

    theme style police

--- trunk/squirrelmail/themes/high_contrast_theme.php
+++ trunk/squirrelmail/themes/high_contrast_theme.php
@@ -1,21 +1,30 @@
 <?php
-   /** Author:       Casey A. Peel
-       Date:         July 13, 2000
-       Theme Name:   "High Contrast"
 
-    **/
+/**
+ * high_contrast_theme.php
+ *    Name:    High Contrast
+ *    Author:  Casey A. Peel
+ *    Date:    July 13, 2000
+ *    Comment: 
+ *
+ * Copyright (c) 2000-2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
 
-    global $color;
-    $color[0]   = "#818181"; // (light gray)     TitleBar
-    $color[1]   = "#FF0000"; // (red)
-    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
-    $color[3]   = "#008080"; // (green-blue)     Left Bar Background
-    $color[4]   = "#FFFFFF"; // (white)          Normal Background
-    $color[5]   = "#FFFF00"; // (light yellow)   Table Headers
-    $color[6]   = "#000000"; // (black)          Text on left bar
-    $color[7]   = "#0000FF"; // (blue)           Links
-    $color[8]   = "#000000"; // (black)          Normal text
-    $color[9]   = "#C0C0C0"; // (mid-gray)       Darker version of #0
-    $color[10]  = "#808080"; // (dark gray)      Darker version of #9
-    $color[11]  = "#FFFFFF"; // (white)          Special Folders color
+global $color;
+$color[0]   = "#818181"; // (light gray)     TitleBar
+$color[1]   = "#FF0000"; // (red)
+$color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+$color[3]   = "#008080"; // (green-blue)     Left Bar Background
+$color[4]   = "#FFFFFF"; // (white)          Normal Background
+$color[5]   = "#FFFF00"; // (light yellow)   Table Headers
+$color[6]   = "#000000"; // (black)          Text on left bar
+$color[7]   = "#0000FF"; // (blue)           Links
+$color[8]   = "#000000"; // (black)          Normal text
+$color[9]   = "#C0C0C0"; // (mid-gray)       Darker version of #0
+$color[10]  = "#808080"; // (dark gray)      Darker version of #9
+$color[11]  = "#FFFFFF"; // (white)          Special Folders color
+
 ?>
