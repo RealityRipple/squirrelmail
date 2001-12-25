c7d0b12bd818252d17e731f1b4651492db5bb2f0
teepe <wouter@teepe.com>
2001-12-25 02:11:30 +0000

    theme style police

--- themes/purple_theme.php
+++ themes/purple_theme.php
@@ -1,23 +1,31 @@
 <?php
-   /** Author:       Luke Ehresman
-       Date:         January 7, 2000
-       Theme Name:   "Purple Theme"
 
-       This Theme is composed entirely of shades of purple.
-    **/
+/**
+ * purple_theme.php
+ *    Name:    Purple Theme
+ *    Author:  Luke Ehresman
+ *    Date:    January 7, 2000
+ *    Comment: This Theme is composed entirely of shades of purple.
+ *
+ * Copyright (c) 2000-2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
 
-    global $color;
-    $color[0]   = "#AC9ECB"; // (light purp)     TitleBar
-    $color[1]   = "#800000"; // (red)
-    $color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
-    $color[3]   = "#B6A7D5"; // (purple)         Left Bar Background
-    $color[4]   = "#EDE4FF"; // (light purple)   Normal Background
-    $color[5]   = "#DDD4EF"; // (darker gray)    Table Headers
-    $color[6]   = "#000000"; // (black)          Text on left bar
-    $color[7]   = "#622680"; // (light purple)   Links
-    $color[8]   = "#000000"; // (black)          Normal text
-    $color[9]   = "#877C9F"; // (mid purp)       Darker version of #0
-    $color[10]  = "#726987"; // (dark purp)      Darker version of #9
-    $color[11]  = "#770000"; // (dark red)       Special Folders color
-    $color[12]  = "#E0D9F2";
+global $color;
+$color[0]   = "#AC9ECB"; // (light purp)     TitleBar
+$color[1]   = "#800000"; // (red)
+$color[2]   = "#CC0000"; // (light red)      Warning/Error Messages
+$color[3]   = "#B6A7D5"; // (purple)         Left Bar Background
+$color[4]   = "#EDE4FF"; // (light purple)   Normal Background
+$color[5]   = "#DDD4EF"; // (darker gray)    Table Headers
+$color[6]   = "#000000"; // (black)          Text on left bar
+$color[7]   = "#622680"; // (light purple)   Links
+$color[8]   = "#000000"; // (black)          Normal text
+$color[9]   = "#877C9F"; // (mid purp)       Darker version of #0
+$color[10]  = "#726987"; // (dark purp)      Darker version of #9
+$color[11]  = "#770000"; // (dark red)       Special Folders color
+$color[12]  = "#E0D9F2";
+
 ?>
--- themes/seaspray_theme.php
+++ themes/seaspray_theme.php
@@ -1,20 +1,30 @@
 <?php
-   /** Author:       Luke Ehresman
-       Date:         March 3, 2000 
-       Theme Name:   "Sea Spray Theme"
-    **/
 
-    global $color;
-    $color[0]   = "#7CA4A4"; 
-    $color[1]   = "#800000";
-    $color[2]   = "#CC0000";
-    $color[3]   = "#588080";
-    $color[4]   = "#DCFFFF";
-    $color[5]   = "#98C0C0";
-    $color[6]   = "#000000";
-    $color[7]   = "#184040";
-    $color[8]   = "#000000";
-    $color[9]   = "#78A0A0";
-    $color[10]  = "#386060";
-    $color[11]  = "#FFFFB0";
+/**
+ * seaspray_theme.php
+ *    Name:    Sea Spray Theme
+ *    Author:  Luke Ehresman
+ *    Date:    March 3, 2000
+ *    Comment: 
+ *
+ * Copyright (c) 2000-2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+global $color;
+$color[0]   = "#7CA4A4"; 
+$color[1]   = "#800000";
+$color[2]   = "#CC0000";
+$color[3]   = "#588080";
+$color[4]   = "#DCFFFF";
+$color[5]   = "#98C0C0";
+$color[6]   = "#000000";
+$color[7]   = "#184040";
+$color[8]   = "#000000";
+$color[9]   = "#78A0A0";
+$color[10]  = "#386060";
+$color[11]  = "#FFFFB0";
+
 ?>
