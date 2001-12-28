f2c106ce7b70120b854fa3b46de1a5ab7153b6bf
fidian <fidian@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-12-28 17:00:53 +0000

    " -> '

--- trunk/squirrelmail/themes/midnight.php
+++ trunk/squirrelmail/themes/midnight.php
@@ -14,19 +14,19 @@
  */
 
 global $color;
-$color[0]   = "#0000FF"; // TitleBar
-$color[1]   = "#800000"; // Unseen messages color (unused?)
-$color[2]   = "#CC0000"; // Warning/Error Messages
-$color[3]   = "#000066"; // Left Bar Background
-$color[4]   = "#000000"; // Normal Background
-$color[5]   = "#003333"; // Table Headers
-$color[6]   = "#FFFFFF"; // Text on left bar
-$color[7]   = "#00FFCC"; // Links
-$color[8]   = "#FFFFFF"; // Normal text
-$color[9]   = "#000099"; // Darker version of #0
-$color[10]  = "#33FF66"; // Dimmed out folder color
-$color[11]  = "#000060"; // Special folders / Unseen messages color
-$color[12]  = "#3300CC"; // Alternate table row color
-$color[13]  = "#3333CC";
+$color[0]   = '#0000FF'; // TitleBar
+$color[1]   = '#800000'; // Unseen messages color (unused?)
+$color[2]   = '#CC0000'; // Warning/Error Messages
+$color[3]   = '#000066'; // Left Bar Background
+$color[4]   = '#000000'; // Normal Background
+$color[5]   = '#003333'; // Table Headers
+$color[6]   = '#FFFFFF'; // Text on left bar
+$color[7]   = '#00FFCC'; // Links
+$color[8]   = '#FFFFFF'; // Normal text
+$color[9]   = '#000099'; // Darker version of #0
+$color[10]  = '#33FF66'; // Dimmed out folder color
+$color[11]  = '#000060'; // Special folders / Unseen messages color
+$color[12]  = '#3300CC'; // Alternate table row color
+$color[13]  = '#3333CC';
 
 ?>
