bb0f8e2abf55ca472908ee2c5880e5085bb25f92
Tyler Akins <fidian@rumkin.com>
2001-12-28 16:31:01 +0000

    Added theme from Jim Riffle

---
+++ themes/midnight.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * midnight.php
+ *    Name:   Midnight
+ *    Author: Jim Riffle
+ *    Date:   November 14, 2001
+ *    Comment: A popular theme with Jim's users
+ *
+ * Copyright (c) 2001-2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+global $color;
+$color[0]   = "#0000FF"; // TitleBar
+$color[1]   = "#800000"; // Unseen messages color (unused?)
+$color[2]   = "#CC0000"; // Warning/Error Messages
+$color[3]   = "#000066"; // Left Bar Background
+$color[4]   = "#000000"; // Normal Background
+$color[5]   = "#003333"; // Table Headers
+$color[6]   = "#FFFFFF"; // Text on left bar
+$color[7]   = "#00FFCC"; // Links
+$color[8]   = "#FFFFFF"; // Normal text
+$color[9]   = "#000099"; // Darker version of #0
+$color[10]  = "#33FF66"; // Dimmed out folder color
+$color[11]  = "#000060"; // Special folders / Unseen messages color
+$color[12]  = "#3300CC"; // Alternate table row color
+$color[13]  = "#3333CC";
+
+?>
