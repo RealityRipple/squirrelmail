bb927dac8b9096ecd2f97c85db71fb49be46df25
philippe_mingo <philippe@squirrelmail.org>
2002-01-02 09:06:58 +0000

    Printer friendly fixed. Now it prints HTML messages.

--- src/printer_friendly_main.php
+++ src/printer_friendly_main.php
@@ -9,34 +9,18 @@
  * $Id$
  */
 
-/*****************************************************************/
-/*** THIS FILE NEEDS TO HAVE ITS FORMATTING FIXED!!!           ***/
-/*** PLEASE DO SO AND REMOVE THIS COMMENT SECTION.             ***/
-/***    + Base level indent should begin at left margin, as    ***/
-/***      the require_once below looks.                        ***/
-/***    + All identation should consist of four space blocks   ***/
-/***    + Tab characters are evil.                             ***/
-/***    + all comments should use "slash-star ... star-slash"  ***/
-/***      style -- no pound characters, no slash-slash style   ***/
-/***    + FLOW CONTROL STATEMENTS (if, while, etc) SHOULD      ***/
-/***      ALWAYS USE { AND } CHARACTERS!!!                     ***/
-/***    + Please use ' instead of ", when possible. Note "     ***/
-/***      should always be used in _( ) function calls.        ***/
-/*** Thank you for your help making the SM code more readable. ***/
-/*****************************************************************/
-
 require_once('../src/validate.php');
 require_once('../functions/page_header.php');
 
-    displayHtmlHeader( _("Printer Friendly"), '', FALSE );
+displayHtmlHeader( _("Printer Friendly"), '', FALSE );
 
-    echo "<frameset rows=\"50, *\" noresize border=\"0\">\n".
-         "<frame src=\"printer_friendly_top.php\" name=\"top_frame\" scrolling=\"off\">".
-         '<frame src="printer_friendly_bottom.php?passed_ent_id=';
-    echo $passed_ent_id . '&mailbox=' . urlencode($mailbox) .
-         '&passed_id=' . $passed_id;
-    echo "\" name=\"bottom_frame\">".
-         "</frameset>\n".
-         "</html>\n";
+echo "<frameset rows=\"60, *\" noresize border=\"0\">\n".
+     "<frame src=\"printer_friendly_top.php\" name=\"top_frame\" scrolling=\"off\">".
+     '<frame src="printer_friendly_bottom.php?passed_ent_id='.
+     $passed_ent_id . '&mailbox=' . urlencode($mailbox) .
+     '&passed_id=' . $passed_id .
+     "\" name=\"bottom_frame\">".
+     "</frameset>\n".
+     "</html>\n";
 
 ?>
