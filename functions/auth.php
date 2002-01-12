533d69a905fc63d92a57701779afb19ac4164d97
Bron Gondwana <brong@fastmail.fm>
2002-01-12 07:49:11 +0000

    More exciting clean-up

--- functions/auth.php
+++ functions/auth.php
@@ -11,40 +11,25 @@
  * $Id$
  */
 
-/*****************************************************************/
-/*** THIS FILE NEEDS TO HAVE ITS FORMATTING FIXED!!!           ***/
-/*** PLEASE DO SO AND REMOVE THIS COMMENT SECTION.             ***/
-/***    + Base level indent should begin at left margin, as    ***/
-/***      the require_once below.                              ***/
-/***    + All identation should consist of four space blocks   ***/
-/***    + Tab characters are evil.                             ***/
-/***    + all comments should use "slash-star ... star-slash"  ***/
-/***      style -- no pound characters, no slash-slash style   ***/
-/***    + FLOW CONTROL STATEMENTS (if, while, etc) SHOULD      ***/
-/***      ALWAYS USE { AND } CHARACTERS!!!                     ***/
-/***    + Please use ' instead of ", when possible. Note "     ***/
-/***      should always be used in _( ) function calls.        ***/
-/*** Thank you for your help making the SM code more readable. ***/
-/*****************************************************************/
-
 require_once( '../functions/page_header.php' );
 
-    function is_logged_in () {
-        global $squirrelmail_language;
+function is_logged_in () {
+    global $squirrelmail_language;
 
-        if ( session_is_registered('user_is_logged_in') )
-            return;
-
-        set_up_language($squirrelmail_language, true);
-
-        displayHtmlHeader( 'SquirrelMail', '', FALSE );
-
-        echo "<body bgcolor=\"ffffff\">\n" .
-            '<br><br><center><b>'.
-            _("You must be logged in to access this page.").'</b><br><br>'.
-            "<a href=\"../src/login.php\" target=\"_top\">"._("Go to the login page")."</a>\n".
-            "</center></body></html>\n";
-        exit;
+    if ( session_is_registered('user_is_logged_in') ) {
+        return;
     }
+
+    set_up_language($squirrelmail_language, true);
+
+    displayHtmlHeader( 'SquirrelMail', '', FALSE );
+
+    echo "<body bgcolor=\"ffffff\">\n" .
+         '<br><br><center><b>' .
+         _("You must be logged in to access this page.").'</b><br><br>' .
+         "<a href=\"../src/login.php\" target=\"_top\">"._("Go to the login page")."</a>\n" .
+         "</center></body></html>\n";
+    exit;
+}
 
 ?>
