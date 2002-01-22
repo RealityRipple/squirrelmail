048f3a7bcb4d598ff8073d9945f1579e38d18a07
philippe_mingo <philippe@squirrelmail.org>
2002-01-22 08:42:26 +0000

    1.3

--- plugins/listcommands/mailout.php
+++ plugins/listcommands/mailout.php
@@ -1,10 +1,10 @@
 <?php
 /*
  * mailout.php part of listcommands plugin
- * last modified: 2002/01/12 by Thijs Kinkhorst
+ * last modified: 2002/01/21 by Thijs Kinkhorst
  *
  */
- 
+
 chdir('..');
 include_once ('../src/validate.php');
 include_once ('../functions/page_header.php');
@@ -12,12 +12,10 @@ include_once ('../src/load_prefs.php');
 
 displayPageHeader($color, $mailbox);
 
-?>
-<P>
-<TABLE align="center" width="75%" BGCOLOR="<?php echo $color[0]; ?>">
-<TR><TH BGCOLOR="<?php echo $color[9] . '">' . _("Mailinglist") . ' ' . _($action); ?></TH></TR>
-<TR><TD>
-<?php
+echo '<P><TABLE align="center" width="75%" BGCOLOR="' . $color[0] ."\">\n"
+    .'<TR><TH BGCOLOR="'. $color[9] . '">' . _("Mailinglist") . ' ' . _($action) .
+    "</TH></TR>\n<TR><TD>";
+
 
 switch ( $action ) {
 case 'Help':
@@ -27,7 +25,7 @@ case 'Subscribe':
     $out_string .= _("This will send a message to %s requesting that you will be subscribed to this list. You will be subscribed with the address below.");
     break;
 case 'Unsubscribe':
-    $out_string = _("This will send a message to %s requesting that you will be subscribed to this list. It will try to unsubscribe the adress below.");
+    $out_string = _("This will send a message to %s requesting that you will be unsubscribed from this list. It will try to unsubscribe the adress below.");
 }
 
 printf( $out_string, htmlspecialchars($send_to) );
@@ -48,9 +46,8 @@ if ($em != '') {
 }
 echo '<P><CENTER>' . _("From:");
 
-$idents = getPref($data_dir, $username, 'identities'); 
-if ($idents != '' && $idents > 1)
-{
+$idents = getPref($data_dir, $username, 'identities');
+if ($idents != '' && $idents > 1) {
     echo ' <select name=identity>' . "\n" .
          '<option value=default>' . $defaultmail;
     for ($i = 1; $i < $idents; $i ++) {
@@ -64,23 +61,18 @@ if ($idents != '' && $idents > 1)
         if ($em != '') {
             echo htmlspecialchars(' <' . $em . '>') . "\n";
         }
-    } 
+    }
     echo '</select>' . "\n" ;
 
-}
-else
-{
+} else {
     echo $defaultmail;
-} 
+}
+
+echo '<BR>'
+. '<input type=hidden name="send_to" value="' . htmlspecialchars($send_to) . '">'
+. '<input type=hidden name="subject" value="' . htmlspecialchars($subject) . '">'
+. '<input type=hidden name="body" value="' . htmlspecialchars($body) . '">'
+. '<input type=hidden name="mailbox" value="' . htmlspecialchars($mailbox) . '">'
+. '<input type=submit name="send" value="' . _("Send Mail") . '"><BR><BR></CENTER>'
+. '</form></TD></TR></TABLE></P></BODY></HTML>';
 ?>
-<BR>
-<input type=hidden name="send_to" value="<?php echo htmlspecialchars($send_to); ?>">
-<input type=hidden name="subject" value="<?php echo htmlspecialchars($subject); ?>">
-<input type=hidden name="body" value="<?php echo htmlspecialchars($body); ?>">
-<input type=hidden name="mailbox" value="<?php echo htmlspecialchars($mailbox); ?>">
-<input type=submit name="send" value="<?php echo _("Send Mail"); ?>"><BR><BR></CENTER>
-</form>
-</TD></TR></TABLE>
-</P>
-</BODY>
-</HTML>
--- plugins/listcommands/setup.php
+++ plugins/listcommands/setup.php
@@ -1,13 +1,13 @@
 <?php
 /*
- * Listcommands plugin v1.2
+ * Listcommands plugin v1.3
  *
  * Implementation of RFC 2369 for SquirrelMail.
  * When viewing a message from a mailinglist complying with this RFC,
  * this plugin displays a menu which gives the user a choice of mailinglist
  * commands such as (un)subscribe, help and list archives.
  *
- * last modified: 2002/01/20 by Thijs Kinkhorst
+ * last modified: 2002/01/21 by Thijs Kinkhorst
  * please send bug reports to <thijs@kinkhorst.com>
  *
  */
@@ -21,20 +21,21 @@ function squirrelmail_plugin_init_listcommands ()
 
 function plugin_listcommands_menu () {
 
-    global $imapConnection, $passed_id, $color, $mailbox, $subject;
+    global $imapConnection, $passed_id, $color, $mailbox,
+           $subject, $ent_num, $priority_level;
 
     /* Array of commands we can deal with from the header. The Reply option is
      * added later because we generate it using the Post information.
      */
-    $fieldsdescr = array( 'Help' => _("Help"), 
+    $fieldsdescr = array( 'Help' => _("Help"),
                           'Unsubscribe' => _("Unsubscribe"),
-                          'Subscribe' => _("Subscribe"), 
+                          'Subscribe' => _("Subscribe"),
                           'Post' => _("Post to the list"),
-                          'Archive' => _("List Archives"), 
+                          'Archive' => _("List Archives"),
                           'Owner' => _("Contact Listowner") );
     $fields = array_keys ($fieldsdescr);
-    $fieldsdescr['Reply'] = 'Reply to the list';
-                    
+    $fieldsdescr['Reply'] = _("Reply to the list");
+
     $cmds = array();
     $output = array();
 
@@ -44,62 +45,49 @@ function plugin_listcommands_menu () {
     fputs ($imapConnection, "$sid FETCH $passed_id BODY.PEEK[HEADER.FIELDS ($lfields)]\r\n");
     $read = sqimap_read_data ($imapConnection, $sid, true, $response, $emessage);
 
-    for ($i = 1; $i < count($read); $i++)
-    {
-        foreach ($fields as $field)
-        {
-            if ( preg_match("/^List-$field: *<(.+?)>/i", $read[$i], $match) )
-            {
+    for ($i = 1; $i < count($read); $i++) {
+        foreach ($fields as $field) {
+            if ( preg_match("/^List-$field: *<(.+?)>/i", $read[$i], $match) ) {
                 $cmds[$field] = $match[1];
             }
         }
     }
 
-    foreach ($cmds as $cmd => $url)
-    {
-        if ( eregi('mailto:(.+)', $url, $regs) )
-        {
+    foreach ($cmds as $cmd => $url) {
+        if ( eregi('mailto:(.+)', $url, $regs) ) {
             $purl = parse_url($url);
-            
-            if ( $cmd == 'Post' || $cmd == 'Owner' )
-            {
+
+            if ( $cmd == 'Post' || $cmd == 'Owner' ) {
                 $url = 'compose.php?';
-            }
-            else
-            {
+            } else {
                 $url = '../plugins/listcommands/mailout.php?action=' . $cmd . '&';
             }
 
             $url .= 'mailbox=' . urlencode($mailbox) . '&send_to=' . $purl['path'];
-            
-            if ( isset($purl['query']) )
-            {
+
+            if ( isset($purl['query']) ) {
                 $url .= '&' . $purl['query'];
             }
 
             $output[] = '<A HREF="' . $url . '">' . $fieldsdescr[$cmd] . '</A>';
 
-            if ( $cmd == 'Post' )
-            {
-                $url .= '&reply_subj=' . urlencode($subject) . '&reply_id=' . $passed_id;
+            if ( $cmd == 'Post' ) {
+                $url .= '&reply_subj=' . urlencode($subject) .
+                        '&reply_id=' . $passed_id .
+                        '&ent_num=' . $ent_num .
+                        '&mailprio=' . $priority_level;
                 $output[] = '<A HREF="' . $url . '">' . $fieldsdescr['Reply'] . '</A>';
             }
-        }
-        elseif ( eregi('^(http|ftp)', $url) )
-        {
+        } elseif ( eregi('^(http|ftp)', $url) ) {
             $output[] = '<A HREF="' . $url . '" TARGET="_blank">' . $fieldsdescr[$cmd] . '</A>';
         }
-
-        
     }
 
-       
     if (count($output) > 0) {
         echo "<tr><td BGCOLOR=\"$color[0]\" WIDTH=\"100%\" colspan=\"3\">".
              '<SMALL>' . _("Mailinglist options:") . ' ' . implode ('&nbsp;|&nbsp;', $output) .
              '</SMALL>'.
              '</td></tr>';
     }
-    
-}    
+}
 ?>
