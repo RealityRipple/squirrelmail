38ae25567f8a734e26ff9ac9a4fc07201e468bfc
philippe_mingo <philippe@squirrelmail.org>
2002-01-27 16:55:18 +0000

    defines.php now indicates the order for options display. Added a group type in order to place options under different titles.
    Not yet finished.

--- plugins/administrator/defines.php
+++ plugins/administrator/defines.php
@@ -22,6 +22,7 @@ define('SMOPT_TYPE_FLOAT', 4);
 define('SMOPT_TYPE_BOOLEAN', 5);
 define('SMOPT_TYPE_HIDDEN', 6);
 define('SMOPT_TYPE_COMMENT', 7);
+define('SMOPT_TYPE_TITLE', 128);
 
 /* Define constants for the options refresh levels. */
 define('SMOPT_REFRESH_NONE', 0);
@@ -48,38 +49,68 @@ foreach ($languages as $lang_key => $lang_attributes) {
 }
 asort( $language_values );
 
-$namcfg = array( '$config_version' => array( 'name' => _("Config File Version"),
-                                             'type' => 'string',
+$defcfg = array( '$config_version' => array( 'name' => _("Config File Version"),
+                                             'type' => SMOPT_TYPE_COMMENT,
                                              'size' => 7 ),
-                 '$org_logo' => array( 'name' => _("Organization Logo"),
-                                       'type' => SMOPT_TYPE_STRING,
-                                       'size' => 40 ),
+                 /* --------------------------------------------------------*/
+                 'Group1' => array( 'name' => _("Organization Preferences"),
+                                    'type' => SMOPT_TYPE_TITLE ),
                  '$org_name' => array( 'name' => _("Organization Name"),
                                        'type' => SMOPT_TYPE_STRING,
                                        'size' => 40 ),
-                 '$org_title' => array( 'name' => _("Organization Name"),
+                 '$org_logo' => array( 'name' => _("Organization Logo"),
+                                       'type' => SMOPT_TYPE_STRING,
+                                       'size' => 40 ),
+                 '$org_title' => array( 'name' => _("Organization Title"),
                                         'type' => SMOPT_TYPE_STRING,
                                         'size' => 40 ),
+                 '$signout_page' => array( 'name' => _("Signout Page"),
+                                           'type' => SMOPT_TYPE_STRING,
+                                           'size' => 40 ),
                  '$squirrelmail_default_language' => array( 'name' => _("Default Language"),
                                                             'type' => SMOPT_TYPE_STRLIST,
                                                             'size' => 7,
                                                             'posvals' => $language_values ),
+                 /* --------------------------------------------------------*/
+                 'Group2' => array( 'name' => _("Server Settings"),
+                                    'type' => SMOPT_TYPE_TITLE ),
+                 '$domain' => array( 'name' => _("Mail Domain"),
+                                                'type' => SMOPT_TYPE_STRING,
+                                                'size' => 40 ),
                  '$imapServerAddress' => array( 'name' => _("IMAP Server Address"),
                                                 'type' => SMOPT_TYPE_STRING,
                                                 'size' => 40 ),
                  '$imapPort' => array( 'name' => _("IMAP Server Port"),
                                                  'type' => SMOPT_TYPE_INTEGER ),
-                 '$domain' => array( 'name' => _("Mail Domain"),
-                                                'type' => SMOPT_TYPE_STRING,
-                                                'size' => 40 ),
+                 '$imap_server_type' => array( 'name' => _("IMAP Server Type"),
+                                               'type' => SMOPT_TYPE_STRLIST,
+                                               'size' => 7,
+                                               'posvals' => array( 'cyrus' => _("Cyrus IMAP server"),
+                                                                   'uw' => _("University of Washington's IMAP server"),
+                                                                   'exchange' => _("Microsoft Exchange IMAP server"),
+                                                                   'courier' => _("Courier IMAP server"),
+                                                                   'other' => _("Not one of the above servers") ) ),
+                 '$optional_delimiter' => array( 'name' => _("IMAP Folder Delimiter"),
+                                                 'type' => SMOPT_TYPE_STRING,
+                                                 'size' => 2 ),
+                 '$useSendmail' => array( 'name' => _("Use Sendmail"),
+                                          'type' => SMOPT_TYPE_BOOLEAN ),
                  '$smtpServerAddress' => array( 'name' => _("SMTP Server Address"),
                                                 'type' => SMOPT_TYPE_STRING,
                                                 'size' => 40 ),
                  '$smtpPort' => array( 'name' => _("SMTP Server Port"),
                                                  'type' => SMOPT_TYPE_INTEGER ),
+                 '$use_authenticated_smtp' => array( 'name' => _("Authenticated SMTP"),
+                                                     'type' => SMOPT_TYPE_BOOLEAN ),
+                 '$invert_time' => array( 'name' => _("Invert Time"),
+                                          'type' => SMOPT_TYPE_BOOLEAN ),
+                 /* --------------------------------------------------------*/
+                 'Group3' => array( 'name' => _("Folders Defaults"),
+                                    'type' => SMOPT_TYPE_TITLE ),
                  '$motd' => array( 'name' => _("Message of the Day"),
                                              'type' => SMOPT_TYPE_STRING,
                                              'size' => 40 ),
+
                );
 
 ?>
