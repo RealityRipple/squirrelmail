a434ce82ece97f7409d404b8692541a86293cbc6
thomppj <thomppj@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-01-31 03:13:37 +0000

    Prettied up list commands plugin. Sorry, translators...

--- trunk/squirrelmail/plugins/listcommands/setup.php
+++ trunk/squirrelmail/plugins/listcommands/setup.php
@@ -20,76 +20,95 @@ function squirrelmail_plugin_init_listcommands () {
     $squirrelmail_plugin_hooks['read_body_header']['listcommands'] = 'plugin_listcommands_menu';
 }
 
-
-function plugin_listcommands_menu () {
-
+function plugin_listcommands_menu() {
     global $imapConnection, $passed_id, $color, $mailbox,
            $subject, $ent_num, $priority_level;
 
-    /* Array of commands we can deal with from the header. The Reply option is
-     * added later because we generate it using the Post information.
+    /**
+     * Array of commands we can deal with from the header. The Reply option
+     * is added later because we generate it using the Post information.
      */
-    $fieldsdescr = array( 'Help' => _("Help"),
-                          'Unsubscribe' => _("Unsubscribe"),
-                          'Subscribe' => _("Subscribe"),
-                          'Post' => _("Post to the list"),
-                          'Archive' => _("List Archives"),
-                          'Owner' => _("Contact Listowner") );
-    $fields = array_keys ($fieldsdescr);
-    $fieldsdescr['Reply'] = _("Reply to the list");
+    $fieldsdescr = array('Post'        => _("Post to List"),
+                         'Reply'       => _("Reply to List"),
+                         'Subscribe'   => _("Subscribe"),
+                         'Unsubscribe' => _("Unsubscribe"),
+                         'Archive'     => _("List Archives"),
+                         'Owner'       => _("Contact Listowner"),
+                         'Help'        => _("Help"));
+    $fields = array_keys($fieldsdescr);
 
-    $cmds = array();
+    $sorted_cmds = array();
+    $unsorted_cmds = array();
     $output = array();
 
     $lfields = 'List-' . implode (' List-', $fields);
 
     $sid = sqimap_session_id();
     fputs ($imapConnection, "$sid FETCH $passed_id BODY.PEEK[HEADER.FIELDS ($lfields)]\r\n");
-    $read = sqimap_read_data ($imapConnection, $sid, true, $response, $emessage);
+    $read = sqimap_read_data($imapConnection, $sid, true, $response, $emessage);
 
     for ($i = 1; $i < count($read); $i++) {
         foreach ($fields as $field) {
             if ( preg_match("/^List-$field: *<(.+?)>/i", $read[$i], $match) ) {
-                $cmds[$field] = $match[1];
+                $unsorted_cmds[$field] = $match[1];
+            }
+        }
+    }
+
+    if (count($unsorted_cmds) == 0) {
+        return;
+    }
+
+    foreach ($fields as $field) {
+        foreach ($unsorted_cmds as $cmd => $url) {
+            if ($field == $cmd) {
+                $cmds[$cmd] = $url;
             }
         }
     }
 
     foreach ($cmds as $cmd => $url) {
-        if ( eregi('mailto:(.+)', $url, $regs) ) {
+        if (eregi('mailto:(.+)', $url, $regs)) {
             $purl = parse_url($url);
 
-            if ( $cmd == 'Post' || $cmd == 'Owner' ) {
+            if (($cmd == 'Post') || ($cmd == 'Owner')) {
                 $url = 'compose.php?';
             } else {
-                $url = '../plugins/listcommands/mailout.php?action=' . $cmd . '&';
+                $url = "../plugins/listcommands/mailout.php?action=$cmd&";
             }
 
-            $url .= 'mailbox=' . urlencode($mailbox) . '&send_to=' . $purl['path'];
+            $url .= 'mailbox=' . urlencode($mailbox)
+                  . '&send_to=' . $purl['path'];
 
-            if ( isset($purl['query']) ) {
+            if (isset($purl['query'])) {
                 $url .= '&' . $purl['query'];
             }
 
             $output[] = '<A HREF="' . $url . '">' . $fieldsdescr[$cmd] . '</A>';
 
-            if ( $cmd == 'Post' ) {
-                $url .= '&reply_subj=' . urlencode($subject) .
-                        '&reply_id=' . $passed_id .
-                        '&ent_num=' . $ent_num .
-                        '&mailprio=' . $priority_level;
+            if ($cmd == 'Post') {
+                $url .= '&reply_subj=' . urlencode($subject)
+                      . '&reply_id=' . $passed_id
+                      . '&ent_num=' . $ent_num
+                      . '&mailprio=' . $priority_level;
                 $output[] = '<A HREF="' . $url . '">' . $fieldsdescr['Reply'] . '</A>';
             }
-        } elseif ( eregi('^(http|ftp)', $url) ) {
-            $output[] = '<A HREF="' . $url . '" TARGET="_blank">' . $fieldsdescr[$cmd] . '</A>';
+        } else if (eregi('^(http|ftp)', $url)) {
+            $output[] = '<A HREF="' . $url . '" TARGET="_blank">'
+                      . $fieldsdescr[$cmd] . '</A>';
         }
     }
 
     if (count($output) > 0) {
-        echo "<tr><td BGCOLOR=\"$color[0]\" WIDTH=\"100%\" colspan=\"3\">".
-             '<SMALL>' . _("Mailinglist options:") . ' ' . implode ('&nbsp;|&nbsp;', $output) .
-             '</SMALL>'.
-             '</td></tr>';
+        echo "<tr>";
+        echo "<td BGCOLOR=\"$color[0]\">"
+           .   str_replace(' ', '&nbsp;', _("Mailing List:"))
+           . '</td>';
+        echo "<td BGCOLOR=\"$color[0]\" WIDTH=\"100%\" colspan=\"2\">"
+           .   '<SMALL>' . implode('&nbsp;|&nbsp;', $output) . '</SMALL>'
+           . '</td>';
+        echo '</tr>';
     }
 }
+
 ?>
