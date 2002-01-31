a4a71654608ee06606be174c5a841ef150c9ffdd
philippe_mingo <philippe@squirrelmail.org>
2002-01-31 11:03:48 +0000

    bugfix

--- plugins/administrator/auth.php
+++ plugins/administrator/auth.php
@@ -8,7 +8,7 @@ function adm_check_user() {
     
     if ( file_exists( '../plugins/administrator/admins' ) ) {
         $auths = file( '../plugins/administrator/admins' );
-        $auth = in_array( $username, $auths );
+        $auth = in_array( "$username\n", $auths );
     }else if ( $adm_id = fileowner('../config/config.php') ) {
         $adm = posix_getpwuid( $adm_id );
         if ( $username == $adm['name'] ) {
