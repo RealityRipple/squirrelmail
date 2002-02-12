132d4d933f33e9227b8db7c3f1303278033bd295
philippe_mingo <philippe@squirrelmail.org>
2002-02-12 19:22:45 +0000

    Code cleanup

--- functions/display_messages.php
+++ functions/display_messages.php
@@ -35,66 +35,67 @@ function error_username_password_incorrect() {
 }
 
 function general_info($motd, $org_logo, $version, $org_name, $color) {
-    echo '<BR>';
-    echo "<TABLE COLS=1 WIDTH=\"80%\" CELLSPACING=0 CELLPADDING=2 NOBORDER ALIGN=CENTER><TR><TD BGCOLOR=\"$color[9]\">";
-    echo '<TABLE COLS=1 WIDTH="100%" CELLSPACING=0 CELLPADDING=3 NOBORDER BGCOLOR="' .  $color[4] . '" ALIGN=CENTER>';
-    echo '   <TR>';
-    echo "      <TD BGCOLOR=\"$color[0]\">";
-    echo '         <B><CENTER>';
+
+    echo '<BR>'.
+         "<TABLE COLS=1 WIDTH=\"80%\" CELLSPACING=0 CELLPADDING=2 NOBORDER ALIGN=CENTER><TR><TD BGCOLOR=\"$color[9]\">".
+         '<TABLE COLS=1 WIDTH="100%" CELLSPACING=0 CELLPADDING=3 NOBORDER BGCOLOR="' .  $color[4] . '" ALIGN=CENTER>'.
+            '<TR>' .
+               "<TD BGCOLOR=\"$color[0]\">" .
+                  '<B><CENTER>';
     printf (_("Welcome to %s's WebMail system"), $org_name);
-    echo '         </CENTER></B>';
-    echo '   <TR><TD BGCOLOR="' . $color[4] .  '">';
-    echo '      <TABLE COLS=2 WIDTH="90%" CELLSPACING=0 CELLPADDING=3 NOBORDER align="center">';
-    echo '         <TR>';
-    echo '            <TD BGCOLOR="' . $color[4] .  '"><CENTER>';
+    echo          '</CENTER></B>'.
+            '<TR><TD BGCOLOR="' . $color[4] .  '">'.
+               '<TABLE COLS=2 WIDTH="90%" CELLSPACING=0 CELLPADDING=3 NOBORDER align="center">'.
+                  '<TR>'.
+                     '<TD BGCOLOR="' . $color[4] .  '"><CENTER>';
     if ( strlen($org_logo) > 3 ) {
-        echo "               <IMG SRC=\"$org_logo\">";
+        echo                "<IMG SRC=\"$org_logo\">";
     } else {
-        echo "               <B>$org_name</B>";
+        echo                "<B>$org_name</B>";
     }
-    echo '         <BR><CENTER>';
+    echo          '<BR><CENTER>';
     printf (_("Running SquirrelMail version %s (c) 1999-2001."), $version);
-    echo '            </CENTER><BR>';
-    echo '            </CENTER></TD></TR><TR>';
-    echo '            <TD BGCOLOR="' . $color[4] .  '">';
-    echo "               $motd";
-    echo '            </TD>';
-    echo '         </TR>';
-    echo '      </TABLE>';
-    echo '   </TD></TR>';
-    echo '</TABLE>';
-    echo '</TD></TR></TABLE>';
+    echo             '</CENTER><BR>'.
+                     '</CENTER></TD></TR><TR>' .
+                     '<TD BGCOLOR="' . $color[4] .  '">' .
+                         $motd.
+                     '</TD>'.
+                  '</TR>'.
+               '</TABLE>'.
+            '</TD></TR>'.
+         '</TABLE>'.
+         '</TD></TR></TABLE>';
 }
 
 function error_message($message, $mailbox, $sort, $startMessage, $color) {
     $urlMailbox = urlencode($mailbox);
-    
-    echo '<BR>';
-    echo "<TABLE COLS=1 WIDTH=\"70%\" NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>";
-    echo '   <TR>';
-    echo "      <TD BGCOLOR=\"$color[0]\">";
-    echo "         <FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>';
-    echo '   </TD></TR><TR><TD>';
-    echo "      <CENTER><BR>$message<BR>\n";
-    echo '      <BR>';
-    echo "         <A HREF=\"right_main.php?sort=$sort&startMessage=$startMessage&mailbox=$urlMailbox\">";
+
+    echo '<BR>'.
+         "<TABLE COLS=1 WIDTH=\"70%\" NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>".
+            '<TR>'.
+               "<TD BGCOLOR=\"$color[0]\">".
+                  "<FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>'.
+            '</TD></TR><TR><TD>'.
+               "<CENTER><BR>$message<BR>\n".
+               '<BR>'.
+                  "<A HREF=\"right_main.php?sort=$sort&startMessage=$startMessage&mailbox=$urlMailbox\">";
     printf (_("Click here to return to %s"), $mailbox);
-    echo '</A>.';
-    echo '   </TD></TR>';
-    echo '</TABLE>';
+    echo '</A>.'.
+            '</TD></TR>'.
+         '</TABLE>';
 }
 
 function plain_error_message($message, $color) {
-    echo '<BR>';
-    echo "<TABLE COLS=1 WIDTH=\"70%\" NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>";
-    echo '   <TR>';
-    echo "      <TD BGCOLOR=\"$color[0]\">";
-    echo "         <FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>';
-    echo '   </TD></TR><TR><TD>';
-    echo "      <CENTER><BR>$message";
-    echo '      </CENTER>';
-    echo '   </TD></TR>';
-    echo '</TABLE>';
+    echo '<BR>'.
+         "<TABLE COLS=1 WIDTH=\"70%\" NOBORDER BGCOLOR=\"$color[4]\" ALIGN=CENTER>".
+            '<TR>'.
+               "<TD BGCOLOR=\"$color[0]\">".
+                  "<FONT COLOR=\"$color[2]\"><B><CENTER>" . _("ERROR") . '</CENTER></B></FONT>'.
+            '</TD></TR><TR><TD>'.
+               "<CENTER><BR>$message".
+               '</CENTER>'.
+            '</TD></TR>'.
+         '</TABLE>';
 }
-    
+
 ?>
