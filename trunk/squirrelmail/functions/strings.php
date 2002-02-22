9c52d1fb35f46674a843d42c5bd420fd5c453c06
teepe <teepe@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-02-22 13:14:57 +0000

    going on -- defrost

--- trunk/squirrelmail/functions/strings.php
+++ trunk/squirrelmail/functions/strings.php
@@ -16,7 +16,7 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.2.5';
+$version = '1.2.6 [cvs]';
 
 /**
  * If $haystack is a full mailbox name and $needle is the mailbox
