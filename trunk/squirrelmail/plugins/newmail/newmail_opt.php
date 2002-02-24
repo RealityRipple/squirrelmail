1b6beb7618fa8aff1cf131cbee131de4aff4a8a9
teepe <teepe@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-02-24 20:47:27 +0000

    allow admin disabling of sound

--- trunk/squirrelmail/plugins/newmail/newmail_opt.php
+++ trunk/squirrelmail/plugins/newmail/newmail_opt.php
@@ -29,13 +29,19 @@
     $media_changetitle = getPref($data_dir,$username,'newmail_changetitle');
     $media = getPref($data_dir,$username,'newmail_media', '../plugins/newmail/sounds/Notify.wav');
 
+    // Set $allowsound to false if you don't want sound files available
+    $allowsound = "true";
+
     echo '<br>' .
          "<table width=95% align=center border=0 cellpadding=2 cellspacing=0><tr><td bgcolor=\"$color[0]\">\n".
          '<center><b>' . _("Options") . ' - ' . _("New Mail Notification") . '</b></center>'.
-         '</td></tr><tr><td>'.
-         '<p>'.
-_("Select <b>Enable Media Playing</b> to turn on playing a media file when unseen mail is in your folders. When enabled, you can specify the media file to play in the provided file box.") .
-         '</p><p>'.
+         '</td></tr><tr><td>';
+    if ($allowsound == "true") {
+        echo '<p>'.
+        _("Select <b>Enable Media Playing</b> to turn on playing a media file when unseen mail is in your folders. When enabled, you can specify the media file to play in the provided file box.") .
+        '</p>';
+    }
+    echo '<p>'.
 _("The <b>Check all boxes, not just INBOX</b> option will check ALL of your folders for unseen mail, not just the inbox for notification.").
          '</p><p>'.
 _("Selecting the <b>Show popup</b> option will enable the showing of a popup window when unseen mail is in your folders (requires JavaScript).") .
@@ -43,20 +49,25 @@ _("Selecting the <b>Show popup</b> option will enable the showing of a popup win
 _("Use the <b>Check RECENT</b> to only check for messages that are recent. Recent messages are those that have just recently showed up and have not been \"viewed\" or checked yet.  This can prevent being continuously annoyed by sounds or popups for unseen mail.").
          '</p><p>'.
 _("Selecting the <b>Change title</b> option will change the title in some browsers to let you know when you have new mail (requires JavaScript, and only works in IE but you won't see errors with other browsers).  This will always tell you if you have new mail, even if you have <b>Check RECENT</b> enabled.").
-         '</p><p>'.
+         '</p>';
+    if ($allowsound == "true") {
+        echo '<p>'.
 _("Select from the list of <b>server files</b> the media file to play when new mail arrives.  Selecting <b>local media</b> will play the file specified in the <b>local media file</b> box to play from the local computer.  If no file is specified, the system will use a default from the server.") .
-         '</p>'.
-         '<form action="../../src/options.php" method=post>'.
+         '</p>';
+    }
+    echo '<form action="../../src/options.php" method=post>'.
          '<table width=100% cellpadding=0 cellspacing=2 border=0>'.
             '<tr>'.
-                '<td align=right nowrap>&nbsp</td>'.
-                '<td><input type=checkbox ';
-    if ($media_enable == 'on') {
-              echo 'checked ';
-    }
-    echo 'name=media_enable><b> ' . _("Enable Media Playing") . '</b></td>'.
-            '</tr>'.
-            '<tr>'.
+                '<td align=right nowrap>&nbsp</td>';
+    if ($allowsound == "true") {
+                echo '<td><input type=checkbox ';
+        if ($media_enable == 'on') {
+                echo 'checked ';
+        }
+        echo 'name=media_enable><b> ' . _("Enable Media Playing") . '</b></td>'.
+                '</tr>';
+    }  
+            echo '<tr>'.
                 '<td align=right nowrap>&nbsp</td>'.
                 '<td><input type=checkbox ';
     if ($media_allbox == 'on') {
@@ -87,8 +98,9 @@ _("Select from the list of <b>server files</b> the media file to play when new m
               echo 'checked ';
     }
     echo 'name=media_popup><b> ' . _("Show popup window on new mail") . '</b> &nbsp; (' . _("requires JavaScript to work") . ')</td>'.
-            '</tr>'.
-            '<tr>'.
+            '</tr>';
+    if ($allowsound == "true") {
+            echo '<tr>'.
                 '<td align=right nowrap>' . _("Select server file:") . '</td>'.
                 '<td><SELECT NAME=media_sel>'.
                     '<OPTION VALUE="(local media)">' . _("(local media)") . '</option>';
@@ -125,8 +137,9 @@ _("Select from the list of <b>server files</b> the media file to play when new m
             '<td align=right nowrap>' . _("Current File:") .
             '</td><td>'.
            "<input type=hidden value=\"$media;\" name=media_default>$media;</td>".
-         '</tr>'.
-         '<tr>'.
+         '</tr>';
+    }
+         echo '<tr>'.
             '<td>&nbsp;</td>'.
             '<td>'.
                '<INPUT TYPE=HIDDEN NAME=optmode VALUE=submit>'. 
