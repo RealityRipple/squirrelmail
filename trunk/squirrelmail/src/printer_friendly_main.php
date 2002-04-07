75fa25d87933d23ffc05a93645123d4f05f4636c
graf25 <graf25@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-04-07 03:51:39 +0000

    HTML spec for frames says it's scrolling="no". So does bug #539775.

--- trunk/squirrelmail/src/printer_friendly_main.php
+++ trunk/squirrelmail/src/printer_friendly_main.php
@@ -15,7 +15,7 @@ require_once('../functions/page_header.php');
 displayHtmlHeader( _("Printer Friendly"), '', FALSE );
 
 echo "<frameset rows=\"60, *\" noresize border=\"0\">\n".
-     "<frame src=\"printer_friendly_top.php\" name=\"top_frame\" scrolling=\"off\">".
+     "<frame src=\"printer_friendly_top.php\" name=\"top_frame\" scrolling=\"no\">".
      '<frame src="printer_friendly_bottom.php?passed_ent_id='.
      $passed_ent_id . '&amp;mailbox=' . urlencode($mailbox) .
      '&amp;passed_id=' . $passed_id .
