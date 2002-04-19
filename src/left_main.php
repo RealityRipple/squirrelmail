d8f32af1e49984f3fc3927545700e73e789e2618
graf25 <icon@duke.edu>
2002-04-19 03:58:17 +0000

    Noting changes, plus committing the "expunge"->"purge" change.

--- src/left_main.php
+++ src/left_main.php
@@ -94,7 +94,7 @@ function formatMailboxName($imapConnection, $box_array) {
         if ($numMessages > 0) {
             $urlMailbox = urlencode($real_box);
             $line .= "\n<small>\n" .
-                    "&nbsp;&nbsp;(<A HREF=\"empty_trash.php\" style=\"text-decoration:none\">"._("expunge")."</A>)" .
+                    "&nbsp;&nbsp;(<A HREF=\"empty_trash.php\" style=\"text-decoration:none\">"._("purge")."</A>)" .
                     "</small>";
         }
     }
