2017605ee3db2361486a00818b38c0c84811636e
jmunro <jason@stdbev.com>
2002-06-10 18:42:08 +0000

    Adding a couple themes, alien_glow is from Todd Hammer, dark_green is from me :)

---
+++ themes/dark_green.php
@@ -0,0 +1,23 @@
+<?php
+   /** Author:       Ray Black
+       Date:         August 15, 2000
+       Theme Name:   "Black Bean Burrito"
+    **/
+
+    global $color;
+    $color[0]   = "#0a2801"; // (mid-dark gray)		TitleBar
+    $color[1]   = "#F88888"; // (light red)			Quoted Replies in Emails
+    $color[2]   = "#ff7171"; // (pinkish)			Warning/Error Messages
+    $color[3]   = "#0a2801"; // (black)				Left Bar Background
+    $color[4]   = "#000000"; // (black)				Normal Background
+    $color[5]   = "#0e3503"; // (dark yellow-green)	Table Headers
+    $color[6]   = "#D0D0D0"; // (off-white)			Text on left bar
+    $color[7]   = "#cfcfa0"; // (very dull yellow)	Links
+    $color[8]   = "#D0D0D0"; // (off-white)			Normal text
+    $color[9]   = "#0e3503"; // (dark gray)			Darker version of #0
+    $color[10]  = "#0a2801"; // (darker gray)		Darker version of #9
+    $color[11]  = "#AAAA44"; // (gold)			Special Folders color
+    $color[12]  = "#0a2801"; // (dark gray)             Alternate color for message backround in index table
+    $color[13]  = "#FF7733"; // (orange-ish)  Single-quoted text
+    $color[14]  = "#FF3377"; // (pink-ish)  2 or more quotes
+?>
