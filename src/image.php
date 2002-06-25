dac74e67ba0df3b28cfbbe88035711162b166fd1
philippe_mingo <philippe@squirrelmail.org>
2002-06-25 07:58:34 +0000

    r2l by Yoav

--- src/image.php
+++ src/image.php
@@ -15,41 +15,49 @@ require_once('../src/validate.php');
 require_once('../functions/date.php');
 require_once('../functions/page_header.php');
 require_once('../src/load_prefs.php');
+require_once('../functions/html.php');
 
 displayPageHeader($color, 'None');
 
-echo '<BR>' . 
-    '<TABLE WIDTH="100%" BORDER=0 CELLSPACING=0 CELLPADDING=2 ALIGN=CENTER>' .
-    "\n" .
-    '<TR><TD BGCOLOR="' . $color[0] . '">' .
-    '<B><CENTER>' .
-    _("Viewing an image attachment") . " - ";
 if (isset($where) && isset($what)) {
   // from a search
-  echo '<a href="../src/read_body.php?mailbox=' . urlencode($mailbox) .
+  $ViewMessageLink = '<a href="../src/read_body.php?mailbox=' . urlencode($mailbox) .
         '&amp;passed_id=' . $passed_id . '&amp;where=' . urlencode($where) . 
         '&amp;what=' . urlencode($what). '">' . _("View message") . '</a>';
 } else {   
-  echo '<a href="../src/read_body.php?mailbox=' . urlencode($mailbox) .
+  $ViewMessageLink = '<a href="../src/read_body.php?mailbox=' . urlencode($mailbox) .
        '&amp;passed_id=' . $passed_id . '&amp;startMessage=' . $startMessage .
        '&amp;show_more=0">' . _("View message") . '</a>';
-}   
+}
 
 $DownloadLink = '../src/download.php?passed_id=' . $passed_id .
                '&amp;mailbox=' . urlencode($mailbox) . 
                '&amp;passed_ent_id=' . $passed_ent_id . '&amp;absolute_dl=true';
 
-echo '</b></td></tr>' . "\n" .
-    '<tr><td align=center><A HREF="' . $DownloadLink . '">' .
-    _("Download this as a file") .
-    '</A></B><BR>&nbsp;' . "\n" .
-    '</TD></TR></TABLE>' . "\n" .
+echo '<br>' . 
+    html_tag( 'table',
+        html_tag( 'tr',
+        html_tag( 'td',
+                '<b>' .
+                _("Viewing an image attachment") . ' - ' .
+                $ViewMessageLink . '</b>' ,
+          'center', $color[0] )
+         ) .
+        html_tag( 'tr',
+        html_tag( 'td',
+                    '<a href="' . $DownloadLink . '">' .
+		    _("Download this as a file") .
+                    '</a><br>&nbsp;' . "\n" ,
+          'center' )
+         ) ,
+     'center', '', 'border="0" width="100%" cellspacing="0" cellpadding="2"' ) .
 
-    '<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2 ALIGN=CENTER>' . "\n" .
-    '<TR><TD BGCOLOR="' . $color[4] . '">' .
-    '<img src="' . $DownloadLink . '">' .
-
-    '</TD></TR></TABLE>' . "\n";
-    '</body></html>' . "\n";
+    html_tag( 'table',
+        html_tag( 'tr',
+        html_tag( 'td',
+                '<img src="' . $DownloadLink . '">'
+        , 'left', $color[4] )
+         )
+   , 'center', '', 'border="0" cellspacing="0" cellpadding="2"' );
 
 ?>
