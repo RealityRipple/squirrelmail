006f9b57518d3ede01ee4d6fc3f12bc89030e3b8
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-07-05 13:50:09 +0000

    removed code and make use of the functions in mailbox_display. There is only one problem, the paginator and sort buttons doesn't work from a search.

--- functions/imap_search.php
+++ functions/imap_search.php
@@ -20,8 +20,9 @@ require_once('../functions/mime.php');
 function sqimap_search($imapConnection, $search_where, $search_what, $mailbox,
                        $color, $search_position = '', $search_all, $count_all) {
 
-    global $msgs, $message_highlight_list, $squirrelmail_language, $languages,
-           $index_order, $pos, $allow_charset_search, $imap_server_type;
+    global $message_highlight_list, $squirrelmail_language, $languages,
+           $index_order, $pos, $allow_charset_search, $uid_support;
+
     $pos = $search_position;
 
     $urlMailbox = urlencode($mailbox);
@@ -32,27 +33,11 @@ function sqimap_search($imapConnection, $search_where, $search_what, $mailbox,
     $search_what  = ereg_replace('[ ]{2,}', ' ', $search_what);
     $multi_search = explode(' ', $search_what);
     $search_string = '';
+    foreach ($multi_search as $multi_search_part) {
+        $search_string .= $search_where . ' {' . strlen($multi_search_part)
+            . "}\r\n" . $multi_search_part . ' ';
+    }
 
-    /* it seems macosx does not support the prefered search 
-       syntax so we fall back to the older style. This IMAP
-       server has a problem with multiple search terms. Instead
-       of returning the messages that match all the terms it
-       returns the messages that match each term. Could be fixed
-       on the client side, but should be fixed on the server
-       as per the RFC */
-       
-    if ($imap_server_type == 'macosx') {
-        foreach ($multi_search as $multi_search_part) {
-            $search_string .= $search_where . ' ' .$multi_search_part. ' ';
-        }
-    }
-    else {
-        foreach ($multi_search as $multi_search_part) {
-            $search_string .= $search_where . ' {' . strlen($multi_search_part)
-                . "}\r\n" . $multi_search_part . ' ';
-        }
-    }
-    
     $search_string = trim($search_string);
 
     /* now use $search_string in the imap search */
@@ -66,7 +51,7 @@ function sqimap_search($imapConnection, $search_where, $search_what, $mailbox,
     }
 
     /* read data back from IMAP */
-    $readin = sqimap_run_command($imapConnection, $ss, false, $result, $message);
+    $readin = sqimap_run_command($imapConnection, $ss, false, $result, $message, $uid_support);
 
     /* try US-ASCII charset if search fails */
     if (isset($languages[$squirrelmail_language]['CHARSET']) 
@@ -77,13 +62,12 @@ function sqimap_search($imapConnection, $search_where, $search_what, $mailbox,
     }
 
     unset($messagelist);
-    $msgs = '';
 
     /* Keep going till we find the SEARCH response */
     foreach ($readin as $readin_part) {
         /* Check to see if a SEARCH response was received */
         if (substr($readin_part, 0, 9) == '* SEARCH ') {
-            $messagelist = explode(' ', substr($readin_part, 9));
+            $messagelist = preg_split("/ /", substr($readin_part, 9));
         } else if (isset($errors)) {
             $errors = $errors.$readin_part;
         } else {
@@ -102,132 +86,20 @@ function sqimap_search($imapConnection, $search_where, $search_what, $mailbox,
         echo "<!-- $errors -->";
     }
 
-    /*
-     * HACKED CODE FROM ANOTHER FUNCTION, could probably dump this and modify
-     * existing code with a search true/false variable.
-     */
 
     global $sent_folder;
-    for ($q = 0; $q < count($messagelist); $q++) {
+
+    $cnt = count($messagelist);
+    for ($q = 0; $q < $cnt; $q++) {
         $id[$q] = trim($messagelist[$q]);
     }
     $issent = ($mailbox == $sent_folder);
-    $hdr_list = sqimap_get_small_header_list($imapConnection, $id, $issent);
-//    $flags = sqimap_get_flags_list($imapConnection, $id, $issent);
 
-    foreach ($hdr_list as $hdr) {
-        $from[] = $hdr->from;
-        $date[] = $hdr->date;
-        $subject[] = $hdr->subject;
-        $to[] = $hdr->to;
-        $priority[] = $hdr->priority;
-        $cc[] = $hdr->cc;
-        $size[] = $hdr->size;
-        $type[] = $hdr->type0;
-	$flag_deleted[] = $hdr->flag_deleted;
-	$flag_answered[] = $hdr->flag_answered;
-	$flag_seen[] = $hdr->flag_seen;
-	$flag_flagged[] = $hdr->flag_flagged;
-    }
+    $msgs = fillMessageArray($imapConnection,$id,$issent,$cnt);
 
-    $j = 0;
-    while ($j < count($messagelist)) {
-            $date[$j] = str_replace('  ', ' ', $date[$j]);
-            $tmpdate = explode(' ', trim($date[$j]));
-
-            $messages[$j]["TIME_STAMP"] = getTimeStamp($tmpdate);
-            $messages[$j]["DATE_STRING"] = getDateString($messages[$j]["TIME_STAMP"]);
-            $messages[$j]["ID"] = $id[$j];
-            $messages[$j]["FROM"] = decodeHeader($from[$j]);
-            $messages[$j]["FROM-SORT"] = strtolower(sqimap_find_displayable_name(decodeHeader($from[$j])));
-            $messages[$j]["SUBJECT"] = decodeHeader($subject[$j]);
-            $messages[$j]["SUBJECT-SORT"] = strtolower(decodeHeader($subject[$j]));
-            $messages[$j]["TO"] = decodeHeader($to[$j]);
-            $messages[$j]["PRIORITY"] = $priority[$j];
-            $messages[$j]["CC"] = $cc[$j];
-            $messages[$j]["SIZE"] = $size[$j];
-            $messages[$j]["TYPE0"] = $type[$j];
-    	    $messages[$j]['FLAG_DELETED'] = $flag_deleted[$j];
-            $messages[$j]['FLAG_ANSWERED'] = $flag_answered[$j];
-            $messages[$j]['FLAG_SEEN'] = $flag_seen[$j];
-            $messages[$j]['FLAG_FLAGGED'] = $flag_flagged[$j];
-/*
-            $num = 0;
-            while ($num < count($flags[$j])) {
-                if ($flags[$j][$num] == 'Deleted') {
-                    $messages[$j]['FLAG_DELETED'] = true;
-                } else if ($flags[$j][$num] == 'Answered') {
-                    $messages[$j]['FLAG_ANSWERED'] = true;
-                } else if ($flags[$j][$num] == 'Seen') {
-                    $messages[$j]['FLAG_SEEN'] = true;
-                } else if ($flags[$j][$num] == 'Flagged') {
-                    $messages[$j]['FLAG_FLAGGED'] = true;
-                }
-                $num++;
-            }
-*/	    
-            $j++;
-
-    }
-
-    /* Find and remove the ones that are deleted */
-    $i = 0;
-    $j = 0;
-    while ($j < count($messagelist)) {
-        if (isset($messages[$j]['FLAG_DELETED']) && $messages[$j]['FLAG_DELETED']) {
-            $j++;
-            continue;
-        }
-        $msgs[$i] = $messages[$j];
-
-        $i++;
-        $j++;
-    }
-    $numMessages = $i;
-
-    /* There's gotta be messages in the array for it to sort them. */
-
-    if (count($messagelist) > 0) {
-        $j=0;
-        if (!isset ($msg)) {
-            $msg = '';
-        }
-        if ($search_all != 'all') {
-            if ( !isset( $start_msg ) ) {
-                $start_msg =0;
-            }
-            if ( !isset( $sort ) ) {
-                    $sort = 0;
-            }
-            mail_message_listing_beginning( $imapConnection,
-                "move_messages.php?msg=$msg&mailbox=$urlMailbox&pos=$pos&where=" . urlencode($search_where) . "&what=".urlencode($search_what),
-                $mailbox,
-                -1,
-                '<b>' . _("Found") . ' ' . count($messagelist) . ' ' . _("messages") . '</b></tr><tr>'.
-                get_selectall_link($start_msg, $sort));
-        }
-        else {
-            mail_message_listing_beginning( $imapConnection,
-                "move_messages.php?msg=$msg&mailbox=$urlMailbox&pos=$pos&where=" . urlencode($search_where) . "&what=".urlencode($search_what),
-                $mailbox,
-                -1,
-                '<b>' . _("Found") . ' ' . count($messagelist) . ' ' . _("messages") . '</b></tr><tr>');
-        }
-        if ( $mailbox == 'INBOX' ) {
-            $showbox = _("INBOX");
-        } else {
-            $showbox = $mailbox;
-        }
-        echo '<b><big>' . _("Folder:") . " $showbox</big></b>";
-        while ($j < count($msgs)) {
-            printMessageInfo($imapConnection, $msgs[$j]["ID"], 0, $j, $mailbox, '', 0, $search_where, $search_what);
-            $j++;
-            echo '</td></tr>';
-        }
-        echo '</table></td></tr></table></form>';
-        $count_all = count($msgs);
-    }
-    return $count_all;
+    return $msgs;
 }
+
+
 
 ?>
