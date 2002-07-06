94d439d4a5570c388a54c09d6db387e8b6df2e8b
indiri69 <indiri69@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-07-06 21:06:49 +0000

    Code cleanup and removed annoying 'sort' stuff that was showing up at the top of messages.

--- trunk/squirrelmail/functions/html.php
+++ trunk/squirrelmail/functions/html.php
@@ -18,152 +18,151 @@
                        $align = '',         // Alignment
                        $bgcolor = '',       // Back color
                        $xtra = '' ) {       // Extra options
-			 
-    	GLOBAL $languages, $squirrelmail_language;
-    	
-    	$align = strtolower( $align );
-    	$bgc = '';
-    	$tag = strtoupper( $tag );
-    	
-    	if ( isset( $languages[$squirrelmail_language]['DIR']) ) {
-    	    $dir = $languages[$squirrelmail_language]['DIR'];
-    	} else {
-    	    $dir = 'ltr';
-    	}
 
-    	if ( $dir == 'ltr' ) {
-    	    $rgt = 'right';
-    	    $lft = 'left';		
-    	} else {
-    	    $rgt = 'left';
-    	    $lft = 'right';	
-    	}
-    	
-    	if ( $bgcolor <> '' ) {
-    	    $bgc = " BGCOLOR=\"$bgcolor\""; 
-    	}
+        GLOBAL $languages, $squirrelmail_language;
 
-    	switch ( $align ) {
-    	case '':
-    	    $alg = '';
-    	    break;
-    	case 'right':
-    	    $alg = " ALIGN=\"$rgt\"";
-    	    break;
-        case 'left':    	    
-            $alg = " ALIGN=\"$lft\"";
-            break;
-    	default:
-    	    $alg = " ALIGN=\"$align\"";
-    	    break;
-    	}
-	
-	$ret = "<$tag";
-	    
-	if ( $dir <> 'ltr' ) {
-	    $ret .= " DIR=\"$dir\"";
-	}
-	$ret .= "$bgc$alg";
-	    
-	if ( $xtra <> '' ) {
-	    $ret .= " $xtra";
-	}
-	$ret .= '>';
-	    
-	if ( $val <> '' ) {
-	    $ret .= "$val</$tag>";
-	}
-	
+        $align = strtolower( $align );
+        $bgc = '';
+        $tag = strtoupper( $tag );
+
+        if ( isset( $languages[$squirrelmail_language]['DIR']) ) {
+        $dir = $languages[$squirrelmail_language]['DIR'];
+        } else {
+            $dir = 'ltr';
+        }
+
+        if ( $dir == 'ltr' ) {
+            $rgt = 'right';
+            $lft = 'left';
+        } else {
+            $rgt = 'left';
+            $lft = 'right';
+        }
+
+        if ( $bgcolor <> '' ) {
+            $bgc = " BGCOLOR=\"$bgcolor\""; 
+        }
+
+        switch ( $align ) {
+            case '':
+                $alg = '';
+                break;
+            case 'right':
+                $alg = " ALIGN=\"$rgt\"";
+                break;
+            case 'left':
+                $alg = " ALIGN=\"$lft\"";
+                break;
+            default:
+                $alg = " ALIGN=\"$align\"";
+                break;
+        }
+
+        $ret = "<$tag";
+
+        if ( $dir <> 'ltr' ) {
+            $ret .= " DIR=\"$dir\"";
+        }
+        $ret .= "$bgc$alg";
+
+        if ( $xtra <> '' ) {
+            $ret .= " $xtra";
+        }
+        $ret .= '>';
+
+        if ( $val <> '' ) {
+            $ret .= "$val</$tag>";
+        }
+
         return( $ret );
     }
 
     /* handy function to set url vars */
     /* especially usefull when $url = $PHP_SELF */
     function set_url_var($url, $var, $val=0) {
-      $k = '';
-      $ret = '';
-      $url = trim(preg_replace('/&amp;/','&',$url));
+        $k = '';
+        $ret = '';
+        $url = trim(preg_replace('/&amp;/','&',$url));
 
-      $pat_a = array (
+        $pat_a = array (
                        '/.+(\\&'.$var.')=(.*)\\&/AU',   /* in the middle */
-		       '/.+\\?('.$var.')=(.*\\&).+/AU', /* at front, more follow */
-		       '/.+(\\?'.$var.')=(.*)$/AU',     /* at front and only var */
-		       '/.+(\\&'.$var.')=(.*)$/AU'      /* at the end */
-		     );
-      switch (true) {
-        case (preg_match($pat_a[0],$url,$regs)):
-          $k = $regs[1];
-          $v = $regs[2];
-	  break;
-        case (preg_match($pat_a[1],$url,$regs)):
-          $k = $regs[1];
-          $v = $regs[2];
-	  break;
-	case (preg_match($pat_a[2],$url,$regs)): 
-          $k = $regs[1];
-          $v = $regs[2];
-	  break;
-        case (preg_match($pat_a[3],$url,$regs)):
-          $k = $regs[1];
-          $v = $regs[2];
-	  break;
-	default:
-          if ($val) {
-            if (strpos($url,'?')) {
-           
-    $url .= "&$var=$val";
-	    } else {
-           
-   $url .= "?$var=$val";
-	    }   
-          }
-	  break;
-      }
-
-      if ($k) {
-        if ($val) {
-           $pat = "/$k=$v/";
-	   $rpl = "$k=$val";
-	   echo "<B>$pat , $rpl </b><BR>";
-           $url = preg_replace($pat,$rpl,$url);
-        } else {
-           $pat = "/$k=$v/";
-           $url = preg_replace($pat,'',$url);
+                       '/.+\\?('.$var.')=(.*\\&).+/AU', /* at front, more follow */
+                       '/.+(\\?'.$var.')=(.*)$/AU',     /* at front and only var */
+                       '/.+(\\&'.$var.')=(.*)$/AU'      /* at the end */
+                     );
+        switch (true) {
+            case (preg_match($pat_a[0],$url,$regs)):
+                $k = $regs[1];
+                $v = $regs[2];
+                break;
+            case (preg_match($pat_a[1],$url,$regs)):
+                $k = $regs[1];
+                $v = $regs[2];
+                break;
+            case (preg_match($pat_a[2],$url,$regs)):
+                $k = $regs[1];
+                $v = $regs[2];
+                break;
+            case (preg_match($pat_a[3],$url,$regs)):
+                $k = $regs[1];
+                $v = $regs[2];
+                break;
+            default:
+                if ($val) {
+                    if (strpos($url,'?')) {
+                        $url .= "&$var=$val";
+                    } else {
+                        $url .= "?$var=$val";
+                    }
+                }
+                break;
         }
-      }	
-      return  preg_replace('/&/','&amp;',$url);
-    } 
 
-    /* Temporary test function to proces template vars with formatting.
-     * I use it for viewing the message_header (view_header.php) with 
-     * a sort of template.
-     */ 
-    function echo_template_var($var, $format_ar = array() ) {
-      $frm_last = count($format_ar) -1;
-
-      if (isset($format_ar[0])) echo $format_ar[0]; 
-         $i = 1;
- 
-      switch (true) {
-        case (is_string($var)):
-          echo $var;
-          break;
-        case (is_array($var)):
-          $frm_a = array_slice($format_ar,1,$frm_last-1);
-          foreach ($var as $a_el) {
-             if (is_array($a_el)) {
-                echo_template_var($a_el,$frm_a);
-             } else {
-	        echo $a_el;
-                if (isset($format_ar[$i])) echo $format_ar[$i];
-                  $i++;
-             }
-          }
-          break;
-        default: 
-          break;
-      }
-      if (isset($format_ar[$frm_last]) && $frm_last>$i ) echo $format_ar[$frm_last];
+        if ($k) {
+            if ($val) {
+                $rpl = "$k=$val";
+            } else {
+                $rpl = '';
+            }
+            $pat = "/$k=$v/";
+            $url = preg_replace($pat,$rpl,$url);
+        }
+        return  preg_replace('/&/','&amp;',$url);
     }
 
+    /* Temporary test function to proces template vars with formatting.
+     * I use it for viewing the message_header (view_header.php) with
+     * a sort of template.
+     */
+    function echo_template_var($var, $format_ar = array() ) {
+        $frm_last = count($format_ar) -1;
+
+        if (isset($format_ar[0])) echo $format_ar[0]; 
+            $i = 1;
+
+        switch (true) {
+            case (is_string($var)):
+                echo $var;
+                break;
+            case (is_array($var)):
+                $frm_a = array_slice($format_ar,1,$frm_last-1);
+                foreach ($var as $a_el) {
+                    if (is_array($a_el)) {
+                        echo_template_var($a_el,$frm_a);
+                    } else {
+                        echo $a_el;
+                        if (isset($format_ar[$i])) {
+                            echo $format_ar[$i];
+                        }
+                        $i++;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        if (isset($format_ar[$frm_last]) && $frm_last>$i ) {
+            echo $format_ar[$frm_last];
+        }
+    }
 ?>
