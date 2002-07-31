f7313ca276e0880e2099511ccc27cc0801b34e80
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-07-31 21:48:15 +0000

    second try :-(

--- trunk/squirrelmail/src/login.php
+++ trunk/squirrelmail/src/login.php
@@ -11,24 +11,14 @@
  *
  * $Id$
  */
+
 require_once('../functions/strings.php');
 require_once('../config/config.php');
 require_once('../functions/i18n.php');
 require_once('../functions/plugin.php');
 require_once('../functions/constants.php');
 require_once('../functions/page_header.php');
-//require_once('../class/browser.class.php');
-
-// initialize some vars
-if(!isset($UA)) $UA = '';
-if(!isset($cc)) $cc = '';
-if(!isset($dl)) $dl = '';
-if(!isset($am)) $am = '';
-
-//$sniffer_settings = array('check_cookies'=>$cc,
-//                          'default_language'=>$dl,
-//                          'allow_masquerading'=>$am);
-//$browser = new phpSniff($UA,$sniffer_settings);
+require_once('../functions/html.php');
 
 /*
  * $squirrelmail_language is set by a cookie when the user selects
@@ -44,8 +34,7 @@ if (!function_exists('sqm_baseuri')){
 }
 $base_uri = sqm_baseuri();
 @session_destroy();
-session_start();
-//session_register('browser');
+
 /*
  * In case the last session was not terminated properly, make sure
  * we get a new one.
@@ -61,7 +50,7 @@ do_hook('login_cookie');
 
 /* Output the javascript onload function. */
 
-$header = "<SCRIPT LANGUAGE=\"JavaScript\" TYPE=\"text/javascript\">\n" .
+$header = "<SCRIPT LANGUAGE=\"JavaScript\" type=\"text/javascript\">\n" .
           "<!--\n".
           "  function squirrelmail_loginpage_onload() {\n".
           "    document.forms[0].js_autodetect_results.value = '" . SMPREF_JS_ON . "';\n".
@@ -73,8 +62,7 @@ $custom_css = 'none';
 displayHtmlHeader( "$org_name - " . _("Login"), $header, FALSE );
 
 /* Set the title of this page. */
-echo '<BODY TEXT="#000000" BGCOLOR="#FFFFFF" LINK="#0000CC" VLINK="#0000CC" ALINK="#0000CC" onLoad="squirrelmail_loginpage_onload();">'.
-     "\n<FORM ACTION=\"redirect.php\" METHOD=\"POST\">\n";
+echo '<body text="#000000" bgcolor="#FFFFFF" link="#0000CC" vlink="#0000CC" alink="#0000CC" onLoad="squirrelmail_loginpage_onload();">';
 
 $username_form_name = 'login_username';
 $password_form_name = 'secretkey';
@@ -84,52 +72,68 @@ $loginname_value = (isset($loginname) ? htmlspecialchars($loginname) : '');
 
 /* Display width and height like good little people */
 $width_and_height = '';
-if (isset($org_logo_width) && is_int($org_logo_width) && $org_logo_width>0) {
-    $width_and_height = " WIDTH=\"$org_logo_width\"";
+if (isset($org_logo_width) && is_numeric($org_logo_width) && $org_logo_width>0) {
+    $width_and_height = " width=\"$org_logo_width\"";
 }
-if (isset($org_logo_height) && is_int($org_logo_height) && $org_logo_height>0) {
-    $width_and_height .= " HEIGHT=\"$org_logo_height\"";
+if (isset($org_logo_height) && is_numeric($org_logo_height) && $org_logo_height>0) {
+    $width_and_height .= " height=\"$org_logo_height\"";
 }
 
-echo '<CENTER>'.
-     "  <IMG SRC=\"$org_logo\" ALT=\"" . sprintf(_("%s Logo"), $org_name) . 
-        "\"$width_and_height><BR>\n".
-     ( $hide_sm_attributions ? '' :
-       '<SMALL>' . sprintf (_("SquirrelMail version %s"), $version) . "<BR>\n".
-       '  ' . _("By the SquirrelMail Development Team") . "<BR></SMALL>\n" ) .
-     "</CENTER>\n".
-
-     "<CENTER>\n".
-     "<TABLE COLS=\"1\" WIDTH=\"350\">\n".
-     "   <TR><TD ALIGN=CENTER BGCOLOR=\"#DCDCDC\">\n".
-     '      <B>' . sprintf (_("%s Login"), $org_name) . "</B>\n".
-     "   </TD></TR>".
-     "   <TR><TD BGCOLOR=\"#FFFFFF\"><TABLE COLS=2 WIDTH=\"100%\">\n".
-     "      <TR>\n".
-     '         <TD WIDTH="30%" ALIGN=right>' . _("Name:") . "</TD>\n".
-     "         <TD WIDTH=\"*\" ALIGN=left>\n".
-     "            <INPUT TYPE=TEXT NAME=\"$username_form_name\" VALUE=\"$loginname_value\">\n".
-     "         </TD>\n".
-     "      </TR>\n".
-     "      <TR>\n".
-     '         <TD WIDTH="30%" ALIGN=right>' . _("Password:") . "</TD>\n".
-     "         <TD WIDTH=\"*\" ALIGN=left>\n".
-     "            <INPUT TYPE=PASSWORD NAME=\"$password_form_name\">\n".
-     "            <INPUT TYPE=HIDDEN NAME=\"js_autodetect_results\" VALUE=\"" . SMPREF_JS_OFF . "\">\n".
-     "            <INPUT TYPE=HIDDEN NAME=\"just_logged_in\" value=1>\n".
-     "         </TD>\n".
-     "      </TR>\n".
-     "   </TABLE></TD></TR>\n".
-     "   <TR><TD>\n".
-     '      <CENTER><INPUT TYPE=SUBMIT VALUE="' . _("Login") . "\"></CENTER>\n".
-     "   </TD></TR>\n".
-     "</TABLE>\n".
-     "</CENTER>\n";
+echo "\n" . '<form action="redirect.php" method="post">' . "\n" .
+html_tag( 'table',
+    html_tag( 'tr',
+        html_tag( 'td',
+            '<center>'.
+            '<img src="' . $org_logo . '" alt="' . sprintf(_("%s Logo"), $org_name) .'"' .
+            $width_and_height .'><br>' . "\n".
+            ( $hide_sm_attributions ? '' :
+            '<small>' . sprintf (_("SquirrelMail version %s"), $version) . '<br>' ."\n".
+            '  ' . _("By the SquirrelMail Development Team") . '<br></small>' . "\n" ) .
+            html_tag( 'table',
+                html_tag( 'tr',
+                    html_tag( 'td',
+                        '<b>' . sprintf (_("%s Login"), $org_name) . "</b>\n",
+                    'center', '#DCDCDC' )
+                ) .
+                html_tag( 'tr',
+                    html_tag( 'td',  "\n" .
+                        html_tag( 'table',
+                            html_tag( 'tr',
+                                html_tag( 'td',
+                                    _("Name:") ,
+                                'right', '', 'width="30%"' ) .
+                                html_tag( 'td',
+                                    '<input type="text" name="' . $username_form_name .'" value="' . $loginname_value .'">' ,
+                                'left', '', 'width="*"' )
+                                ) . "\n" .
+                            html_tag( 'tr',
+                                html_tag( 'td',
+                                    _("Password:") ,
+                                'right', '', 'width="30%"' ) .
+                                html_tag( 'td',
+                                    '<input type="password" name="' . $password_form_name . '">' . "\n" .
+                                    '<input type=hidden name="js_autodetect_results" value="SMPREF_JS_OFF">' . "\n" .
+                                    '<input type=hidden name="just_logged_in" value=1>' . "\n" .
+                                    $rcptaddress_input . "\n" ,
+                                'left', '', 'width="*"' )
+                            ) ,
+                        'center', '#ffffff', 'border="0" cols="2" width="100%"' ) ,
+                    'left', '#FFFFFF' )
+                ) . 
+                html_tag( 'tr',
+                    html_tag( 'td',
+                        '<center><input type="submit" value="' . _("Login") . '"></center>',
+                    'left' )
+                ),
+            '', '#ffffff', 'border="0" cols="1" width="350"' ),
+        'center' )
+    ) ,
+'', '#ffffff', 'border="0" cellspacing="0" cellpadding="0" width="100%"' ) .
+'</form>' . "\n";
 
 do_hook('login_form');
-echo "</FORM>\n";
 
 do_hook('login_bottom');
-echo "</BODY>\n".
-     "</HTML>\n";
+echo "</body>\n".
+     "</html>\n";
 ?>
