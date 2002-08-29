2692b136f39a33923d870a4846b5ba0e25b7ca6b
thomppj <captbunzo@squirrelmail.org>
2002-08-29 23:45:08 +0000

    Ok. One simple mistake... :)

--- class/mime.class.php
+++ class/mime.class.php
@@ -15,7 +15,7 @@ require_once('../class/mime/Rfc822Header.class.php');
 require_once('../class/mime/MessageHeader.class.php');
 require_once('../class/mime/AddressStructure.class.php');
 require_once('../class/mime/Message.class.php');
-require_once('../class/mime/SmimeMessage.class.php');
+require_once('../class/mime/SMimeMessage.class.php');
 require_once('../class/mime/Disposition.class.php');
 require_once('../class/mime/Language.class.php');
 require_once('../class/mime/ContentType.class.php');
--- class/mime/SMimeMessage.class.php
+++ class/mime/SMimeMessage.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * SmimeMessage.class.php
+ * SMimeMessage.class.php
  *
  * Copyright (c) 2002 The SquirrelMail Project Team
  * Licensed under the GNU GPL. For full terms see the file COPYING.
