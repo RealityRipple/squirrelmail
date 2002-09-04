66cb0a4172af1349e0b474413b8307562c4272b3
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-09-04 22:00:49 +0000

    IMAP backend for the deliver class

--- class/deliver/Deliver_IMAP.class.php
+++ class/deliver/Deliver_IMAP.class.php
@@ -4,5 +4,13 @@ require_once('Deliver.class.php');
 
 class Deliver_IMAP extends Deliver {
 
+    function getBcc() {
+       return true;
+    }
+    
+    /* to do: finishing the imap-class so the initStream function can call the 
+       imap-class */
 }
+
+
 ?>
