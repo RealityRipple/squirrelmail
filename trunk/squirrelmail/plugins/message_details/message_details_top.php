4b13ad8f18fa8508a59950d37021b20f011d4d8e
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-09-05 13:44:57 +0000

    Added Message details plugin to CVS

---
+++ trunk/squirrelmail/plugins/message_details/message_details_top.php
@@ -0,0 +1,40 @@
+<?php
+/** Message Source  
+*
+* Plugin to view the RFC822 raw message output and the bodystructure of a message
+*
+* Copyright (c) 2002 Marc Groot Koerkamp, The Netherlands
+* Licensed under the GNU GPL. For full terms see the file COPYING.
+* 
+* $Id$
+**/
+
+    chdir('..');
+    require_once ('../src/validate.php');
+    require_once ('../functions/strings.php');
+    require_once ('../config/config.php');
+
+
+displayHtmlHeader( _("Message details"),
+             "<script language=\"javascript\">\n".
+             "<!--\n".
+             "function printPopup() {\n".
+                "parent.frames[1].focus();\n".
+                "parent.frames[1].print();\n".
+             "}\n".
+             "-->\n".
+             "</script>\n", FALSE );
+
+
+echo "<body text=\"$color[8]\" bgcolor=\"$color[3]\" link=\"$color[7]\" vlink=\"$color[7]\" alink=\"$color[7]\">\n" .
+     '<center><b>'.
+     '<FORM name=rfc822 action="message_details_download.php?savemessage=1&passed_id='."$passed_id".'" METHOD=POST>' .     
+     '<input type="button" value="' . _("Print") . '" onClick="printPopup()">&nbsp&nbsp '.
+     '<input type="button" value="' . _("Close Window") . '" onClick="window.parent.close()">&nbsp&nbsp'.
+     '<input type="submit" value="' . _("Save Message") . '"> '.
+     '<input type="hidden" name="mailbox" value="'. "$mailbox" . '"> ' .
+     '</form>'.
+     '</b>'.
+     '</body>'.
+     "</html>\n";
+?>
---
+++ trunk/squirrelmail/plugins/message_details/setup.php
@@ -0,0 +1,52 @@
+<?php
+/** Message Source  
+*
+* Plugin to view the RFC822 raw message output and the bodystructure of a message
+*
+* Copyright (c) 2002 Marc Groot Koerkamp, The Netherlands
+* Licensed under the GNU GPL. For full terms see the file COPYING.
+* 
+* $Id$
+**/
+
+
+function squirrelmail_plugin_init_message_details()
+{
+  global $squirrelmail_plugin_hooks;
+
+  do_hook("read_body_header_right");
+  $squirrelmail_plugin_hooks['read_body_header_right']['message_details'] = "show_message_details";
+}
+
+function show_message_details() {
+    global $passed_id, $mailbox, $ent_num, $color,
+           $pf_subtle_link,
+           $javascript_on;
+
+    if (strlen(trim($mailbox)) < 1) {
+        $mailbox = 'INBOX';
+    }
+
+    $params = '?passed_ent_id=' . $ent_num .
+              '&mailbox=' . urlencode($mailbox) .
+              '&passed_id=' . $passed_id;
+
+    $print_text = _("View Message details");
+
+
+    $result = '';
+    /* Output the link. */
+    if ($javascript_on) {
+        $result = '<script language="javascript">' . "\n" .
+                '<!--' . "\n" .
+                "  function MessageSource() {\n" .
+                '    window.open("../plugins/message_details/message_details_main.php' .
+                        $params . '","Print","width=800,height=600");' . "\n".
+                "  }\n" .
+                "// -->\n" .
+                "</script>\n" .
+                "&nbsp;|&nbsp;<A HREF=\"javascript:MessageSource();\">$print_text</A>\n";
+    } 
+    echo $result;
+}
+ 
