97bee67c66252794bf44e9181493f3ca75477ba7
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-09-05 13:44:57 +0000

    Added Message details plugin to CVS

---
+++ plugins/message_details/message_details_main.php
@@ -0,0 +1,23 @@
+<?php
+/** Message Source  
+*
+* Plugin to view the RFC822 raw message output and the bodystructure of a message
+*
+* Copyright (c) 2002 Marc Groot Koerkamp, The Netherlands
+* Licensed under the GNU GPL. For full terms see the file COPYING.
+* 
+* $Id$
+**/
+
+chdir ('..');
+require_once('../src/validate.php');
+require_once('../functions/page_header.php');
+require_once('../functions/strings.php');
+
+displayHtmlHeader( _("Message details"), '', FALSE );
+
+echo "<frameset rows=\"60, *\" noresize border=\"0\">\n";
+echo '<frame src="message_details_top.php?mailbox=' . urlencode($mailbox) .'&passed_id=' . "$passed_id". '" name="top_frame" scrolling="off">';
+echo '<frame src="message_details_bottom.php?mailbox=' . urlencode($mailbox) .'&passed_id=' . "$passed_id" . '" name="bottom_frame">';
+echo  '</frameset>'."\n"."</html>\n";
+?>
