318515879a2de52d3ebdcf6e37fc1a2c0d4ba867
indiri69 <sethr@missoulafcu.org>
2002-09-10 22:48:33 +0000

    Message details seems to work without chdir now.

--- plugins/message_details/message_details_main.php
+++ plugins/message_details/message_details_main.php
@@ -10,8 +10,8 @@
  */
 
 /* Path for SquirrelMail required files. */
-chdir('..');
-define('SM_PATH','../');
+#chdir('..');
+define('SM_PATH','../../');
 
 /* SquirrelMail required files. */
 require_once(SM_PATH . 'include/validate.php');
