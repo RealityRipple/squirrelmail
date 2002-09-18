1cd3987e93366899e9c4705a1436f93ab7aee2b4
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-09-18 12:55:09 +0000

    fix undefined var

--- trunk/squirrelmail/class/mime/Rfc822Header.class.php
+++ trunk/squirrelmail/class/mime/Rfc822Header.class.php
@@ -361,7 +361,7 @@ class Rfc822Header {
         $props = '';
         if ($pos > 0) {
             $name = trim(substr($value, 0, $pos));
-            $props = trim(substr($type, $pos+1));
+            $props = trim(substr($value, $pos+1));
         } else {
             $name = $value;
         }
