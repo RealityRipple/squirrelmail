b74a42a918535b36e5c8be7c5e3b2888aec8ff4d
Thijs Kinkhorst <3808792+thijskh@users.noreply.github.com>
2002-09-30 17:34:31 +0000

    Writeout session data to disk before redirecting. Suggestion from Thomas.

--- src/redirect.php
+++ src/redirect.php
@@ -196,7 +196,8 @@ $attachments = unserialize(getPref($data_dir, $username, 'attachments', 0));
        $redirect_url = 'webmail.php';
     }
 
-/* Send them off to the appropriate page. */
+/* Write session data and send them off to the appropriate page. */
+session_write_close();
 header("Location: $redirect_url");
 
 /* --------------------- end main ----------------------- */
