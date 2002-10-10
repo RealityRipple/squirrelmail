f16978dbdf7dd83fc674a4e14e472d082ab2151d
centaurix <sm@sels.com>
2002-10-10 01:34:51 +0000

    just the file to remind me to actually *code* what i post on mailing lists ;)
    will add stuff time by time. feel free to help everybody. (plz email me before coding)

---
+++ class/helper/VCard.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * vCard.class
+ *
+ * Copyright (c) 2002 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * This (will) contain functions needed to vCards.
+ *
+ * $Id$
+ */
+
+
+?>
