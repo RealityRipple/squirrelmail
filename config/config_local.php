c5cd49d830c06779dfcd9ca523b366aaa5a8519f
tassium <tassium@squirrelmail.org>
2002-11-12 16:13:46 +0000

    Config override file, included by config.php.  Placeholder - no overrides by default.

---
+++ config/config_local.php
@@ -0,0 +1,6 @@
+<?
+/* Local config overrides.
+   You can override the config.php settings here.  Don't do it unless you know what you're doing.
+   Use standard PHP syntax, see config.php for examples. */
+
+?>
