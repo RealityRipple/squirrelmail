28849bc4b87a48524ed81273501c7b6a3fd7738a
Thijs Kinkhorst <3808792+thijskh@users.noreply.github.com>
2002-12-24 11:59:08 +0000

    Fix another short open tag

--- config/config_local.php
+++ config/config_local.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /* Local config overrides.
    You can override the config.php settings here.  Don't do it unless you know what you're doing.
    Use standard PHP syntax, see config.php for examples. */
