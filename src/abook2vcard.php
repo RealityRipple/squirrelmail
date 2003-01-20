461d21e68ae5739dd90a537bfbcabd9a98124bf9
centaurix <sm@sels.com>
2003-01-20 06:01:16 +0000

    sleeping sucks anyway. ;) it writes vcards now. not pretty ones (strip whitespace, encoding, etc is still missing). but works for me as inital tool.

--- src/abook2vcard.php
+++ src/abook2vcard.php
@@ -22,28 +22,41 @@ require_once(SM_PATH . 'functions/prefs.php');
 require_once(SM_PATH . 'config/config.php');
 
 $abook_file=$data_dir.$username.".abook";
-$vcard_dir=$data_dir.$username."/";
+$vcard_base=$data_dir.$username;
+$i=0;
 
 $fp = fopen ($abook_file,"r");
 while (!feof ($fp)) {
-    $buffer .= fgets($fp, 4096);
+    $buffer = fgets($fp, 8096);
+   $line=explode("|",$buffer);
+   if (count($line)>1) {
+    write_vcard($line);
+   }
 }
 
 fclose ($fp);
 
-$abook=explode("|",$buffer);
-print_r($abook);
-echo "<p>".$buffer;
 
-while ( list($nick,$email,$fn,$ln,$email)=each($abook) ) {
-echo "
-BEGIN:VCARD
+function write_vcard($abook) {
+global $vcard_base,$i;
+
+
+// FIXME check if filename is ok
+$vcard_fn = $vcard_base.".".$abook[0].".vcard";
+
+$fp0 = fopen ($vcard_fn,"w");
+
+fputs($fp0, "BEGIN:VCARD
 VERSION:3.0
-N:$ln;$fn;
-NICKNAME:$nick
-EMAIL;INTERNET:$email
+N:$abook[1];$abook[2];
+NICKNAME:$abook[0]
+EMAIL;INTERNET:$abook[3];
 END:VCARD<P>
-";
+");
+$i++;
+fclose($fp0);
+
 }
 
+echo "done ;) seem to have written $i files.";
 ?>
