93178ffeac0658a5fa07aed0d290b39ee7a35b0b
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-03-09 18:54:34 +0000

    added explicit include of global.php

--- trunk/squirrelmail/src/view_header.php
+++ trunk/squirrelmail/src/view_header.php
@@ -16,6 +16,7 @@ define('SM_PATH','../');
 
 /* SquirrelMail required files. */
 require_once(SM_PATH . 'include/validate.php');
+require_once(SM_PATH . 'functions/global.php');
 require_once(SM_PATH . 'functions/imap.php');
 require_once(SM_PATH . 'functions/html.php');
 require_once(SM_PATH . 'functions/url_parser.php');
