5b38bd726c37ffa6c14784d85483fb6ad608bc62
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-03-11 09:02:13 +0000

    fwrite returns 0 when 0 bytes written, so use === FALSE test instead. Thanks Jon for the tip.

--- trunk/squirrelmail/functions/file_prefs.php
+++ trunk/squirrelmail/functions/file_prefs.php
@@ -73,8 +73,8 @@ function cachePrefValues($data_dir, $username) {
                 $prefs_cache[$key] = $value;
             }
         }
-     }
-     fclose($file);
+    }
+    fclose($file);
 
     $prefs_are_cached = TRUE;
 
@@ -119,7 +119,7 @@ function savePrefValues($data_dir, $username) {
     }
     foreach ($prefs_cache as $Key => $Value) {
         if (isset($Value)) {
-            if ( ! @fwrite($file, $Key . '=' . $Value . "\n") ) {
+            if ( @fwrite($file, $Key . '=' . $Value . "\n") === FALSE ) {
                logout_error( sprintf( _("Preference file, %s, could not be written. Contact your system administrator to resolve this issue.") , $filename . '.tmp') );
                exit;
             }
@@ -222,7 +222,7 @@ function setSig($data_dir, $username, $number, $value) {
         logout_error( sprintf( _("Signature file, %s, could not be opened. Contact your system administrator to resolve this issue."), $filename . '.tmp') );
         exit;
     }
-    if (! @fwrite($file, $value) ) {
+    if ( @fwrite($file, $value) === FALSE ) {
        logout_error( sprintf( _("Signature file, %s, could not be written. Contact your system administrator to resolve this issue.") , $filename . '.tmp'));
        exit;
     }
