05bdce1640836d9dd563c424508bc22abc0894c1
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-05-22 15:22:07 +0000

    Drop obsoleted imap_parse.php

--- trunk/squirrelmail/functions/imap.php
+++ trunk/squirrelmail/functions/imap.php
@@ -16,6 +16,5 @@ require_once(SM_PATH . 'functions/imap_mailbox.php');
 require_once(SM_PATH . 'functions/imap_messages.php');
 require_once(SM_PATH . 'functions/imap_general.php');
 require_once(SM_PATH . 'functions/imap_search.php');
-require_once(SM_PATH . 'functions/imap_parse.php');
 
 ?>
