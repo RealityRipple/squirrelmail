b6944d336c057d49ac2e45bd6f5544ce328cbe76
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-07-27 11:06:50 +0000

    Added Minimal BW theme. Purpose of theme - colorless environment for browsers that don't support colors and for CGA :) displays if you can find one.

---
+++ trunk/squirrelmail/themes/minimal_bw.php
@@ -0,0 +1,26 @@
+<?php
+   /** Author:       Tomas Kuliavas
+       Date:         July 26, 2003
+       Theme Name:   'Minimal BW Theme'
+    
+    This theme uses black color for text and white for background.
+    **/
+
+    global $color;
+    $color[0]   = '#FFFFFF'; // (white)   TitleBar
+    $color[1]   = '#000000'; // (black)   (unused)
+    $color[2]   = '#000000'; // (black)   Warning/Error Messages
+    $color[3]   = '#FFFFFF'; // (white)   Left Bar Background
+    $color[4]   = '#FFFFFF'; // (white)   Normal Background
+    $color[5]   = '#FFFFFF'; // (white)   Table Headers
+    $color[6]   = '#000000'; // (black)   Text on left bar
+    $color[7]   = '#000000'; // (black)   Links
+    $color[8]   = '#000000'; // (black)   Normal text
+    $color[9]   = '#FFFFFF'; // (white)   Darker version of #0
+    $color[10]  = '#FFFFFF'; // (white)   Darker version of #9
+    $color[11]  = '#000000'; // (black)   Special Folders color
+    $color[12]  = '#FFFFFF'; // (white)   Alternate color for message list
+    $color[13]  = '#000000'; // (black)   Color for quoted text -- > 1 quote
+    $color[14]  = '#000000'; // (black)   Color for quoted text -- >> 2 or more
+    $color[15]  = '#000000'; // (black)   Unselectable folders
+?>
