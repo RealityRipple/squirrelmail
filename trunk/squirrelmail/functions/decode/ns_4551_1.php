758231c82487aa98a6a76153b9ca1482ad10741b
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-01-29 19:37:29 +0000

    Adding last two functions from i18n.php iso_8859_default decoding is changed to us_ascii. Purpose - header cleanup, if mailer adds non-encoded 8bit symbols in header. Other iso-8859-x charsets are supported. Original function is shorter, but it is hard to count all ?.
    ns_4551_1 decoding might need some adjustments.

---
+++ trunk/squirrelmail/functions/decode/ns_4551_1.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * functions/decode/ns_4551_1.php
+ *
+ * Copyright (c) 2004 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * This file contains ns_4551-1 decoding function that is needed to read
+ * ns_4551-1 encoded mails in non-ns_4551-1 locale.
+ *
+ * This is the same as ISO-646-NO and is used by some
+ * Microsoft programs when sending Norwegian characters 
+ *
+ * $Id$
+ * @package squirrelmail
+ * @subpackage decode
+ */
+
+/**
+ * ns_4551_1 decoding function
+ *
+ * @param string $string
+ * @return string 
+ */
+function charset_decode_ns_4551_1 ($string) {
+    /*
+     * These characters are:
+     * Latin capital letter AE
+     * Latin capital letter O with stroke
+     * Latin capital letter A with ring above
+     * and the same as small letters
+     */
+    return strtr ($string, "[\\]{|}", "ÆØÅæøå");
+}
+?>
