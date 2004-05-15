f6b57ee142a64ea52d0b5d60e27ce3b37d11a417
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-05-15 09:56:28 +0000

    Removing HTML from strings and cleaning up code

--- trunk/squirrelmail/plugins/newmail/newmail_opt.php
+++ trunk/squirrelmail/plugins/newmail/newmail_opt.php
@@ -35,94 +35,103 @@ $media = getPref($data_dir,$username,'newmail_media', '(none)');
 // Set $allowsound to false if you don't want sound files available
 $allowsound = "true";
 
-echo html_tag( 'table', '', 'center', '', 'width="95%" border="0" cellpadding="1" cellspacing="0"' ) . "\n" .
-         html_tag( 'tr', "\n" .
-                 html_tag( 'td', '<b>' . _("Options") . ' - ' . _("New Mail Notification") . '</b>', 'center', $color[0] )
-            ) . "\n" .
-             html_tag( 'tr' ) . "\n" .
-                 html_tag( 'td', '', 'left' );
-if ($allowsound == "true") {
-     echo html_tag( 'p',
-            _("Select <b>Enable Media Playing</b> to turn on playing a media file when unseen mail is in your folders. When enabled, you can specify the media file to play in the provided file box.")
-          ) . "\n";
-}
-      echo html_tag( 'p',
-             _("The <b>Check all boxes, not just INBOX</b> option will check ALL of your folders for unseen mail, not just the inbox for notification.")
-           ) . "\n" .
-           html_tag( 'p',
-               _("Selecting the <b>Show popup</b> option will enable the showing of a popup window when unseen mail is in your folders (requires JavaScript).")
-           ) . "\n" .
-           html_tag( 'p',
-               _("Use the <b>Check RECENT</b> to only check for messages that are recent. Recent messages are those that have just recently showed up and have not been \"viewed\" or checked yet. This can prevent being continuously annoyed by sounds or popups for unseen mail.")
-           ) . "\n" .
-           html_tag( 'p',
-               _("Selecting the <b>Change title</b> option will change the title in some browsers to let you know when you have new mail (requires JavaScript, and only works in IE but you won't see errors with other browsers). This will always tell you if you have new mail, even if you have <b>Check RECENT</b> enabled.")
-           );
-    if ($allowsound == "true") {
-        echo html_tag( 'p',
-                    _("Select from the list of <b>server files</b> the media file to play when new mail arrives. If no file is specified, \"(none)\", no sound will be used.")
-               ) . "\n";
-    }
-    echo '<form action="'.sqm_baseuri().'src/options.php" method=post>'.
-         html_tag( 'table', '', '', '', 'width="100%" cellpadding="0" cellspacing="2" border="0"' ) . "\n" .
-             html_tag( 'tr' ) . "\n" .
-                 html_tag( 'td', '&nbsp;', 'right', '', 'nowrap' ) . "\n";
-    if ($allowsound == "true") {
-                echo html_tag( 'td', '', 'left' ) . 
-                     '<input type="checkbox" ';
-        if ($media_enable == 'on') {
-                echo 'checked ';
-        }
-        echo 'name="media_enable"><b> ' . _("Enable Media Playing") . '</b></td>'.
-                '</tr>' . "\n";
-    }  
-            echo html_tag( 'tr' ) . "\n" .
-                html_tag( 'td', '&nbsp;', 'right', '', 'nowrap' ) . "\n" .
-                html_tag( 'td', '', 'left' ) .
-                    '<input type="checkbox" ';
-    if ($media_allbox == 'on') {
-              echo 'checked ';
-    }
-    echo 'name="media_allbox"><b> ' . _("Check all boxes, not just INBOX") . '</b></td>'.
-            '</tr>'.  "\n" .
-            html_tag( 'tr' ) . "\n" .
-                html_tag( 'td', '&nbsp;', 'right', '', 'nowrap' ) . "\n" .
-                html_tag( 'td', '', 'left' ) .
-                    '<input type="checkbox" ';
-    if ($media_recent == 'on') {
-              echo 'checked ';
-    }
-    echo 'name="media_recent"><b> ' . _("Count only messages that are RECENT") . '</b></td>'.
-            '</tr>'. "\n" .
-            html_tag( 'tr' ) . "\n" .
-                html_tag( 'td', '&nbsp;', 'right', '', 'nowrap' ) . "\n" .
-                html_tag( 'td', '', 'left' ) .
-                    '<input type="checkbox" ';
-    if ($media_changetitle == 'on') {
-              echo 'checked ';
-    }
-    echo 'name="media_changetitle"><b> ' . _("Change title on supported browsers.") . '</b> &nbsp; (' . _("requires JavaScript to work") . ')</td>'.
-            '</tr>'. "\n" .
-            html_tag( 'tr' ) . "\n" .
-                html_tag( 'td', '&nbsp;', 'right', '', 'nowrap' ) . "\n" .
-                html_tag( 'td', '', 'left' ) .
-                    '<input type="checkbox" ';
-    if($media_popup == 'on') {
-              echo 'checked ';
-    }
-    echo 'name="media_popup"><b> ' . _("Show popup window on new mail") . '</b> &nbsp; (' . _("requires JavaScript to work") . ')</td>'.
-            '</tr>' . "\n";
-    if ($allowsound == "true") {
-            echo html_tag( 'tr' ) . "\n" .
-                        html_tag( 'td', _("Select server file:"), 'right', '', 'nowrap' ) . "\n" .
-                        html_tag( 'td', '', 'left' ) .
-                            '<select name="media_sel">'.  "\n".
-                            '<option value="(none)"';
-            if ( $media == '(none)') {
-                echo 'selected ';
-            }
-            echo '>' . _("(none)") . '</option>' .  "\n";
+echo html_tag( 'table', '', 'center', $color[0], 'width="95%" cellpadding="1" cellspacing="0" border="0"' ) . "\n" .
+        html_tag( 'tr' ) . "\n" .
+            html_tag( 'td', '', 'center' ) .
+                '<b>' . _("Options") . ' - ' . _("New Mail Notification") . "</b><br />\n" .
+                html_tag( 'table', '', '', '', 'width="100%" cellpadding="5" cellspacing="0" border="0"' ) . "\n" .
+                    html_tag( 'tr' ) . "\n" .
+                        html_tag( 'td', '', 'left', $color[4] ) . "<br />\n";
 
+echo html_tag( 'p',
+        sprintf(_("The %s option will check ALL of your folders for unseen mail, not just the inbox for notification."), '&quot;'._("Check all boxes, not just INBOX").'&quot;')
+     ) . "\n" .
+     html_tag( 'p',
+        sprintf(_("Selecting the %s option will enable the showing of a popup window when unseen mail is in your folders (requires JavaScript)."), '&quot;'._("Show popup window on new mail").'&quot;')
+     ) . "\n" .
+     html_tag( 'p',
+        sprintf(_("Use the %s option to only check for messages that are recent. Recent messages are those that have just recently showed up and have not been \"viewed\" or checked yet. This can prevent being continuously annoyed by sounds or popups for unseen mail."), '&quot;'._("Count only messages that are RECENT").'&quot;')
+     ) . "\n" .
+     html_tag( 'p',
+        sprintf(_("Selecting the %s option will change the title in some browsers to let you know when you have new mail (requires JavaScript, and only works in IE but you won't see errors with other browsers). This will always tell you if you have new mail, even if you have %s enabled."), 'quot;'._("Change title on supported browsers").'&quot;', '&quot;'._("Count only messages that are RECENT").'&quot;')
+     ) . "\n";
+if ($allowsound == "true") {
+    echo html_tag( 'p',
+            sprintf(_("Select %s to turn on playing a media file when unseen mail is in your folders. When enabled, you can specify the media file to play in the provided file box."), '&quot;'._("Enable Media Playing").'&quot;')
+         ) . "\n" .
+         html_tag( 'p',
+            sprintf(_("Select from the list of %s the media file to play when new mail arrives. If no file is specified, %s, no sound will be used."), '&quot;'._("Select server file").'&quot;', '&quot;'._("(none)").'&quot;')
+         ) . "\n";
+}
+
+echo '</td></tr>' .
+        html_tag( 'tr' ) .
+            html_tag( 'td', '', 'center', $color[4] ) . "\n" . '<hr style="width: 25%; height: 1px;" />' . "\n";
+
+echo '<form action="'.sqm_baseuri().'src/options.php" method="post">' . "\n" .
+        html_tag( 'table', '', '', '', 'width="100%" cellpadding="5" cellspacing="0" border="0"' ) . "\n";
+
+// Option: media_allbox
+echo html_tag( 'tr' ) .
+        html_tag( 'td', _("Check all boxes, not just INBOX").':', 'right', '', 'nowrap' ) .
+            html_tag( 'td', '', 'left' ) .
+                '<input type="checkbox" ';
+if ($media_allbox == 'on') {
+    echo 'checked="checked" ';
+}
+echo 'name="media_allbox" /></td></tr>' . "\n";
+
+// Option: media_recent
+echo html_tag( 'tr' ) .
+        html_tag( 'td', _("Count only messages that are RECENT").':', 'right', '', 'nowrap' ) .
+            html_tag( 'td', '', 'left' ) .
+                '<input type="checkbox" ';
+if ($media_recent == 'on') {
+    echo 'checked="checked" ';
+}
+echo 'name="media_recent" /></td></tr>' . "\n";
+
+// Option: media_changetitle
+echo html_tag( 'tr' ) .
+        html_tag( 'td', _("Change title on supported browsers").':', 'right', '', 'nowrap' ) .
+            html_tag( 'td', '', 'left' ) .
+                '<input type="checkbox" ';
+if ($media_changetitle == 'on') {
+    echo 'checked="checked" ';
+}
+echo 'name="media_changetitle" />&nbsp;('._("requires JavaScript to work").')</td></tr>' . "\n";
+
+// Option: media_popup
+echo html_tag( 'tr' ) .
+        html_tag( 'td', _("Show popup window on new mail").':', 'right', '', 'nowrap' ) .
+            html_tag( 'td', '', 'left' ) .
+                '<input type="checkbox" ';
+if($media_popup == 'on') {
+    echo 'checked="checked" ';
+}
+echo 'name="media_popup" />&nbsp;('._("requires JavaScript to work").')</td></tr>' . "\n";
+
+if ($allowsound == "true") {
+// Option: media_enable
+    echo html_tag( 'tr' ) .
+            html_tag( 'td', _("Enable Media Playing").':', 'right', '', 'nowrap' ) .
+                html_tag( 'td', '', 'left' ) .
+                    '<input type="checkbox" ';
+    if ($media_enable == 'on') {
+        echo 'checked="checked" ';
+    }
+    echo 'name="media_enable" /></td></tr>' . "\n";
+
+// Option: media_sel
+    echo html_tag( 'tr' ) .
+        html_tag( 'td', _("Select server file").':', 'right', '', 'nowrap' ) .
+            html_tag( 'td', '', 'left' ) .
+                '<select name="media_sel">' . "\n" .
+                    '<option value="(none)"';
+    if ( $media == '(none)') {
+        echo 'selected="selected" ';
+    }
+    echo '>' . _("(none)") . '</option>' .  "\n";
     // Iterate sound files for options
     $d = dir(SM_PATH . 'plugins/newmail/sounds');
     while($entry=$d->read()) {
@@ -130,38 +139,33 @@ if ($allowsound == "true") {
         if ($entry != '..' && $entry != '.' && $entry != 'CVS') {
             echo '<option ';
             if ($fname == $media) {
-                echo 'selected ';
+                echo 'selected="selected" ';
             }
             echo 'value="' . htmlspecialchars($fname) . '">' .
                 htmlspecialchars($entry) . "</option>\n";
         }
     }
     $d->close();
+    $media_output = ($media == '(none)') ? _("(none)") : substr($media, strrpos($media, '/')+1);
     echo '</select>'.
-               '<input type="submit" value=" ' . _("Try") . ' " name="test" onClick="'.
-                    "window.open('testsound.php?sound='+media_sel.options[media_sel.selectedIndex].value, 'TestSound',".
-                    "'width=150,height=30,scrollbars=no');".
-                    'return false;'.
-               '">'.
-            '</td>'.
-         '</tr>'.
-         html_tag( 'tr', "\n" .
-             html_tag( 'td', _("Current File:"), 'right', '', 'nowrap' ) .
-             html_tag( 'td', '<input type="hidden" value="' . 
-                 htmlspecialchars($media) . '" name="media_default">' .
-                 htmlspecialchars($media) . '', 'left' )
-         ) . "\n";
-    }
-         echo html_tag( 'tr', "\n" .
-                     html_tag( 'td', '&nbsp;' ) .
-                     html_tag( 'td',
-                         '<input type="hidden" name="optmode" value="submit">'. 
-                         '<input type="submit" value="' . _("Submit") . '" name="submit_newmail">',
-                     'left' )
-                 ) . "\n" .
-      '</table>'. "\n" .
-   '</form>'. "\n" .
-   '</td></tr></table>'. "\n" .
-'</body></html>';
-
+        '<input type="submit" value="' . _("Try") . '" name="test" onClick="' .
+            "window.open('testsound.php?sound='+media_sel.options[media_sel.selectedIndex].value, 'TestSound'," .
+            "'width=150,height=30,scrollbars=no');" .
+            'return false;' .
+            ' /"></td></tr>' .
+            html_tag( 'tr', "\n" .
+                html_tag( 'td', _("Current File:"), 'right', '', 'nowrap' ) .
+                    html_tag( 'td', '<input type="hidden" value="' .
+                        htmlspecialchars($media) . '" name="media_default">' .
+                        htmlspecialchars($media_output) . '', 'left' )
+             ) . "\n";
+}
+echo html_tag( 'tr', "\n" .
+    html_tag( 'td', '&nbsp;' ) .
+        html_tag( 'td',
+            '<input type="hidden" name="optmode" value="submit" />' .
+            '<input type="submit" value="' . _("Submit") . '" name="submit_newmail" />',
+        'left' )
+     ) . "\n";
 ?>
+</table></form></td></tr></table></td></tr></table></body></html>
