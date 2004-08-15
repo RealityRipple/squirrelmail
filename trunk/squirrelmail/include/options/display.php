264e464801f4f230f4b7d82838a485eba4a31996
jangliss <jangliss@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-08-15 20:33:47 +0000

    Bad grammar

--- trunk/squirrelmail/include/options/display.php
+++ trunk/squirrelmail/include/options/display.php
@@ -428,7 +428,7 @@ function load_optpage_data_display() {
 
     $optvals[SMOPT_GRP_MESSAGE][] = array(
         'name'    => 'internal_date_sort',
-        'caption' => _("Enable Sort by of Receive Date"),
+        'caption' => _("Sort by Receive Date"),
         'type'    => SMOPT_TYPE_BOOLEAN,
         'refresh' => SMOPT_REFRESH_ALL
     );
