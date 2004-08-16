f1bab4661932f5e2dbd0d9a4b346ef29de8e7e8e
Fredrik Jervfors <jervfors@users.noreply.github.com>
2004-08-16 00:04:29 +0000

    Moving some HTML out of PHP

--- src/printer_friendly_main.php
+++ src/printer_friendly_main.php
@@ -27,14 +27,12 @@ $passed_ent_id = $_GET['passed_ent_id'];
 $passed_id = (int) $_GET['passed_id'];
 $mailbox = $_GET['mailbox'];
 /* end globals */
-
-echo "<frameset rows=\"60, *\" noresize border=\"0\">\n".
-     '<frame src="printer_friendly_top.php" name="top_frame" scrolling="no" />'.
-     '<frame src="printer_friendly_bottom.php?passed_ent_id='.
-     urlencode($passed_ent_id) . '&amp;mailbox=' . urlencode($mailbox) .
-     '&amp;passed_id=' . $passed_id .
-     '" name="bottom_frame" />'.
-     "\n</frameset>\n".
-     "</html>\n";
-
 ?>
+<frameset rows="60, *" noresize="noresize" border="0">
+    <frame src="printer_friendly_top.php" name="top_frame" scrolling="no" />
+    <frame src="printer_friendly_bottom.php?passed_ent_id=<?php
+        echo urlencode($passed_ent_id) . '&amp;mailbox=' . urlencode($mailbox) .
+             '&amp;passed_id=' . $passed_id;
+        ?>" name="bottom_frame" />
+</frameset>
+</html>
