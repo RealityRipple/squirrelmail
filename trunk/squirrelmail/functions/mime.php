5382b963d570a6b05bf1db09f46d3a9d69f0a5fa
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-08-27 11:48:44 +0000

    send urlencoded filename to IE

--- trunk/squirrelmail/functions/mime.php
+++ trunk/squirrelmail/functions/mime.php
@@ -2003,6 +2003,7 @@ function magicHTML($body, $id, $message, $mailbox = 'INBOX') {
      // version
      //set all the Cache Control Headers for IE
      if ($isIE) {
+         $filename=urlencode($filename);
          header ("Pragma: public");
          header ("Cache-Control: no-store, max-age=0, no-cache, must-revalidate"); # HTTP/1.1
          header ("Cache-Control: post-check=0, pre-check=0", false);
