140edba2dc0be0e1a9f769a70236f4fe7765c37e
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-10-01 18:39:39 +0000

    moving functions to separate file, adding configuration files

---
+++ trunk/squirrelmail/plugins/fortune/functions.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Fortune plugin functions
+ *
+ * @copyright (c) 2004 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * @package plugins
+ * @subpackage fortune
+ */
+
+/**
+ * Declare configuration globals
+ */
+global $fortune_location, $fortune_options;
+
+/**
+ * Load default config
+ */
+include_once(SM_PATH . 'plugins/fortune/config_default.php');
+
+/**
+ * Load site config
+ */
+if (file_exists(SM_PATH . 'config/fortune_config.php')) {
+    include_once(SM_PATH . 'config/fortune_config.php');
+} elseif (file_exists(SM_PATH . 'plugins/fortune/config.php')) {
+    include_once(SM_PATH . 'plugins/fortune/config.php');
+}
+
+/**
+ * Show fortune
+ * @access private
+ * @since 1.5.1
+ */
+function fortune_function() {
+    global $fortune_visible, $color, $fortune_location, $fortune_options;
+
+    if (!$fortune_visible) {
+        return;
+    }
+
+    $exist = file_exists($fortune_location);
+
+    if ($fortune_options!='') {
+        $fortune_command=$fortune_location . ' ' . $fortune_options;
+    } else {
+        $fortune_command=$fortune_location;
+    }
+
+    echo "<center><table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" bgcolor=\"$color[10]\">\n".
+        "<tr><td><table width=\"100%\" cellpadding=\"2\" cellspacing=\"1\" border=\"0\" bgcolor=\"$color[5]\">\n".
+        "<tr><td align=\"center\">\n";
+    echo '<table><tr><td>';
+    if (!$exist) {
+        echo sprintf(_("%s is not found."),$fortune_location);
+    } else {
+        echo "<center><em>" . _("Today's Fortune") . "</em></center><pre>\n";
+        htmlspecialchars(system($fortune_command));
+        echo "</pre>\n";
+    }
+
+    echo '</td></tr></table></td></tr></table></td></tr></table></center>';
+}
+
+/**
+ * Add fortune options
+ * @access private
+ * @since 1.5.1
+ */
+function fortune_function_options() {
+    global $optpage_data;
+
+    $optpage_data['grps']['fortune'] = _("Fortunes:");
+    $optionValues = array();
+    $optionValues[] = array('name'    => 'fortune_visible',
+                            'caption' => _("Show fortunes at top of mailbox"),
+                            'type'    => SMOPT_TYPE_BOOLEAN,
+                            'initial_value' => false );
+    $optpage_data['vals']['fortune'] = $optionValues;
+}
+
+/**
+ * Get fortune prefs
+ * @access private
+ * @since 1.5.1
+ */
+function fortune_function_load() {
+    global $username, $data_dir, $fortune_visible;
+
+    $fortune_visible = getPref($data_dir, $username, 'fortune_visible');
+}
+?>
--- trunk/squirrelmail/plugins/fortune/setup.php
+++ trunk/squirrelmail/plugins/fortune/setup.php
@@ -1,100 +1,58 @@
 <?php
-
 /**
- * plugins/fortune/setup.php
- *
- * Original code contributed by paulm@spider.org
- *
- * Simple SquirrelMail WebMail Plugin that displays the output of
- * fortune above the message listing.
+ * Fortune plugin setup script
  *
  * @copyright (c) 1999-2004 The SquirrelMail Project Team
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id$
  * @package plugins
  * @subpackage fortune
- *
- * FIXME
- * There should be no code in setup.php, just hook registrations. Create functions.php
- * and move the code there.
  */
+
+/**
+ * If SM_PATH isn't defined, define it.
+ * @ignore
+ */
+if (!defined('SM_PATH'))  {
+    define('SM_PATH','../../');
+}
 
 /**
  * Init plugin
  * @access private
  */
 function squirrelmail_plugin_init_fortune() {
-  global $squirrelmail_plugin_hooks;
+    global $squirrelmail_plugin_hooks;
 
-  $squirrelmail_plugin_hooks['mailbox_index_before']['fortune'] = 'fortune';
-  $squirrelmail_plugin_hooks['options_display_inside']['fortune'] = 'fortune_options';
-  $squirrelmail_plugin_hooks['options_display_save']['fortune'] = 'fortune_save';
-  $squirrelmail_plugin_hooks['loading_prefs']['fortune'] = 'fortune_load';
+    $squirrelmail_plugin_hooks['mailbox_index_before']['fortune'] = 'fortune';
+    $squirrelmail_plugin_hooks['loading_prefs']['fortune'] = 'fortune_load';
+    $squirrelmail_plugin_hooks['optpage_loadhook_display']['fortune'] = 'fortune_options';
 }
 
 /**
- * Show fortune
+ * Call fortune display function
  * @access private
  */
 function fortune() {
-    global $fortune_visible, $color;
-
-    if (!$fortune_visible) {
-        return;
-    }
-
-    $fortune_location = '/usr/games/fortune';
-    $exist = file_exists($fortune_location);
-    echo "<center><table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" bgcolor=\"$color[10]\">\n".
-        "<tr><td><table width=\"100%\" cellpadding=\"2\" cellspacing=\"1\" border=\"0\" bgcolor=\"$color[5]\">\n".
-        "<tr><td align=\"center\">\n";
-    echo '<table><tr><td>';
-    if (!$exist) {
-        echo "$fortune_location" . _(" not found.");
-    } else {
-        echo "<center><em>" . _("Today's Fortune") . "</em><br /></font></center><pre>";
-        htmlspecialchars(system($fortune_location));
-    }
-
-    echo '</pre></td></tr></table></td></tr></table></td></tr></table></center>';
+    include_once(SM_PATH . 'plugins/fortune/functions.php');
+    fortune_function();
 }
 
 /**
- * Get fortune prefs
- * @access private
- */
-function fortune_load() {
-    global $username, $data_dir, $fortune_visible;
-
-    $fortune_visible = getPref($data_dir, $username, 'fortune_visible');
-}
-
-/**
- * Add fortune options
+ * Call fortune option display function
  * @access private
  */
 function fortune_options() {
-    global $fortune_visible;
-
-    echo "<tr>" . html_tag('td',_("Fortunes:"),'right','','nowrap') . "\n";
-    echo '<td><input name="fortune_fortune_visible" type="checkbox"';
-    if ($fortune_visible)
-        echo ' checked="checked"';
-    echo " /> " . _("Show fortunes at top of mailbox") . "</td></tr>\n";
+    include_once(SM_PATH . 'plugins/fortune/functions.php');
+    fortune_function_options();
 }
 
 /**
- * Save fortune prefs
+ * Call fortune prefs load function
  * @access private
  */
-function fortune_save() {
-    global $username,$data_dir;
-
-    if (sqgetGlobalVar('fortune_fortune_visible',$fortune_fortune_visible,SQ_POST)) {
-        setPref($data_dir, $username, 'fortune_visible', '1');
-    } else {
-        setPref($data_dir, $username, 'fortune_visible', '');
-    }
+function fortune_load() {
+    include_once(SM_PATH . 'plugins/fortune/functions.php');
+    fortune_function_load();
 }
-
 ?>
