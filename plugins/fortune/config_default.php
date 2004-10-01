476e5a1bd51eaa94e38d08f9f4361d729fa6c0f8
tokul <tokul@users.sourceforge.net>
2004-10-01 18:39:39 +0000

    moving functions to separate file, adding configuration files

---
+++ plugins/fortune/config_default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Default Fortune plugin configuration
+ * 
+ * Configuration defaults to /usr/games/fortune with short quotes
+ *
+ * @copyright (c) 2004 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * @package plugins
+ * @subpackage fortune
+ */
+
+/**
+ * program that displays quotes
+ * @global string $fortune_location
+ */
+$fortune_location = '/usr/games/fortune';
+
+/**
+ * options that have to be passed to program
+ * @global string $fortune_options
+ * @since 1.5.1
+ */
+$fortune_options = '-s';
+?>
