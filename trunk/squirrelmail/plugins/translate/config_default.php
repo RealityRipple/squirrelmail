4f43f87b1aacbeec02bcdc0438609e148eebd895
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-10-24 10:57:27 +0000

    adding phpdoc tags. updating translation engines.

--- trunk/squirrelmail/plugins/translate/config_default.php
+++ trunk/squirrelmail/plugins/translate/config_default.php
@@ -10,54 +10,67 @@
  * @subpackage translate
  */
 
-/** */
+/**
+ * Default translation engine
+ * @global string $translate_default_engine
+ */
 global $translate_default_engine;
 $translate_default_engine='babelfish';
 
 /**
- *
+ * Babelfish translation engine controls
+ * @global boolean $translate_babelfish_enabled
  */
 global $translate_babelfish_enabled;
 $translate_babelfish_enabled=true;
 
 /**
+ * Go.com translation engine controls
  *
+ * Translation is no longer available
+ * @global boolean $translate_go_enabled
  */
 global $translate_go_enabled;
 $translate_go_enabled=false;
 
 /**
- *
+ * Dictionary.com translation engine controls
+ * @global boolean $translate_dictionary_enabled
  */
 global $translate_dictionary_enabled;
 $translate_dictionary_enabled=true;
 
 /**
- *
+ * Google translation engine controls
+ * @global boolean $translate_google_enabled
  */
 global $translate_google_enabled;
 $translate_google_enabled=true;
 
 /**
- *
+ * Intertran translation engine controls
+ * @global boolean $translate_intertran_enabled
  */
 global $translate_intertran_enabled;
 $translate_intertran_enabled=true;
 
 /**
- *
+ * Promt translation engine controls
+ * @global boolean $translate_promt_enabled
  */
 global $translate_promt_enabled;
 $translate_promt_enabled=true;
 
 /**
- *
+ * Otenet translation engine controls
+ * @global boolean $translate_otenet_enabled
  */
 global $translate_otenet_enabled;
 $translate_otenet_enabled=true;
 
 /**
- *
+ * Gpltrans translation engine controls
+ * @global boolean $translate_gpltrans_enabled
  */
 global $translate_gpltrans_enabled;
 $translate_gpltrans_enabled=true;
@@ -74,7 +87,10 @@ global $translate_gpltrans_url;
 $translate_gpltrans_url='';
 
 /**
+ * Translation in compose controls
  *
+ * Currently unimplemened and disabled
+ * @global boolean $disable_compose_translate
  */
 global $disable_compose_translate;
 $disable_compose_translate=true;
@@ -82,10 +98,10 @@ $disable_compose_translate=true;
 /** Custom translation engine setup */
 
 /**
- * Controls inclusion of custom translation engines.
+ * Controls inclusion of custom translation engine.
  *
  * If you enable custon translation engines, you must include
- * translate_custom(), translate_custom_showtrad() and
+ * translate_form_custom(), translate_custom_showtrad() and
  * $translate_custom_showoption() functions in your config.
  * @example config-sample.php
  * @global bool $translate_custom_enabled
--- trunk/squirrelmail/plugins/translate/options.php
+++ trunk/squirrelmail/plugins/translate/options.php
@@ -18,66 +18,17 @@
  */
 define('SM_PATH','../../');
 
-/* SquirrelMail required files. */
+/** SquirrelMail required files. */
 include_once(SM_PATH . 'include/validate.php');
-include_once(SM_PATH . 'functions/display_messages.php');
-include_once(SM_PATH . 'functions/imap.php');
+/** Plugin functions */
 include_once(SM_PATH . 'plugins/translate/functions.php');
 
 displayPageHeader($color, 'None');
-
-// Save preferences
-if (sqgetGlobalVar('submit_translate',$tmp,SQ_POST)) {
-    if (sqgetGlobalVar('translate_translate_server',$translate_translate_server,SQ_POST)) {
-        setPref($data_dir, $username, 'translate_server', $translate_translate_server);
-    } else {
-        setPref($data_dir, $username, 'translate_server', $translate_default_engine);
-    }
-
-    if (sqgetGlobalVar('translate_translate_location',$translate_translate_location,SQ_POST)) {
-        setPref($data_dir, $username, 'translate_location', $translate_translate_location);
-    } else {
-        setPref($data_dir, $username, 'translate_location', 'center');
-    }
-
-    if (sqgetGlobalVar('translate_translate_show_read',$tmp,SQ_POST)) {
-        setPref($data_dir, $username, 'translate_show_read', '1');
-    } else {
-        setPref($data_dir, $username, 'translate_show_read', '');
-    }
-
-    if (sqgetGlobalVar('translate_translate_show_send',$tmp,SQ_POST)) {
-        setPref($data_dir, $username, 'translate_show_send', '1');
-    } else {
-        setPref($data_dir, $username, 'translate_show_send', '');
-    }
-
-    if (sqgetGlobalVar('translate_translate_same_window',$tmp,SQ_POST)) {
-       setPref($data_dir, $username, 'translate_same_window', '1');
-    } else {
-        setPref($data_dir, $username, 'translate_same_window', '');
-    }
-}
-
-// Move these calls to separate function
-$translate_server = getPref($data_dir, $username, 'translate_server',$translate_default_engine);
-
-$translate_location = getPref($data_dir, $username, 'translate_location');
-if ($translate_location == '') {
-    $translate_location = 'center';
-}
-$translate_show_read = getPref($data_dir, $username, 'translate_show_read');
-$translate_show_send = getPref($data_dir, $username, 'translate_show_send');
-$translate_same_window = getPref($data_dir, $username, 'translate_same_window');
 
 ?>
    <table width="95%" align="center" border="0" cellpadding="1" cellspacing="0"><tr><td bgcolor="<?php echo $color[0]; ?>">
       <center><b><?php echo _("Options") . ' - '. _("Translator"); ?></b></center>
    </td></tr></table>
-
-    <?php if (sqgetGlobalVar('submit_translate',$tmp,SQ_POST)) {
-        print '<center><h4>'._("Saved Translation Options")."</h4></center>\n";
-    }?>
 
    <p><?php echo _("Your server options are as follows:"); ?></p>
 
@@ -89,7 +40,9 @@ $translate_same_window = getPref($data_dir, $username, 'translate_same_window');
    <p>
 <?php
    echo _("You also decide if you want the translation box displayed, and where it will be located.") .
-        '<form action="'.$PHP_SELF.'" method="post">'.
+        '<form action="'.sqm_baseuri().'src/options.php'.'" method="post">'.
+        '<input type="hidden" name="optmode" value="submit">' .
+        '<input type="hidden" name="optpage" value="translate">' .
         '<table border="0" cellpadding="0" cellspacing="2">'.
             '<tr><td align="right" nowrap="nowrap">' .
              _("Select your translator:") .
