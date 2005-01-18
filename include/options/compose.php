99c9959347bfc4bcc47a6391a7ecd68d9ad50936
Thijs Kinkhorst <3808792+thijskh@users.noreply.github.com>
2005-01-18 18:58:53 +0000

    Add a new Options section "compose". Move some options from Display Preferences there - DP was way overcrowded with options not logically sorted. This is just a simple re-arranging of some options, as a simple attempt to at least clean things up a small bit.

---
+++ include/options/compose.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * options_compose.php
+ *
+ * Copyright (c) 1999-2005 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * Displays all options concerning composing of new messages
+ *
+ * @version $Id$
+ * @package squirrelmail
+ */
+
+/** Define the group constants for this options page. */
+define('SMOPT_GRP_GENERAL', 0);
+
+/**
+ * This function builds an array with all the information about
+ * the options available to the user, and returns it. The options
+ * are grouped by the groups in which they are displayed.
+ * For each option, the following information is stored:
+ * - name: the internal (variable) name
+ * - caption: the description of the option in the UI
+ * - type: one of SMOPT_TYPE_*
+ * - refresh: one of SMOPT_REFRESH_*
+ * - size: one of SMOPT_SIZE_*
+ * - save: the name of a function to call when saving this option
+ * @return array all option information
+ */
+function load_optpage_data_compose() {
+
+    /* Build a simple array into which we will build options. */
+    $optgrps = array();
+    $optvals = array();
+
+    /******************************************************/
+    /* LOAD EACH GROUP OF OPTIONS INTO THE OPTIONS ARRAY. */
+    /******************************************************/
+    
+    /*** Load the General Options into the array ***/
+    $optgrps[SMOPT_GRP_GENERAL] = _("Message Composition");
+    $optvals[SMOPT_GRP_GENERAL] = array();
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'editor_size',
+        'caption' => _("Width of Editor Window"),
+        'type'    => SMOPT_TYPE_INTEGER,
+        'refresh' => SMOPT_REFRESH_NONE,
+        'size'    => SMOPT_SIZE_TINY
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'editor_height',
+        'caption' => _("Height of Editor Window"),
+        'type'    => SMOPT_TYPE_INTEGER,
+        'refresh' => SMOPT_REFRESH_NONE,
+        'size'    => SMOPT_SIZE_TINY
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'location_of_buttons',
+        'caption' => _("Location of Buttons when Composing"),
+        'type'    => SMOPT_TYPE_STRLIST,
+        'refresh' => SMOPT_REFRESH_NONE,
+        'posvals' => array(SMPREF_LOC_TOP     => _("Before headers"),
+                           SMPREF_LOC_BETWEEN => _("Between headers and message body"),
+                           SMPREF_LOC_BOTTOM  => _("After message body"))
+    );
+
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'use_javascript_addr_book',
+        'caption' => _("Addressbook Display Format"),
+        'type'    => SMOPT_TYPE_STRLIST,
+        'refresh' => SMOPT_REFRESH_NONE,
+        'posvals' => array('1' => _("Javascript"),
+                           '0' => _("HTML"))
+    );
+    
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'forward_cc',
+        'caption' => _("Include CCs when Forwarding Messages"),
+        'type'    => SMOPT_TYPE_BOOLEAN,
+        'refresh' => SMOPT_REFRESH_NONE
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'include_self_reply_all',
+        'caption' => _("Include Me in CC when I Reply All"),
+        'type'    => SMOPT_TYPE_BOOLEAN,
+        'refresh' => SMOPT_REFRESH_NONE
+    );
+    
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'compose_new_win',
+        'caption' => _("Compose Messages in New Window"),
+        'type'    => SMOPT_TYPE_BOOLEAN,
+        'refresh' => SMOPT_REFRESH_ALL
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'compose_width',
+        'caption' => _("Width of Compose Window"),
+        'type'    => SMOPT_TYPE_INTEGER,
+        'refresh' => SMOPT_REFRESH_ALL,
+        'size'    => SMOPT_SIZE_TINY
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'compose_height',
+        'caption' => _("Height of Compose Window"),
+        'type'    => SMOPT_TYPE_INTEGER,
+        'refresh' => SMOPT_REFRESH_ALL,
+        'size'    => SMOPT_SIZE_TINY
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'sig_first',
+        'caption' => _("Append Signature before Reply/Forward Text"),
+        'type'    => SMOPT_TYPE_BOOLEAN,
+        'refresh' => SMOPT_REFRESH_NONE
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'body_quote',
+        'caption' => _("Prefix for Original Message when Replying"),
+        'type'    => SMOPT_TYPE_STRING,
+        'refresh' => SMOPT_REFRESH_NONE,
+        'size'    => SMOPT_SIZE_TINY,
+        'save'    => 'save_option_reply_prefix'
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'reply_focus',
+        'caption' => _("Cursor Position when Replying"),
+        'type'    => SMOPT_TYPE_STRLIST,
+        'refresh' => SMOPT_REFRESH_NONE,
+        'posvals' => array('' => _("To: field"),
+                           'focus' => _("Focus in body"),
+                           'select' => _("Select body"),
+                           'none' => _("No focus"))
+    );
+
+    $optvals[SMOPT_GRP_GENERAL][] = array(
+        'name'    => 'strip_sigs',
+        'caption' => _("Strip signature when replying"),
+        'type'    => SMOPT_TYPE_BOOLEAN,
+        'refresh' => SMOPT_REFRESH_NONE
+    );
+
+    /* Assemble all this together and return it as our result. */
+    $result = array(
+        'grps' => $optgrps,
+        'vals' => $optvals
+    );
+    return ($result);
+}
+
+/******************************************************************/
+/** Define any specialized save functions for this option page. ***/
+/******************************************************************/
+
+function save_option_header($option) {
+}
+
+
+?>
