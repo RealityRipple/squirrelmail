7aa03a1af8e3b13c40c3ea952303e40757e95e0b
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-02-07 13:43:16 +0000

    small comment changes.

--- trunk/squirrelmail/plugins/change_password/config.php
+++ trunk/squirrelmail/plugins/change_password/config.php
@@ -10,11 +10,13 @@
  * @subpackage change_password
  */
 
-// the password changing mechanism you're using
+/** the password changing mechanism you're using */
 $cpw_backend = 'template';
 
-
+/** the minimum and maximum length that the plugin will enforce on new passwords */
 $cpw_pass_min_length = 4;
 $cpw_pass_max_length = 25;
 
+/** whether we require the use of SSL/https to change a password */
 $cpw_require_ssl = FALSE;
+
