c1fefa23ee22709fd6b452bafa2311f5929d3bc7
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-03-26 18:52:41 +0000

    add includes from imap_search.php to imap.php for backwards compatibility. Thanks Alex for the tip.

--- trunk/squirrelmail/functions/imap.php
+++ trunk/squirrelmail/functions/imap.php
@@ -19,4 +19,9 @@ require_once(SM_PATH . 'functions/imap_mailbox.php');
 require_once(SM_PATH . 'functions/imap_messages.php');
 require_once(SM_PATH . 'functions/imap_general.php');
 
+/** This is here for bc */
+require_once(SM_PATH . 'functions/date.php');
+require_once(SM_PATH . 'functions/mailbox_display.php');
+require_once(SM_PATH . 'functions/mime.php');
+
 ?>
