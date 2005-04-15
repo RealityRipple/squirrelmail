3cf396d87513a6aec8cd350aa6cdd91de027fadd
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-04-15 08:44:06 +0000

    Looks like message highlighting contains a bug. The color is returned without the leading # character. This will fix it.
    TODO, somebody may want to check why the color is stored without the leading
    # char.

--- functions/mailbox_display.php
+++ functions/mailbox_display.php
@@ -627,6 +627,10 @@ function highlightMessage($sCol, $sVal, $highlight_list, &$aFormat) {
         }
     }
     if ($hlt_color) {
+        // Bug in highlight color??? 
+        if ($hlt_color{0} != '#') {
+            $hlt_color = '#'. $hlt_color;
+        }
         $aFormat['row']['color'] = $hlt_color;
         return true;
     } else {
