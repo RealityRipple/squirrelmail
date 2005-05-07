f356bd8eb446fdd473895d14d8de26cffd29619c
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-05-07 20:32:18 +0000

    add dovecot

--- trunk/squirrelmail/config/conf.pl
+++ trunk/squirrelmail/config/conf.pl
@@ -1099,13 +1099,16 @@ sub command19 {
     print "these servers.  If you would like to use them, please select your\n";
     print "IMAP server.  If you do not wish to use these work-arounds, you can\n";
     print "set this to \"other\", and none will be used.\n";
-    print "    cyrus       = Cyrus IMAP server\n";
-    print "    uw          = University of Washington's IMAP server\n";
-    print "    exchange    = Microsoft Exchange IMAP server\n";
     print "    courier     = Courier IMAP server\n";
-    print "    macosx      = Mac OS X Mailserver\n";
+    print "    cyrus       = Cyrus IMAP server\n";
+    print "    dovecot     = Dovecot Secure IMAP server\n";
+    print "    exchange    = Microsoft Exchange IMAP server\n";
     print "    hmailserver = hMailServer\n";
+    print "    macosx      = Mac OS X Mailserver\n";
+    print "    uw          = University of Washington's IMAP server\n";
+    print "\n";
     print "    other       = Not one of the above servers\n";
+    print "\n";
     print "[$WHT$imap_server_type$NRM]: $WHT";
     $new_imap_server_type = <STDIN>;
 
@@ -3508,14 +3511,17 @@ sub set_defaults {
     $continue = 0;
     while ( $continue != 1 ) {
         print "Please select your IMAP server:\n";
-        print "    cyrus       = Cyrus IMAP server\n";
-        print "    uw          = University of Washington's IMAP server\n";
-        print "    exchange    = Microsoft Exchange IMAP server\n";
         print "    courier     = Courier IMAP server\n";
-        print "    macosx      = Mac OS X Mailserver\n";
+        print "    cyrus       = Cyrus IMAP server\n";
+        print "    dovecot     = Dovecot Secure IMAP server\n";
+        print "    exchange    = Microsoft Exchange IMAP server\n";
         print "    hmailserver = hMailServer\n";
+        print "    macosx      = Mac OS X Mailserver\n";
         print "    mercury32   = Mercury/32\n";
+        print "    uw          = University of Washington's IMAP server\n";
+        print "\n";
         print "    quit        = Do not change anything\n";
+        print "\n";
         print "Command >> ";
         $server = <STDIN>;
         $server =~ s/[\r\n]//g;
@@ -3621,6 +3627,21 @@ sub set_defaults {
             $optional_delimiter             = "detect";
             $delete_folder                  = true;
             $force_username_lowercase       = true;
+
+            $continue = 1;
+        } elsif ( $server eq "dovecot" ) {
+            $imap_server_type               = "dovecot";
+            $default_folder_prefix          = "";
+            $trash_folder                   = "Trash";
+            $sent_folder                    = "Sent";
+            $draft_folder                   = "Drafts";
+            $show_prefix_option             = false;
+            $default_sub_of_inbox           = false;
+            $show_contain_subfolders_option = false;
+            $delete_folder                  = false;
+            $force_username_lowercase       = true;
+            $optional_delimiter             = "detect";
+            $disp_default_folder_prefix     = "<none>";
 
             $continue = 1;
         } elsif ( $server eq "quit" ) {
