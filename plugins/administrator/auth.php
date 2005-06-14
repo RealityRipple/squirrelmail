3207097950d949d801a19e874090f81c67f4983c
Thijs Kinkhorst <3808792+thijskh@users.noreply.github.com>
2005-06-14 10:22:22 +0000

    Disable access to the administrator plugin when plugin is disabled in config. Stable team/Jon: can I commit this into stable aswell?

--- plugins/administrator/auth.php
+++ plugins/administrator/auth.php
@@ -19,8 +19,13 @@
  * @return boolean
  */
 function adm_check_user() {
-    global $PHP_SELF;
+    global $PHP_SELF, $plugins;
     require_once(SM_PATH . 'functions/global.php');
+
+    /* fail if the plugin is not enabled */
+    if ( !in_array('administrator', $plugins) ) {
+        return FALSE;
+    }
 
     if ( !sqgetGlobalVar('username',$username,SQ_SESSION) ) {
         $username = '';
