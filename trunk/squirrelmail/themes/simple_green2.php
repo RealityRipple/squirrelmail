715f3a7afdbc271b9d94d07fca86e6d03e8ac1db
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-07-03 10:32:32 +0000

    adding two themes. #1217066 and #1217069 Simple Green 2 theme is similar to Simple Green theme, but colors are softer and contrast differs.

---
+++ trunk/squirrelmail/themes/simple_green2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Author:       Vicky Pyne <dotvicky@dotvicky.net>
+ * Date:         June 7, 2005
+ * Theme Name:   'Simple Green'
+ * Copyright:    Vicky Pyne <dotvicky@dotvicky.net>, 2005
+ * Licence:      GNU Public License
+ *
+ * Published on SquirrelMail SourceForge tracker (#1217066).
+ * Modified by SquirrelMail developers to match documentation guidelines.
+ * Copyright (c) 2005 The SquirrelMail Project Team
+ * @package squirrelmail
+ * @subpackage themes
+ */
+
+global $color;
+$color[0]   = '#DCDCDC'; // (light gray)     TitleBar
+$color[1]   = '#800000'; // (red)
+$color[2]   = '#CC0000'; // (light red)      Warning/Error Messages
+$color[3]   = '#669966'; // (grey-green)     Left Bar Background
+$color[4]   = '#FFFFFF'; // (white)          Normal Background
+$color[5]   = '#99CC99'; // (light-green)    Table Headers
+$color[6]   = '#000000'; // (black)          Text on left bar
+$color[7]   = '#006633'; // (green)          Links
+$color[8]   = '#000000'; // (black)          Normal text
+$color[9]   = '#ABABAB'; // (mid-gray)       Darker version of #0
+$color[10]  = '#666666'; // (dark gray)      Darker version of #9
+$color[11]  = '#003300'; // (darker green)   Special Folders color
+$color[12]  = '#EDEDED'; // (light gray)     Alternate color for message list
+$color[13]  = '#6666CC'; // (dull blue)      Color for quoted text -- > 1 quote
+$color[14]  = '#333399'; // (dark blue)      Color for quoted text -- >> 2 or more
+$color[15]  = '#669966'; // (grey-green)     Unselectable folders
+?>
