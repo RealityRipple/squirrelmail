995bc29f6fbd73e0fca7ac8b9e7c4f895870a318
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-07-03 12:22:27 +0000

    adding since tags. information is useful for developers that use SquirrelMail API.

--- trunk/squirrelmail/functions/identity.php
+++ trunk/squirrelmail/functions/identity.php
@@ -10,20 +10,24 @@
  *
  * @version $Id$
  * @package squirrelmail
+ * @since 1.4.2
  */
 
-/** Used to simplify includes */
+/** @ignore
+ * Used to simplify includes */
 if (!defined('SM_PATH')) {
     define('SM_PATH','../');
 }
 
+/** preference and signature functions */
 include_once(SM_PATH . 'include/load_prefs.php');
 
 /**
-* Returns an array of all the identities.
-* Array is keyed: full_name, reply_to, email_address, index, signature
-* @return array full_name,reply_to,email_address,index,signature
-*/
+ * Returns an array of all the identities.
+ * Array is keyed: full_name, reply_to, email_address, index, signature
+ * @return array full_name,reply_to,email_address,index,signature
+ * @since 1.4.2
+ */
 function get_identities() {
 
     global $username, $data_dir, $domain;
@@ -63,6 +67,7 @@ function get_identities() {
  * Function to save the identities array
  *
  * @param  array     $identities     Array of identities
+ * @since 1.5.1 and 1.4.5
  */
 function save_identities($identities) {
 
@@ -112,6 +117,7 @@ function save_identities($identities) {
  * @param   int         $id             Identity to modify
  * @param   string      $action         Action to perform
  * @return  array
+ * @since 1.5.1 and 1.4.5
  */
 function sqfixidentities( $identities, $id, $action ) {
 
@@ -185,6 +191,7 @@ function sqfixidentities( $identities, $id, $action ) {
  *
  * @param   array   $identity   Identitiy Array
  * @return  boolean
+ * @since 1.5.1 and 1.4.5
  */
 function empty_identity($ident) {
     if (empty($ident['full_name']) && empty($ident['email_address']) && empty($ident['signature']) && empty($ident['reply_to'])) {
