d65588dd17e8512e19920b28b1df5877d3b61d92
tokul <tokul@users.sourceforge.net>
2005-08-06 05:18:15 +0000

    some languages use syntax that can't be adapted for printf command. If translator tries to translate string correctly, he/she can create msgfmt errors.

--- src/help.php
+++ src/help.php
@@ -119,8 +119,8 @@ if (!isset($squirrelmail_language)) {
 if (file_exists("../help/$squirrelmail_language")) {
     $user_language = $squirrelmail_language;
 } else if (file_exists('../help/en_US')) {
-    echo "<center><font color=\"$color[2]\">";
-    printf (_("The help has not been translated to %s. It will be displayed in English instead."), $languages[$squirrelmail_language]['NAME']);
+    echo "<center><font color=\"$color[2]\">"
+        ._("The help has not been translated to selected language. It will be displayed in English instead.");
     echo '</font></center><br />';
     $user_language = 'en_US';
 } else {
