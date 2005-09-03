67cdf4b690589e1909061b9cf6867b67fd99f6c1
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-09-03 22:16:29 +0000

    String and PHP fix

--- trunk/squirrelmail/include/errors.php
+++ trunk/squirrelmail/include/errors.php
@@ -24,20 +24,21 @@ define('SQM_ERROR_PLUGIN',32);
 $aErrors['SQM_IMAP_NO_THREAD'] = array(
     'level'    => E_USER_ERROR,
     'category' => SQM_ERROR_IMAP,
-    'message'  =>  _("Thread sorting is not supported by your IMAP server.") . "\n" .
-                   _("Please contact your system administrator and report this error."),
+    'message'  => _("Thread sorting is not supported by your IMAP server.") . "\n" .
+                  _("Please contact your system administrator and report this error."),
     'link'     => '',
-    'tip'     => _("Run \"configure\", choose option 4 (General options) and set option 10 (Allow server thread sort to false")
+    'tip'      => _("Run \"configure\", choose option 4 (General options) and set option 10 (Allow server thread sort to false).")
 );
 
 $aErrors['SQM_IMAP_NO_SORT'] = array(
     'level'    => E_USER_ERROR,
     'category' => SQM_ERROR_IMAP,
-    'message'  =>  _( "Server-side sorting is not supported by your IMAP server.") . "\n" .
-                   _("Please contact your system administrator and report this error."),
+    'message'  => _( "Server-side sorting is not supported by your IMAP server.") . "\n" .
+                  _("Please contact your system administrator and report this error."),
     'link'     => '',
-    'tip'     => _("Run \"configure\", choose option 4 (General options) and set option 11 (Allow server-side sorting to false")
+    'tip'      => _("Run \"configure\", choose option 4 (General options) and set option 11 (Allow server-side sorting to false).")
 );
 
-
 //$aError['SQM_FS'] // Filesystem related errors
+
+?>
