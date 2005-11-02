c3b9aa768452b2d66e657e28b62cea904a22623e
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-11-02 22:26:24 +0000

    Sorry, copy paste didn't workout as it should.

--- trunk/squirrelmail/src/compose.php
+++ trunk/squirrelmail/src/compose.php
@@ -1672,13 +1672,13 @@ function deliverMessage($composeMessage, $draft=false) {
             $aMailbox = sqm_api_mailbox_select($imap_stream, $iAccount, $mailbox,array('setindex' => $what, 'offset' => $startMessage),array());
             //sqimap_mailbox_select ($imap_stream, $mailbox);
             $aUpdatedMsgs = sqimap_toggle_flag($imap_stream, array($passed_id), '\\Answered', true, false);
-             if (isset($aUpdatedMsgs[$iUid]['FLAGS'])) {
+             if (isset($aUpdatedMsgs[$passed_id]['FLAGS'])) {
                 /**
                  * Only update the cached headers if the header is
                  * cached.
                  */
-                if (isset($aMailbox['MSG_HEADERS'][$iUid])) {
-                    $aMailbox['MSG_HEADERS'][$iUid]['FLAGS'] = $aMsg['FLAGS'];
+                if (isset($aMailbox['MSG_HEADERS'][$passed_id])) {
+                    $aMailbox['MSG_HEADERS'][$passed_id]['FLAGS'] = $aMsg['FLAGS'];
                 }
             }
         }
