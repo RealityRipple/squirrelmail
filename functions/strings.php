7346f9a58023079ca9c122d91a108285e0fd3125
tokul <tokul@users.sourceforge.net>
2006-02-12 16:11:50 +0000

    Upping HEAD version number. 1.5.1 scripts are stored in SM-1_5_1 branch.

--- functions/strings.php
+++ functions/strings.php
@@ -19,14 +19,14 @@ if (!defined('SM_PATH')) define('SM_PATH','../');
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.5.1 [CVS]';
+$version = '1.5.2 [CVS]';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
  * $sm_internal_version = array (release, major, minor)
  */
 global $SQM_INTERNAL_VERSION;
-$SQM_INTERNAL_VERSION = array(1,5,1);
+$SQM_INTERNAL_VERSION = array(1,5,2);
 
 /**
  * There can be a circular issue with includes, where the $version string is
