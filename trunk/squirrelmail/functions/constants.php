88d6eef21d088c2bd36c00f2c20abecfda61cd9b
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-02-20 18:11:01 +0000

    adding information about version that introduced constants adding SM_PATH define in order to avoid PHP errors on direct file access.

--- trunk/squirrelmail/functions/constants.php
+++ trunk/squirrelmail/functions/constants.php
@@ -11,7 +11,11 @@
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id$
  * @package squirrelmail
+ * @since 1.2.0
  */
+
+/** @ignore */
+if (! defined('SM_PATH')) define('SM_PATH','../');
 
 /** Need to enable plugin functions for a hook */
 require_once(SM_PATH . 'functions/plugin.php');  /* Required for the hook */
@@ -20,56 +24,75 @@ require_once(SM_PATH . 'functions/plugin.php');  /* Required for the hook */
 /* Set values for constants used by SquirrelMail preferences. */
 /**************************************************************/
 
-/* Define basic, general purpose preference constants. */
+/**
+ * Define basic, general purpose preference constants.
+ * @since 1.2.0
+ */
 define('SMPREF_NO', 0);
 define('SMPREF_OFF', 0);
 define('SMPREF_YES', 1);
 define('SMPREF_ON', 1);
 define('SMPREF_NONE', 'none');
 
-/* Define constants for location based preferences. */
+/**
+ * Define constants for location based preferences.
+ * @since 1.2.0
+ */
 define('SMPREF_LOC_TOP', 'top');
 define('SMPREF_LOC_BETWEEN', 'between');
 define('SMPREF_LOC_BOTTOM', 'bottom');
 define('SMPREF_LOC_LEFT', '');
 define('SMPREF_LOC_RIGHT', 'right');
 
-/* Define preferences for folder settings. */
+/**
+ * Define preferences for folder settings.
+ * @since 1.2.0
+ */
 define('SMPREF_UNSEEN_NONE', 1);
 define('SMPREF_UNSEEN_INBOX', 2);
 define('SMPREF_UNSEEN_ALL', 3);
-define('SMPREF_UNSEEN_SPECIAL', 4); // Only special folders
-define('SMPREF_UNSEEN_NORMAL', 5);  // Only normal folders
+define('SMPREF_UNSEEN_SPECIAL', 4); // Only special folders (since 1.2.5)
+define('SMPREF_UNSEEN_NORMAL', 5);  // Only normal folders (since 1.2.5)
 define('SMPREF_UNSEEN_ONLY', 1);
 define('SMPREF_UNSEEN_TOTAL', 2);
 
-/* Define constants for time/date display preferences. */
+/**
+ * Define constants for time/date display preferences.
+ * @since 1.2.0
+ */
 define('SMPREF_TIME_24HR', 1);
 define('SMPREF_TIME_12HR', 2);
 
-/* Define constants for javascript preferences. */
+/**
+ * Define constants for javascript preferences.
+ * @since 1.2.0
+ */
 define('SMPREF_JS_OFF', 0);
 define('SMPREF_JS_ON', 1);
 define('SMPREF_JS_AUTODETECT', 2);
 
 /**
  * default value for page_selector_max
+ * @since 1.5.1
  */
 define('PG_SEL_MAX', 10);
 
 
 /**
  * The number of pages to cache msg headers
+ * @since 1.5.1
  */
 define('SQM_MAX_PAGES_IN_CACHE',5);
 
 /**
  * The number of mailboxes to cache msg headers
+ * @since 1.5.1
  */
 define('SQM_MAX_MBX_IN_CACHE',3);
 
 /**
  * Sort constants used for sorting of messages
+ * @since 1.5.1
  */
 define('SQSORT_NONE',0);
 define('SQSORT_DATE_ASC',1);
@@ -91,11 +114,13 @@ define('SQSORT_INT_DATE_DESC',14);
  * Special sort constant thread which is added to above sort mode.
  * By doing a bitwise check ($sort & SQSORT_THREAD) we know if the mailbox
  * is sorted by thread.
+ * @since 1.5.1
  */
 define('SQSORT_THREAD',32);
 
 /**
  * Mailbox preference array keys
+ * @since 1.5.1
  */
 define('MBX_PREF_SORT',0);
 define('MBX_PREF_LIMIT',1);
@@ -106,6 +131,7 @@ define('MBX_PREF_COLUMNS',4);
 
 /**
  * Email address array keys
+ * @since 1.5.1
  */
 define('SQM_ADDR_PERSONAL', 0);
 define('SQM_ADDR_ADL',      1);
@@ -115,6 +141,7 @@ define('SQM_ADDR_HOST',     3);
 /**
  * Supported columns to show in a messages list
  * The MBX_PREF_COLUMNS contains an ordered array with these columns
+ * @since 1.5.1
  */
 define('SQM_COL_CHECK',0);
 define('SQM_COL_FROM',1);
@@ -129,7 +156,7 @@ define('SQM_COL_TO', 9);
 define('SQM_COL_CC', 10);
 define('SQM_COL_BCC', 11);
 
-
+/** @since 1.2.0 */
 do_hook('loading_constants');
 
 ?>
