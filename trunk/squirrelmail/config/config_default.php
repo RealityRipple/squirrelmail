cb2ea37d43f34c0de189817abf9296df099ab643
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-07-09 22:20:46 +0000

    - Make the default attachment dir /var/local/squirrelmail/attach, not   $data_dir.

--- trunk/squirrelmail/config/config_default.php
+++ trunk/squirrelmail/config/config_default.php
@@ -493,7 +493,7 @@ $data_dir = '/var/local/squirrelmail/data';
  *    + It should probably be another directory than data_dir.
  * @global string $attachment_dir
  */
-$attachment_dir = $data_dir;
+$attachment_dir = '/var/local/squirrelmail/attach';
 
 /**
  * Hash level used for data directory.
