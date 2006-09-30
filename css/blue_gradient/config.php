8294c11b7dfba9d9c63e151e1f30705a90957309
stevetruckstuff <stevetruckstuff@users.sourceforge.net>
2006-09-30 10:39:30 +0000

    Demo user provided alternate stylsheet

---
+++ css/blue_gradient/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * CSS Theme description
+ *
+ * @copyright &copy; 2004-2006 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * @package squirrelmail
+ * @subpackage css_themes
+ */
+$css_themes[] = array('NAME'=>_("Blue Gradient"),'PATH'=> SM_PATH.'css/blue_gradient/');
