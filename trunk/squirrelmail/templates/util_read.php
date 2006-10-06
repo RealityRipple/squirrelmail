b31d223db3910ddf0f44aabf08a0a2bdf70251a8
stevetruckstuff <stevetruckstuff@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-10-06 21:56:45 +0000

    Utility file for reading messages.

---
+++ trunk/squirrelmail/templates/util_read.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * util_read.php
+ *
+ * Utility file containing functions related to reading messages
+ * 
+ * @copyright &copy; 1999-2006 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * @package squirrelmail
+ * @subpackage templates
+ */
+
+/**
+ * Return a string representing the priority of a message
+ */
+function priorityStr($p) {
+    return htmlspecialchars(getPriorityStr($p));
+}
+
+?>
