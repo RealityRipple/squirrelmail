e459a9306b5a0da65e91e1be6bb3a3c87220bf69
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-12-10 19:43:27 +0000

    (finally)fix message_details plugin for new plugin infrastructure, and as added bonus, make it work without javascript too

--- trunk/squirrelmail/plugins/message_details/setup.php
+++ trunk/squirrelmail/plugins/message_details/setup.php
@@ -29,10 +29,10 @@ function squirrelmail_plugin_init_message_details()
  * Add message details link in message view
  * @access private
  */
-function show_message_details() {
+function show_message_details($links) {
     global $passed_id, $mailbox, $ent_num,
            $javascript_on;
-
+    
     if (strlen(trim($mailbox)) < 1) {
         $mailbox = 'INBOX';
     }
@@ -41,20 +41,23 @@ function show_message_details() {
               '&mailbox=' . urlencode($mailbox) .
               '&passed_id=' . $passed_id;
 
-    $print_text = _("View Message details");
+    $url = $javascript_on ? 'javascript:MessageSource();' : 
+        '../plugins/message_details/message_details_main.php' .
+        $params;
 
-    $result = '';
     /* Output the link. */
+    $links[] = array('URL' => $url,
+        'Text' => _("View Message details") );
+
     if ($javascript_on) {
-        $result = '<script type="text/javascript">' . "\n" .
+        echo '<script type="text/javascript">' . "\n" .
                 '<!--' . "\n" .
                 "  function MessageSource() {\n" .
                 '    window.open("../plugins/message_details/message_details_main.php' .
                         $params . '","MessageDetails","width=800,height=600");' . "\n".
                 "  }\n" .
                 "// -->\n" .
-                "</script>\n" .
-                "&nbsp;|&nbsp;<a href=\"javascript:MessageSource();\">$print_text</a>\n";
+                "</script>\n\n";
     }
-    echo $result;
 }
+
