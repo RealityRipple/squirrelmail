c2fef8cbf8d63bf50fdcef3f2c37a9583d65c66f
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-01-07 17:02:32 +0000

    new file for compose-related functions

---
+++ trunk/squirrelmail/functions/compose.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * compose.php
+ *
+ * Functions for message compositon: writing a message, attaching files etc.
+ *
+ * @author Thijs Kinkhorst <kink at squirrelmail.org>
+ * @copyright &copy; 1999-2007 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * @package squirrelmail
+ */
+
+
