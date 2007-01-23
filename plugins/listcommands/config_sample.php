babe6a7f192b6811e49dc32442ae821c93e4e131
pdontthink <paul@squirrelmail.org>
2007-01-23 03:37:36 +0000

    Somehow went missing options.php and config file, minor adjustments to template output formatting.

---
+++ plugins/listcommands/config_sample.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * SquirrelMail List Commands Plugin
+ * Configuration File (config_sample.php, to use, please copy to config.php)
+ *
+ * @copyright &copy; 1999-2007 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * @package plugins
+ * @subpackage listcommands
+ */
+
+
+// Set this to 1 if users should be allowed to add a list of
+// mailing list addresses that don't follow RFC 2369, but when
+// replying to should also show "reply to list" and "post to list"
+// links... set to 0 (zero) otherwise.
+//
+$listcommands_allow_non_rfc_list_management = 0;
+
+
