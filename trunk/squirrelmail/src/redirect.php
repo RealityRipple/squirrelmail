c520661dd31b654743b31a16d364bf6bf8feb533
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-06-25 15:24:42 +0000

    drop obsolete parameter, thanks Tomas Kuliavas for finding

--- trunk/squirrelmail/src/redirect.php
+++ trunk/squirrelmail/src/redirect.php
@@ -129,11 +129,9 @@ $attachment_common_types_parsed = array();
 sqsession_register($attachment_common_types, 'attachment_common_types');
 sqsession_register($attachment_common_types_parsed, 'attachment_common_types_parsed');
 
-$debug = false;
-
 if ( sqgetGlobalVar('HTTP_ACCEPT', $http_accept, SQ_SERVER) &&
     !isset($attachment_common_types_parsed[$http_accept]) ) {
-    attachment_common_parse($http_accept, $debug);
+    attachment_common_parse($http_accept);
 }
 
 /* Complete autodetection of Javascript. */
@@ -169,7 +167,7 @@ exit;
 
 /* --------------------- end main ----------------------- */
 
-function attachment_common_parse($str, $debug) {
+function attachment_common_parse($str) {
     global $attachment_common_types, $attachment_common_types_parsed;
 
     /*
