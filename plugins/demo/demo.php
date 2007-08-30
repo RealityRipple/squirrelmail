006dd4d12f7bdd6e7d5277eb684dffcf2396d126
pdontthink <paul@squirrelmail.org>
2007-08-30 09:13:19 +0000

    Initial import

---
+++ plugins/demo/demo.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+  * SquirrelMail Demo Plugin
+  *
+  * This page is used as a place holder for custom plugin 
+  * pages that are accessed directly by the client.
+  *
+  * @copyright &copy; 2006-2007 The SquirrelMail Project Team
+  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+  * @version $Id$
+  * @package plugins
+  * @subpackage demo
+  */
+
+
+include_once('../../include/init.php');
+
+
+// Make sure plugin is activated!
+//
+global $plugins;
+if (!in_array('demo', $plugins))
+   exit;
+
+
+global $oTemplate, $color;
+displayPageHeader($color, 'None');
+
+$oTemplate->display('plugins/demo/demo.tpl');
+$oTemplate->display('footer.tpl');
+
+
+
---
+++ plugins/demo/templates/default/demo.tpl
@@ -0,0 +1,29 @@
+<?php
+
+/**
+  * demo.tpl
+  *
+  * This template is used as a place holder for custom plugin pages
+  * that are accessed directly by the client for the Demo plugin.
+  *
+  * The following variables are available in this template:
+  *
+  * (none)
+  *
+  * @copyright &copy; 1999-2007 The SquirrelMail Project Team
+  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+  * @version $Id$
+  * @package squirrelmail
+  * @subpackage plugins
+  */
+
+
+// retrieve the template vars
+//
+extract($t);
+
+
+?>
+
+<strong>HELLO WORLD</strong>
+
