33b3166b39fea98984a5208e6ccc80f66d442b6a
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-08-31 23:19:36 +0000

    Make identities matching case insensitive

--- trunk/squirrelmail/src/compose.php
+++ trunk/squirrelmail/src/compose.php
@@ -855,7 +855,7 @@ function newMail ($mailbox='', $passed_id='', $passed_ent_id='', $action='', $se
         if (count($idents) > 1) {
             foreach($idents as $nr=>$data) {
                 $enc_from_name = '"'.$data['full_name'].'" <'. $data['email_address'].'>';
-                if($enc_from_name == $orig_from) {
+                if(strtolower($enc_from_name) == strtolower($orig_from)) {
                     $identity = $nr;
                     break;
                 }
