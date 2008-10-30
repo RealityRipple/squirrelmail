cce81f96d7545dcc97f09cb0048de08bda9f610a
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2008-10-30 21:30:09 +0000

    Minor spacing fix

--- trunk/squirrelmail/functions/options.php
+++ trunk/squirrelmail/functions/options.php
@@ -451,7 +451,8 @@ class SquirrelOption {
                 $width = 25;
         }
 
-        return addInput('new_' . $this->name, $this->value, $width, 0, $this->aExtraAttribs) . htmlspecialchars($this->trailing_text); 
+//TODO: might be better to have a separate template file for all widgets, because then the layout of the widget and the "trailing text" can be customized - they are still hard coded here
+        return addInput('new_' . $this->name, $this->value, $width, 0, $this->aExtraAttribs) . ' ' . htmlspecialchars($this->trailing_text);
     }
 
     /**
