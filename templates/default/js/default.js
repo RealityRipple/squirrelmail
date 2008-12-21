7525bfeb689d0ad15e85009bcf6579ec85ad7606
pdontthink <paul@squirrelmail.org>
2008-12-21 09:43:36 +0000

    Add textarea to list of possible focus candidates

--- templates/default/js/default.js
+++ templates/default/js/default.js
@@ -271,7 +271,7 @@ function checkForm(smaction) {
             var e = document.forms[i].elements.length;
             var j = 0;
             while( pos == -1 && j < e ) {
-                if ( document.forms[i].elements[j].type == 'text' || document.forms[i].elements[j].type == 'password' ) {
+                if ( document.forms[i].elements[j].type == 'text' || document.forms[i].elements[j].type == 'password' || document.forms[i].elements[j].type == 'textarea' ) {
                     pos = j;
                 }
                 j++;
