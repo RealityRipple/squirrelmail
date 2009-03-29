128735b58af83f124a4066476ed986cbc9d34126
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-03-29 00:11:34 +0000

    Add PHP index file

---
+++ trunk/squirrelmail/doc/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Redirects to the index.html file.
+ *
+ * @copyright &copy; 1999-2009 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: index.php 12128 2007-01-13 20:15:44Z kink $
+ * @package squirrelmail
+ */
+
+header('Location: index.html');
+
