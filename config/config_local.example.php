d7b944fff73cfec3e3b1fb21865d71c9d41130f1
pdontthink <paul@squirrelmail.org>
2009-04-01 21:16:57 +0000

    Document hidden settings for use in config_local

--- config/config_local.example.php
+++ config/config_local.example.php
@@ -13,3 +13,39 @@
  * @package squirrelmail
  * @subpackage config
  */
+
+
+/**
+ * What follows are notes about "hidden" settings that
+ * are not defined in config.php and are only meant to
+ * be optionally defined by administrators who need to
+ * suit specific (unusual) setups.  This file, of course,
+ * is not limited to setting these values - you can still
+ * specify overrides for anything in config.php.
+ *
+ * $custom_session_handlers (array) allows the definition
+ * of custom PHP session handlers.  This feature is well
+ * documented in the code in include/init.php
+ *
+ * hide_squirrelmail_header (must be defined as a constant:
+ * define('hide_squirrelmail_header', 1);
+ * This allows the administrator to force SquirrelMail never
+ * to add its own Received headers with user information in
+ * them.  This is VERY DANGEROUS and is HIGHLY DISCOURAGED
+ *
+ * $show_timezone_name allows (boolean) the addition of the
+ * timezone name to the Date header in outgoing messages.
+ * Turning this on violates RFC 822 syntax and can result in
+ * more serious problems (unencoded 8 bit characters in headers)
+ * on some systems.
+ *
+ * $force_crlf_default (string) Can be used to force CRLF or LF
+ * line endings in decoded message parts.  In some environments
+ * this allows attachments to be downloaded with operating-system
+ * friendly line endings.  This setting may be overridden by
+ * certain plugins or on systems running PHP versions less than
+ * 4.3.0.  Set to 'CRLF' or 'LF' or, to force line endings to be
+ * unmolested, set to some other string, such as 'NOCHANGE'
+ *
+ */
+
