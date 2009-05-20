0f64b782533b62782ab05c9071b658c949111274
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-05-20 20:59:44 +0000

    When sending an address literal to an SMTP EHLO command, do it with the right syntax

--- trunk/squirrelmail/class/deliver/Deliver_SMTP.class.php
+++ trunk/squirrelmail/class/deliver/Deliver_SMTP.class.php
@@ -135,6 +135,11 @@ class Deliver_SMTP extends Deliver {
             $helohost = $domain;
         }
 
+        // if the host is an IPv4 address, enclose it in brackets
+        //
+        if (preg_match('/\d+\.\d+\.\d+\.\d+/', $helohost))
+            $helohost = '[' . $helohost . ']';
+
         /* Lets introduce ourselves */
         fputs($stream, "EHLO $helohost\r\n");
         // Read ehlo response
