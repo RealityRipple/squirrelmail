fd6531c9c45bba3c7225a55914958583e2f12a7f
Andy <webmaster@RealityRipple.com>
2018-09-25 18:44:59 -0700

    DKIM Header Fields Don't Need Color

--- config/config_dkim.php
+++ config/config_dkim.php
@@ -2,7 +2,7 @@
  global $color, $row_highlite_color;
  global $openssl_cmds, $tmp_dir;
 
- $row_highlite_color = $color[16];
+ $row_highlite_color = '';//$color[16];
  $openssl_cmds = SM_PATH . 'plugins/dkim/openssl-cmds.sh';
  $tmp_dir = $GLOBALS['siteRoot'].'/rrs/.maildata/data/tmp/';
 ?>
