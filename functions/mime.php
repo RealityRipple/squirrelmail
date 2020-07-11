eb7f686a56bb6862c7e6eac4626d75be6d72f6a0
pdontthink <paul@squirrelmail.org>
2020-07-11 04:31:18 +0000

    Add MIME type parameters ("type0", "type1") to the end of the plugin arguments for all attachment hooks

--- functions/mime.php
+++ functions/mime.php
@@ -598,7 +598,8 @@ function buildAttachmentArray($message, $exclude_id, $mailbox, $id) {
            returning any changes, changes should simply be made to the original
            arguments themselves. */
         $temp = array(&$links, &$startMessage, &$id, &$urlMailbox, &$ent,
-                    &$defaultlink, &$display_filename, &$where, &$what);
+                    &$defaultlink, &$display_filename, &$where, &$what,
+                    $type0, $temp1);
         do_hook("attachment $type0/$type1", $temp);
         /* The API for this hook has changed as of 1.5.2 so that all plugin
            arguments are passed in an array instead of each their own plugin
