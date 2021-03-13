45ae2d0072c63435b83ac51d716dd0fb06007502
pdontthink <paul@squirrelmail.org>
2021-03-13 00:14:00 +0000

    Remove stray curly brackets

--- class/deliver/Deliver_SMTP.class.php
+++ class/deliver/Deliver_SMTP.class.php
@@ -163,7 +163,7 @@ class Deliver_SMTP extends Deliver {
         $tmp = $this->parse_ehlo_response($stream);
         if ($this->errorCheck($tmp,$stream)) {
             // fall back to HELO if EHLO is not supported (error 5xx)
-            if ($this->dlv_ret_nr{0} == '5') {
+            if ($this->dlv_ret_nr[0] == '5') {
                 fputs($stream, "HELO $helohost\r\n");
                 $tmp = fgets($stream,1024);
                 if ($this->errorCheck($tmp,$stream)) {
